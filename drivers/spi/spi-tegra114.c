64566e239fc67686259cc7f6e0b3e951117db4ca
Shardar Shariff Md <smohammed@nvidia.com>
2013-10-25 21:04:07 +0530

    spi: tegra: Add tegra_spi_cs_low callback
    added tegra_spi_cs_low() callback, called
    from spi core for spi client users to set chip
    select state(low/high).
    
    Bug 1371286
    Bug 200601675
    
    Change-Id: Ifea4282a46f88c1744c44e6fd6dee7d98d72bc05
    Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
    Reviewed-on: http://git-master/r/303904
    (cherry picked from commit e40ed8fb46dabb6a9b63937ceb73ad5af4d5b593)
    Signed-off-by: Krishna Yarlagadda <kyarlagadda@nvidia.com>
    Reviewed-on: http://git-master/r/1165012
    (cherry picked from commit 4556ab827a8c452e71fac92c1ab9c222cd2bafa4)
    Reviewed-on: https://git-master.nvidia.com/r/1747149
    (cherry picked from commit edc9dfba11e7e2d42e05dc59c4f05ea9f8081ff9)
    Signed-off-by: Vishwaroop A <va@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.4/+/2313084
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/spi/spi-tegra114.c
+++ drivers/spi/spi-tegra114.c
@@ -1029,6 +1029,34 @@ static void tegra_spi_dump_regs(struct tegra_spi_data *tspi)
 		tegra_spi_readl(tspi, SPI_FIFO_STATUS));
 }
 
+static  int tegra_spi_cs_low(struct spi_device *spi, bool state)
+{
+	struct tegra_spi_data *tspi = spi_controller_get_devdata(spi->master);
+	int ret;
+	unsigned long val;
+	unsigned long flags;
+
+	ret = pm_runtime_get_sync(tspi->dev);
+	if (ret < 0) {
+		dev_err(tspi->dev, "pm runtime failed, e = %d\n", ret);
+		return ret;
+	}
+
+	spin_lock_irqsave(&tspi->lock, flags);
+	if (!(spi->mode & SPI_CS_HIGH)) {
+		val = tegra_spi_readl(tspi, SPI_COMMAND1);
+		if (state)
+			val &= ~SPI_CS_POL_INACTIVE(spi->chip_select);
+		else
+			val |= SPI_CS_POL_INACTIVE(spi->chip_select);
+		tegra_spi_writel(tspi, val, SPI_COMMAND1);
+	}
+
+	spin_unlock_irqrestore(&tspi->lock, flags);
+	pm_runtime_put(tspi->dev);
+	return 0;
+}
+
 static int tegra_spi_transfer_one_message(struct spi_master *master,
 			struct spi_message *msg)
 {
@@ -1330,6 +1358,7 @@ static int tegra_spi_probe(struct platform_device *pdev)
 	bus_num = of_alias_get_id(pdev->dev.of_node, "spi");
 	if (bus_num >= 0)
 		master->bus_num = bus_num;
+	master->spi_cs_low = tegra_spi_cs_low;
 
 	tspi->master = master;
 	tspi->dev = &pdev->dev;
