d5bc45e254d544b2c3a05cc30737e3703d341158
Peter De Schrijver <pdeschrijver@nvidia.com>
2016-12-20 14:44:58 +0200

    clk: tegra: switch to determine rate
    This allows honoring min and max rates.
    
    Includes fixes by Aleks Frid <afrid@nvidia.com>
    
    Change-Id: Ic18e7180c7c9aede9bae81f0f0c1b9fe43394812
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1548511
    (cherry picked from commit 02af13566c8e05a22a87aff184fd2283d20b07d9)
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1735529
    (cherry picked from commit ad9abc0b4ffb8366599a13cfbc0e1f714ea55e3d)
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2427518
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/clk/tegra/clk-periph.c
+++ drivers/clk/tegra/clk-periph.c
@@ -45,8 +45,8 @@ static unsigned long clk_periph_recalc_rate(struct clk_hw *hw,
 	return div_ops->recalc_rate(div_hw, parent_rate);
 }
 
-static long clk_periph_round_rate(struct clk_hw *hw, unsigned long rate,
-				  unsigned long *prate)
+static int clk_periph_determine_rate(struct clk_hw *hw,
+				     struct clk_rate_request *req)
 {
 	struct tegra_clk_periph *periph = to_clk_periph(hw);
 	const struct clk_ops *div_ops = periph->div_ops;
@@ -54,7 +54,7 @@ static long clk_periph_round_rate(struct clk_hw *hw, unsigned long rate,
 
 	__clk_hw_set_clk(div_hw, hw);
 
-	return div_ops->round_rate(div_hw, rate, prate);
+	return div_ops->determine_rate(div_hw, req);
 }
 
 static int clk_periph_set_rate(struct clk_hw *hw, unsigned long rate,
@@ -121,7 +121,7 @@ const struct clk_ops tegra_clk_periph_ops = {
 	.get_parent = clk_periph_get_parent,
 	.set_parent = clk_periph_set_parent,
 	.recalc_rate = clk_periph_recalc_rate,
-	.round_rate = clk_periph_round_rate,
+	.determine_rate = clk_periph_determine_rate,
 	.set_rate = clk_periph_set_rate,
 	.is_enabled = clk_periph_is_enabled,
 	.enable = clk_periph_enable,
@@ -142,7 +142,7 @@ static const struct clk_ops tegra_clk_periph_no_gate_ops = {
 	.get_parent = clk_periph_get_parent,
 	.set_parent = clk_periph_set_parent,
 	.recalc_rate = clk_periph_recalc_rate,
-	.round_rate = clk_periph_round_rate,
+	.determine_rate = clk_periph_determine_rate,
 	.set_rate = clk_periph_set_rate,
 	.restore_context = clk_periph_restore_context,
 };
