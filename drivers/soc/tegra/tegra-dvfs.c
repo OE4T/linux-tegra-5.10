93649bdb81a2f935183452afa1a8d8988e1887fc
Bharat Nihalani <bnihalani@nvidia.com>
2017-01-09 20:32:28 +0530

    soc: tegra: dvfs: do not suspend "vdd-gpu"
    Suspending "vdd-gpu" rail sets the rail to nominal voltage
    during shutdown, reboot and suspend operations.
    
    Doing this for "vdd-gpu" rail causes hang during reboot. Hence
    avoid doing this operation from tegra dvfs suspend.
    
    nvgpu driver will take care of turning off the rail in case of
    suspend and shutdown paths.
    
    Bug 200260070
    
    Change-Id: I08545fa7606b37bfb92b57845875912081bb5511
    Signed-off-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-on: http://git-master/r/1282227

--- drivers/soc/tegra/tegra-dvfs.c
+++ drivers/soc/tegra/tegra-dvfs.c
@@ -1106,7 +1106,8 @@ static int tegra_dvfs_suspend_one(void)
 	int ret = 0;
 
 	list_for_each_entry(rail, &dvfs_rail_list, node) {
-		if ((rail->suspended) ||
+		if ((rail == tegra_gpu_rail) || /* don't suspend vdd-gpu */
+		    (rail->suspended) ||
 		    (rail->disabled) ||
 		    (!tegra_dvfs_from_rails_suspended_or_solved(rail)))
 			continue;
