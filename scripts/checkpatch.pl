7f576cab9e124f43d24cc9e8bf14809f8118d3a7
Sachin Nikam <snikam@nvidia.com>
2017-01-19 09:00:59 +0530

    scripts: checkpatch: reminder to update NVIDIA-REVIEWERS
    Whenever any folder is add/move/delete need to update
    NVIDIA-REVIEWERS as well.
    As this reviewers file is reference for various tools.
    Automatic Commit Validation (ACV) already runs this checkpatch.pl
    hence add reminder to update for NVIDIA-REVIEWERS as well.
    
    Bug 200270574
    Bug 200298099
    
    Change-Id: I54e80b44b4d07d6962f53bdf71e0e160d71b883c
    Signed-off-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-on: http://git-master/r/1287898
    Reviewed-on: http://git-master/r/1460382
    (cherry picked from commit 2cbcc4ad1e3811f752b81c67ccabbd819585e2c4)
    Reviewed-on: https://git-master.nvidia.com/r/1745637
    (cherry picked from commit 71c7594fdf3cbac0454e0af90a8bebaca0e843d0)
    Signed-off-by: Krishna Yarlagadda <kyarlagadda@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.4/+/2306464
    Tested-by: Bitan Biswas <bbiswas@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2618,9 +2618,13 @@ sub process {
 			}
 		}
 
-# Check if MAINTAINERS is being updated.  If so, there's probably no need to
-# emit the "does MAINTAINERS need updating?" message on file add/move/delete
+# Check if MAINTAINERS and/or NVIDIA-REVIEWERS is being updated.  If so, there's probably no need to
+# emit the "does MAINTAINERS/NVIDIA-REVIEWERS need updating?" message on file add/move/delete
 		if ($line =~ /^\s*MAINTAINERS\s*\|/) {
+			$reported_maintainer_file = 1;
+		}
+
+		if ($line =~ /^\s*NVIDIA-REVIEWERS\s*\|/) {
 			$reported_maintainer_file = 1;
 		}
 
@@ -2826,7 +2830,7 @@ sub process {
 			$is_patch = 1;
 			$reported_maintainer_file = 1;
 			WARN("FILE_PATH_CHANGES",
-			     "added, moved or deleted file(s), does MAINTAINERS need updating?\n" . $herecurr);
+			     "added, moved or deleted file(s), does MAINTAINERS and/or NVIDIA-REVIEWERS need updating?\n" . $herecurr);
 		}
 
 # Check for wrappage within a valid hunk of the file
