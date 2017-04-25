b8eb9af1dfa2b941e07e1e128f17eafce8160b71
R Raj Kumar <rrajk@nvidia.com>
2017-04-25 14:08:56 +0530

    pwm: core: use set_capture_window_length
    In pwm_apply_state() use set_capture_window_length callback to
    update capture_win_len.
    
    Note: after moving pwm-tegra-tachometer driver to kernel/nvidia
    tree. The original change d154f9e7ef98dacdda9068416e20023ce74deb90
    has been broken down into two parts. See commit
    77a3d380ce1a1ab8400b290a2f0d6abeaf1acb84 for changes made in
    pwm-tegra-tachometer driver.
    
    Bug 200237693
    Bug 2849379
    
    Change-Id: I00495545efdfcf2b452cf8e857be0e4bbaee9cb9
    Signed-off-by: R Raj Kumar <rrajk@nvidia.com>
    Reviewed-on: http://git-master/r/1481759
    (cherry picked from commit 9825f667a2ebd8dc382d27cbb226150298afdd46)
    Reviewed-on: https://git-master.nvidia.com/r/1769007
    (cherry picked from commit d154f9e7ef98dacdda9068416e20023ce74deb90
    in dev-kernel-4.14)
    Signed-off-by: Kartik <kkartik@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.4/+/2294103
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/pwm/core.c
+++ drivers/pwm/core.c
@@ -527,7 +527,7 @@ int pwm_apply_state(struct pwm_device *pwm, const struct pwm_state *state)
 			if (!pwm->chip->ops->set_capture_window_length)
 				return -ENOTSUPP;
 
-			err = pwm->chip->ops->set_ramp_time(
+			err = pwm->chip->ops->set_capture_window_length(
 					pwm->chip, pwm, state->capture_win_len);
 			if (err)
 				return err;
