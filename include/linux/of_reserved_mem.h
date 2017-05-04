21af12c3dcbe890205e4d8624ab2bcb46ffce914
Sri Krishna chowdary <schowdary@nvidia.com>
2017-05-04 18:04:23 +0530

    of: fix terminating condition in of_reserved_mem_device_init
    Even though memory-region property is properly filled,
    of_reserved_mem_device_init() is returning -ENODEV always
    i.e., the loop exits when a invalid phandle is queried for.
    Fix this by checking the number of phandles available and
    query only valid phandles.
    
    Bug 200317605
    Bug 200420140
    
    Change-Id: Iecf6ce5ae98d809abef11f2bd2a5220432e78093
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/1475416
    (cherry picked from commit 4f8bfcc120bdf987580a9ea9c6dccfa6257ffc6a)
    (cherry picked from commit 8a44c56ed62bc1d2ba883a31544437fcd7726572)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.4/+/2336022
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Sandipan Patra <spatra@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Sandipan Patra <spatra@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- include/linux/of_reserved_mem.h
+++ include/linux/of_reserved_mem.h
@@ -3,6 +3,7 @@
 #define __OF_RESERVED_MEM_H
 
 #include <linux/device.h>
+#include <linux/of.h>
 
 struct of_phandle_args;
 struct reserved_mem_ops;
@@ -68,7 +69,19 @@ static inline struct reserved_mem *of_reserved_mem_lookup(struct device_node *np
  */
 static inline int of_reserved_mem_device_init(struct device *dev)
 {
-	return of_reserved_mem_device_init_by_idx(dev, dev->of_node, 0);
+	int ret = -ENODEV, idx = 0, max = 0;
+
+	if (!dev)
+		return ret;
+
+	while (of_parse_phandle(dev->of_node, "memory-region", max))
+		max++;
+
+	do {
+		ret = of_reserved_mem_device_init_by_idx(dev,
+			dev->of_node, idx++);
+	} while (!ret && (idx != max));
+	return ret;
 }
 
 #endif /* __OF_RESERVED_MEM_H */
