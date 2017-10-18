0f9c54bea2de363b144fa977a7ece40b602ef0ff
Peter De Schrijver <pdeschrijver@nvidia.com>
2017-10-18 16:41:57 +0300

    clk: tegra: prepare/unprepare for super mux clocks
    This ensures DVFS gets the correct initial frequency even if the clock
    will be always on as the first prepare will cause tegra_dvfs_set_rate() to
    be called.
    
    Change-Id: I1b90991f84f1d8cbbc698e750fbc499b5a807410
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1582490
    (cherry picked from commit 52dbcf4868c8c84b5c2c6b24e6378137127d5ae8)
    Reviewed-on: https://git-master.nvidia.com/r/1764843
    (cherry picked from commit b2e54b54377cc2d9014743046a0b7e62ad71e68c)
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2444450
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/clk/tegra/clk-super.c
+++ drivers/clk/tegra/clk-super.c
@@ -136,12 +136,6 @@ static void clk_super_mux_restore_context(struct clk_hw *hw)
 	clk_super_set_parent(hw, parent_id);
 }
 
-static const struct clk_ops tegra_clk_super_mux_ops = {
-	.get_parent = clk_super_get_parent,
-	.set_parent = clk_super_set_parent,
-	.restore_context = clk_super_mux_restore_context,
-};
-
 static int clk_super_prepare(struct clk_hw *hw)
 {
 	return tegra_dvfs_set_rate(hw->clk, clk_hw_get_rate(hw));
@@ -151,6 +145,14 @@ static void clk_super_unprepare(struct clk_hw *hw)
 {
 	tegra_dvfs_set_rate(hw->clk, 0);
 }
+
+const struct clk_ops tegra_clk_super_mux_ops = {
+	.get_parent = clk_super_get_parent,
+	.set_parent = clk_super_set_parent,
+	.restore_context = clk_super_mux_restore_context,
+	.prepare = clk_super_prepare,
+	.unprepare = clk_super_unprepare,
+};
 
 static int clk_super_determine_rate(struct clk_hw *hw,
 				    struct clk_rate_request *req)
