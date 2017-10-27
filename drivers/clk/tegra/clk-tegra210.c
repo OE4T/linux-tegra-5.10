c1aaa5afe91ee096df2d8b580c589e4ed1845e9f
Timo Alho <talho@nvidia.com>
2017-10-27 15:34:44 +0300

    clk: tegra: add con_id's for GPU related clocks
    Bug 200340064
    
    Change-Id: If07ac5d642c612043da4d825d7004dcf8c196a8b
    Signed-off-by: Timo Alho <talho@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1586991
    (cherry picked from commit 085fff0124cf01642052add9341e565096d57971)
    [vinceh@nvidia.com: fixed conflict]
    Signed-off-by: Vince Hsu <vinceh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1763803
    (cherry picked from commit 62a6ec73adeba5483ed772e4a37f0c13e8af7720)
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2444446
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/clk/tegra/clk-tegra210.c
+++ drivers/clk/tegra/clk-tegra210.c
@@ -2855,6 +2855,10 @@ static struct tegra_devclk devclks[] __initdata = {
 	{ .con_id = "sbc4", .dt_id = TEGRA210_CLK_SBC4 },
 	{ .con_id = "abus", .dt_id = TEGRA210_CLK_ABUS },
 	{ .con_id = "gbus", .dt_id = TEGRA210_CLK_GBUS },
+	{ .con_id = "gpu_ref", .dt_id = TEGRA210_CLK_PLL_G_REF },
+	{ .con_id = "pll_p_out5", .dt_id = TEGRA210_CLK_PLL_P_OUT5 },
+	{ .con_id = "gpu", .dt_id = TEGRA210_CLK_GM20B_GBUS },
+	{ .con_id = "gpu_gate", .dt_id = TEGRA210_CLK_GPU },
 };
 
 static struct tegra_audio_clk_info tegra210_audio_plls[] = {
