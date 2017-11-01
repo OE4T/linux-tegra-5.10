aa482acdd851bd7c50368e4c771970e6c4401c06
Peter De Schrijver <pdeschrijver@nvidia.com>
2017-11-01 13:34:13 +0200

    clk: tegra: init DFLL out_rate_max in PWM mode
    This field was accidently left to 0 when using the DFLL in PWM mode. This
    causes all clk_set_rate requests to be denied as they exceed the max rate
    of 0.
    
    Bug 200357442
    
    Change-Id: Ie1f417b7fc67ba2497c52ec5252d64f8bea4ed61
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1589973
    (cherry picked from commit fc78fc863b22c5a4e81444d990cdb5df634396a0)
    Reviewed-on: https://git-master.nvidia.com/r/1749355
    (cherry picked from commit d278d7efd0caea0496e52243ffc521d82392d134)
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2441555
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/clk/tegra/clk-dfll.c
+++ drivers/clk/tegra/clk-dfll.c
@@ -2809,6 +2809,8 @@ static int dfll_build_pwm_lut(struct tegra_dfll *td, unsigned long v_max)
 		return -EINVAL;
 	}
 	td->dvco_rate_min = td->out_rate_min = rate;
+	rate = get_dvco_rate_below(td, td->lut_size - 1);
+	td->out_rate_max = rate;
 
 	return 0;
 }
