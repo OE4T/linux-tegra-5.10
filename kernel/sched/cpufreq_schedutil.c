a4c525c23edf1e2ec2bb3a638f672301ef56a388
Waqar Ali <waqara@nvidia.com>
2018-08-07 11:05:53 -0700

    sched: cpufreq: sysfs knob for capacity_margin
    This commit makes the capacity_margin knob configurable from user-space.
    By tinkering with this knob, cpufreq scaling of frequency can be made
    more aggressive. Allowed values for this knob range from 0 to 1024. By
    default, it has the value of 256 which sets the frequency scaling factor
    to be 1.25
    
    Bug 200674300
    
    Change-Id: I76138a3668f28548e6d08ae5321c1b1d9ce6f39d
    Signed-off-by: Waqar Ali <waqara@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1794474
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.10/+/2578481
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Sumit Gupta <sumitg@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -14,11 +14,20 @@
 #include <trace/events/power.h>
 
 #define IOWAIT_BOOST_MIN	(SCHED_CAPACITY_SCALE / 8)
+#define CAPACITY_MARGIN_MAX_VALUE		(1024)
+#define CAPACITY_MARGIN_FLOOR_FACTOR		(10)
 
 struct sugov_tunables {
 	struct gov_attr_set	attr_set;
 	unsigned int		rate_limit_us;
 	unsigned long		iowait_boost_max;
+
+	/*
+	 * This knob can be used to control the aggressiveness of schedutil
+	 * frequency scaling. Increasing the value will make scaling more
+	 * aggressive.
+	 */
+	unsigned int		capacity_margin;
 };
 
 struct sugov_policy {
@@ -151,7 +160,9 @@ static void sugov_deferred_update(struct sugov_policy *sg_policy, u64 time,
  *
  * next_freq = C * curr_freq * util_raw / max
  *
- * Take C = 1.25 for the frequency tipping point at (util / max) = 0.8.
+ * By default, take C = 1.25 for the frequency tipping point at (util / max) =
+ * 0.8. The value of C can be changed indirectly via sysfs knob
+ * 'capacity_margin'
  *
  * The lowest driver-supported frequency which is equal or greater than the raw
  * next_freq (as calculated above) is returned, subject to policy min/max and
@@ -161,10 +172,24 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 				  unsigned long util, unsigned long max)
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
+	unsigned int capacity_margin = sg_policy->tunables->capacity_margin;
 	unsigned int freq = arch_scale_freq_invariant() ?
 				policy->cpuinfo.max_freq : policy->cur;
 
-	freq = map_util_freq(util, freq, max);
+	/*
+	 * Cast freq to 64-bit so that we do not have to check for overflow in
+	 * the calculations later on
+	 */
+	uint64_t tfreq = (uint64_t) freq;
+
+	tfreq = (((tfreq * capacity_margin) >> CAPACITY_MARGIN_FLOOR_FACTOR) +
+			tfreq) * util / max;
+
+	/*
+	 * tfreq will always be less than UINT_MAX as long as freq is less than
+	 * UINT_MAX / 2
+	 */
+	freq = (unsigned int) tfreq;
 
 	if (freq == sg_policy->cached_raw_freq && !sg_policy->need_freq_update)
 		return sg_policy->next_freq;
@@ -631,9 +656,38 @@ static ssize_t iowait_boost_max_store(struct gov_attr_set *attr_set,
 
 static struct governor_attr iowait_boost_max = __ATTR_RW(iowait_boost_max);
 
+static ssize_t capacity_margin_show(struct gov_attr_set *attr_set, char *buf)
+{
+	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
+
+	return sprintf(buf, "%u\n", tunables->capacity_margin);
+}
+
+static ssize_t capacity_margin_store(struct gov_attr_set *attr_set,
+		const char *buf, size_t count)
+{
+	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
+	unsigned int capacity_margin;
+
+	if (kstrtouint(buf, 10, &capacity_margin))
+		return -EINVAL;
+
+	if (capacity_margin > CAPACITY_MARGIN_MAX_VALUE) {
+		pr_info("capacity_margin value exceeds maximum");
+		return -EINVAL;
+	}
+
+	tunables->capacity_margin = capacity_margin;
+
+	return count;
+}
+
+static struct governor_attr capacity_margin = __ATTR_RW(capacity_margin);
+
 static struct attribute *sugov_attrs[] = {
 	&rate_limit_us.attr,
 	&iowait_boost_max.attr,
+	&capacity_margin.attr,
 	NULL
 };
 
@@ -796,6 +850,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
+
+	tunables->capacity_margin = 256;
 
 	ret = kobject_init_and_add(&tunables->attr_set.kobj, &sugov_tunables_ktype,
 				   get_governor_parent_kobj(policy), "%s",
