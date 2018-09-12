3f0c4dce47ede75c20f54a8fab4c0be4143a1786
Bhanu Murthy V <bmurthyv@nvidia.com>
2018-09-12 11:10:54 -0700

    include: media: Add camera device driver header
    This header defines the I2C, SPI bus, multiplexer
    configurations and the overall sensor configuration
    for the camera device.
    This header exposes the structures which can be
    used in userspace for the interface communication
    with camera device driver.
    
    Bug 2433156
    
    Change-Id: I05ae2ec4144c01bef664d329082d640b7d6ac1a1
    Signed-off-by: Bhanu Murthy V <bmurthyv@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1822198
    Reviewed-by: Vincent Chung <vincentc@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Frank Chen <frankc@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- include/media/sensor_common.h
+++ include/media/sensor_common.h
@@ -36,8 +36,10 @@
 #include <media/v4l2-ctrls.h>
 #include <linux/v4l2-mediabus.h>
 #include <media/tegra-v4l2-camera.h>
+#include <media/camera_device.h>
 
 struct sensor_properties {
+	struct sensor_cfg cfg;
 	/* sensor_modes points to an array of mode properties */
 	struct sensor_mode_properties *sensor_modes;
 	u32 num_modes;
