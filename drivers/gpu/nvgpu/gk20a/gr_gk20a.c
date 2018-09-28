bccb1690c56471da56910d1bcd051c2441d9bafe
Terje Bergstrom <tbergstrom@nvidia.com>
2018-09-28 12:18:18 -0700

    gpu: nvgpu: Remove GR dependency to MC header
    Do not include hw_mc_gk20a.h header in gr_gk20a.c anymore. The code
    that needed it got deleted.
    
    JIRA NVGPU-954
    
    Change-Id: I62f1523255e176c0b73988cb72701025263389b9
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1851327
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/gr_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/gr_gk20a.c
@@ -57,7 +57,6 @@
 #include <nvgpu/hw/gk20a/hw_fifo_gk20a.h>
 #include <nvgpu/hw/gk20a/hw_gr_gk20a.h>
 #include <nvgpu/hw/gk20a/hw_gmmu_gk20a.h>
-#include <nvgpu/hw/gk20a/hw_mc_gk20a.h>
 #include <nvgpu/hw/gk20a/hw_ram_gk20a.h>
 #include <nvgpu/hw/gk20a/hw_pri_ringmaster_gk20a.h>
 #include <nvgpu/hw/gk20a/hw_top_gk20a.h>
