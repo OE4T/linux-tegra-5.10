eb887094e4f4446e6eed9c17e88f4a36ac5ae972
Preetham Chandru Ramchandra <pchandru@nvidia.com>
2019-01-17 22:37:39 +0530

    gpu: nvgpu: nvgpu locks to vanilla Linux locks
    Replace nvgpu locks to vanilla Linux locks. For the custom kernel
    driver when they include nv-p2p.h, nvgpu/linux/lock.h will not be
    available because nvgpu/linux/lock.h is not copied to
    /usr/src/kernel_header_file.
    
    Bug 200438879
    
    Change-Id: I55b52c6f791970650388b7d51c4d30b5fe75bbb8
    Signed-off-by: Preetham Chandru Ramchandra <pchandru@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1997950
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Alex Waterman <alexw@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- include/linux/nv-p2p.h
+++ include/linux/nv-p2p.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2018-2019, NVIDIA CORPORATION.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -64,7 +64,7 @@ struct nvidia_p2p_page_table {
 
 	struct mm_struct *mm;
 	struct mmu_notifier mn;
-	struct nvgpu_mutex lock;
+	struct mutex lock;
 	void (*free_callback)(void *data);
 	void *data;
 };
