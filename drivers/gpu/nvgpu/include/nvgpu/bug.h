763f79b2be3782b4f4f572e03e292efd969ae226
Shashank Singh <shashsingh@nvidia.com>
2019-01-23 23:24:36 +0530

    gpu: nvgpu: use posix bug implementation for qnx
    Jira NVGPU-1675
    
    Change-Id: I65662921803c9049318c28645f050e8fa6590b1d
    Signed-off-by: Shashank Singh <shashsingh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2002084
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/include/nvgpu/bug.h
+++ drivers/gpu/nvgpu/include/nvgpu/bug.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2017-2019, NVIDIA CORPORATION.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -24,10 +24,8 @@
 
 #ifdef __KERNEL__
 #include <linux/bug.h>
-#elif defined(__NVGPU_POSIX__)
-#include <nvgpu/posix/bug.h>
 #else
-#include <nvgpu_rmos/include/bug.h>
+#include <nvgpu/posix/bug.h>
 #endif
 
 /*
