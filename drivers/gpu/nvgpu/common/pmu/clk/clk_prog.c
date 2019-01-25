f13b5d90e3129bb864836ecc197b73863c783d7d
Vaibhav Kachore <vkachore@nvidia.com>
2019-01-25 19:15:12 +0530

    Revert "Revert "gpu: nvgpu: Add quantization to slave VF Points""
    This reverts commit 8f3bf00b5adb52350bfaff55fb8007bfd6d678e2.
    
    Bug 2487534
    
    Change-Id: I6d2ce7229adf010080b4a04386c449f2433fedae
    Signed-off-by: Vaibhav Kachore <vkachore@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2003915
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-misra <svc-mobile-misra@nvidia.com>
    Reviewed-by: svc-misra-checker <svc-misra-checker@nvidia.com>
    Reviewed-by: Konsta Holtta <kholtta@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Ranjanikar Nikhil Prabhakarrao <rprabhakarra@nvidia.com>
    Reviewed-by: Ankur Kishore <ankkishore@nvidia.com>

--- drivers/gpu/nvgpu/common/pmu/clk/clk_prog.c
+++ drivers/gpu/nvgpu/common/pmu/clk/clk_prog.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2019, NVIDIA CORPORATION.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -1765,6 +1765,8 @@ static int getslaveclk_prog_1x_master(struct gk20a *g,
 				return -EINVAL;
 			}
 			*pclkmhz = (masterclkmhz * pslaveents->ratio)/100U;
+			/* Floor/Quantize all the slave clocks to the multiple of step size*/
+			*pclkmhz = (*pclkmhz / FREQ_STEP_SIZE_MHZ) * FREQ_STEP_SIZE_MHZ;
 		} else {
 			/* only support ratio for now */
 			return -EINVAL;
