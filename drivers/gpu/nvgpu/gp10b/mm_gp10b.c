5b25686d54ad5652930b7883e3588ae8e35ec15f
Alex Waterman <alexw@nvidia.com>
2019-02-07 08:26:38 -0800

    Revert "gpu: nvgpu: Discard coherency check on gmmu"
    This reverts commit 4e1769097526e5203f7c18a663ab3c29f5568ae5.
    
    Causes instability on Xavier.
    
    Change-Id: I42084a39d496790aad7af1cd85aa0c2c8eac70aa
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2014014
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-misra <svc-mobile-misra@nvidia.com>
    Reviewed-by: svc-misra-checker <svc-misra-checker@nvidia.com>
    Reviewed-by: Mahantesh Kumbar <mkumbar@nvidia.com>
    Tested-by: Mahantesh Kumbar <mkumbar@nvidia.com>
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-by: Vinod Gopalakrishnakurup <vinodg@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/gp10b/mm_gp10b.c
+++ drivers/gpu/nvgpu/gp10b/mm_gp10b.c
@@ -254,7 +254,7 @@ static void update_gmmu_pte_locked(struct vm_gk20a *vm,
 		"vm=%s "
 		"PTE: i=%-4u size=%-2u | "
 		"GPU %#-12llx  phys %#-12llx "
-		"pgsz: %3dkb perm=%-2s kind=%#02x APT=%-6s %c%c%c%c "
+		"pgsz: %3dkb perm=%-2s kind=%#02x APT=%-6s %c%c%c%c%c "
 		"ctag=0x%08x "
 		"[0x%08x, 0x%08x]",
 		vm->name,
@@ -267,6 +267,7 @@ static void update_gmmu_pte_locked(struct vm_gk20a *vm,
 		attrs->cacheable ? 'C' : '-',
 		attrs->sparse    ? 'S' : '-',
 		attrs->priv      ? 'P' : '-',
+		attrs->coherent  ? 'I' : '-',
 		attrs->valid     ? 'V' : '-',
 		(u32)attrs->ctag / g->ops.fb.compression_page_size(g),
 		pte_w[1], pte_w[0]);
