a259274e29bd95e31a59d0b1e424c6d761679d2e
Mallikarjun Kasoju <mkasoju@nvidia.com>
2019-02-15 11:11:53 +0530

    tegra-cryptodev: Allow null key only for SE driver
    Null key is allowed in SE driver for using SSK.
    For other drivers, if the key is null, return error.
    
    Bug 200490640
    
    Change-Id: Ibebddd81f90564e7f0f044d1ca2af400ace07e21
    Signed-off-by: Mallikarjun Kasoju <mkasoju@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2019922
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/crypto/tegra-cryptodev.c
+++ drivers/crypto/tegra-cryptodev.c
@@ -231,6 +231,7 @@ static int process_crypt_req(struct file *filp, struct tegra_crypto_ctx *ctx,
 	struct tegra_crypto_completion tcrypt_complete;
 	char aes_algo[5][10] = {"ecb(aes)", "cbc(aes)", "ofb(aes)", "ctr(aes)",
 				"xts(aes)"};
+	const char *algo;
 
 	if (crypt_req->op != TEGRA_CRYPTO_CBC) {
 		if (crypt_req->op >= TEGRA_CRYPTO_MAX)
@@ -282,6 +283,19 @@ static int process_crypt_req(struct file *filp, struct tegra_crypto_ctx *ctx,
 		key = crypt_req->key;
 
 	if (!crypt_req->skip_key) {
+		algo = crypto_tfm_alg_driver_name(crypto_skcipher_tfm(tfm));
+		if (!algo) {
+			pr_err("Invalid algo driver name");
+			ret = -EINVAL;
+			goto process_req_out;
+		}
+
+		/* Null key is only allowed in SE driver */
+		if (!strstr(algo, "tegra")) {
+			ret = -EINVAL;
+			goto process_req_out;
+		}
+
 		ret = crypto_skcipher_setkey(tfm, key, crypt_req->keylen);
 		if (ret < 0) {
 			pr_err("setkey failed");
