9d7fdecdca2ee1c060f224148a34987cfb28a12f
Frank Chen <frankc@nvidia.com>
2019-02-17 20:51:21 -0800

    imx274: Don't report failure when updating control
    The exposure_short control can be called when driver
    is trying to update all control ranges, even if current
    sensor mode is not HDR mode.
    
    We should just ignore this control update and don't report
    it as failure.
    
    Bug 200492596
    
    Change-Id: I9d0c15f7b447cbe91485339a5531e1aea928ede3
    Signed-off-by: Frank Chen <frankc@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2021705
    (cherry picked from commit fa0ccf1426972d977fdac315aaef6dc30747d703)
    Reviewed-on: https://git-master.nvidia.com/r/2029081
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-misra <svc-mobile-misra@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Vincent Chung <vincentc@nvidia.com>
    Reviewed-by: Ian Kaszubski <ikaszubski@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/media/i2c/imx274.c
+++ drivers/media/i2c/imx274.c
@@ -1,7 +1,7 @@
 /*
  * imx274.c - imx274 sensor driver
  *
- * Copyright (c) 2015-2018, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2019, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -336,7 +336,7 @@ int imx274_set_exposure_shr_dol_short(struct tegracam_device *tc_dev, s64 val)
 	if (hdr_en != SWITCH_ON)  {
 		dev_dbg(dev, "%s: SHR DOL1 is ignored for non-HDR mode\n",
 			__func__);
-		goto fail;
+		return 0;
 	}
 
 	if (s_data->mode == IMX274_MODE_3840X2160_DOL_30FPS) {
