19a9a4c0acb927e5d8a9121d82b04b713f3a47cf
Prafull Suryawanshi <prafulls@nvidia.com>
2019-02-27 21:45:41 -0800

    clk: tegra: avoid re-enabling pll
    The commit '4420719 clk: tegra: add sdm_reset mask for PLLD2/DP'
    introduced SDM_RESET for frequency locking. The toggling of
    SDM_RESET results in Fvco changes. This will cause glitches if this
    enable function call is applied to an enabled pll.
    Hence bail out immediately if pll has been in enabled state
    
    bug 200483582
    
    Change-Id: I7e7f6dc76968c070e4b99504d73d2680a7f6ad23
    Signed-off-by: Prafull Suryawanshi <prafulls@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2084527
    (cherry picked from commit 6f59c4fa8ba6780ac4874deaaad8ec996ee110b4)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2406919
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Naveen Kumar S <nkumars@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/clk/tegra/clk-pll.c
+++ drivers/clk/tegra/clk-pll.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2012, 2013, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2020, NVIDIA CORPORATION.  All rights reserved.
  */
 
 #include <linux/slab.h>
@@ -347,6 +347,9 @@ static void _clk_pll_enable(struct clk_hw *hw)
 {
 	struct tegra_clk_pll *pll = to_clk_pll(hw);
 	u32 val;
+
+	if (clk_pll_is_enabled(hw))
+		return;
 
 	if (pll->params->iddq_reg) {
 		val = pll_readl(pll->params->iddq_reg, pll);
