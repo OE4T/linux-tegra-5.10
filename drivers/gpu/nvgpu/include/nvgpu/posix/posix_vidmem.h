e34ff7574df7e269ca5c0d6c536e1d736269d885
Vedashree Vidwans <vvidwans@nvidia.com>
2019-03-04 08:52:11 -0800

    gpu: nvgpu: posix: added dma_alloc_vid functions
    Implement allocate and free functions for mocking vidmem in posix.
    
    JIRA 2235
    
    Change-Id: I4d5889b9b52e8cd7396dad5f880ec55dbc31c30e
    Signed-off-by: Vedashree Vidwans <vvidwans@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2032021
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/include/nvgpu/posix/posix_vidmem.h
+++ drivers/gpu/nvgpu/include/nvgpu/posix/posix_vidmem.h
@@ -20,41 +20,26 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-#include <nvgpu/bug.h>
-#include <nvgpu/vidmem.h>
-#include <nvgpu/nvgpu_mem.h>
-#include <nvgpu/nvgpu_sgt.h>
-#include <nvgpu/page_allocator.h>
-#include <nvgpu/posix/posix_vidmem.h>
+#ifndef NVGPU_POSIX_VIDMEM_H
+#define NVGPU_POSIX_VIDMEM_H
 
-bool nvgpu_addr_is_vidmem_page_alloc(u64 addr)
+#ifdef CONFIG_GK20A_VIDMEM
+
+void nvgpu_vidmem_set_page_alloc(struct nvgpu_mem_sgl *sgl, u64 addr);
+struct nvgpu_page_alloc *nvgpu_vidmem_get_page_alloc(struct nvgpu_mem_sgl *sgl);
+
+#else
+static inline void nvgpu_vidmem_set_page_alloc(struct nvgpu_mem_sgl *sgl,
+					       u64 addr)
 {
-	return ((addr & 1ULL) != 0ULL);
 }
 
-void nvgpu_vidmem_set_page_alloc(struct nvgpu_mem_sgl *sgl, u64 addr)
+static inline struct nvgpu_page_alloc *nvgpu_vidmem_get_page_alloc(
+	struct nvgpu_mem_sgl *sgl)
 {
-	/* set bit 0 to indicate vidmem allocation */
-	sgl->dma = (addr | 1ULL);
-	sgl->phys = (addr | 1ULL);
+	return NULL;
 }
 
-struct nvgpu_page_alloc *nvgpu_vidmem_get_page_alloc(struct nvgpu_mem_sgl *sgl)
-{
-	u64 addr;
+#endif
 
-	addr = sgl->dma;
-
-	if (nvgpu_addr_is_vidmem_page_alloc(addr))
-		addr = addr & ~1ULL;
-	else
-		WARN_ON(true);
-
-	return (struct nvgpu_page_alloc *)(uintptr_t)addr;
-}
-
-
-void __nvgpu_mem_free_vidmem_alloc(struct gk20a *g, struct nvgpu_mem *vidmem)
-{
-	BUG();
-}
+#endif /* NVGPU_POSIX_VIDMEM_H */
--- drivers/gpu/nvgpu/os/posix/posix-dma.c
+++ drivers/gpu/nvgpu/os/posix/posix-dma.c
@@ -27,9 +27,14 @@
 #include <nvgpu/bug.h>
 #include <nvgpu/dma.h>
 #include <nvgpu/gmmu.h>
+#include <nvgpu/kmem.h>
 #include <nvgpu/nvgpu_mem.h>
+#include <nvgpu/nvgpu_sgt.h>
+#include <nvgpu/page_allocator.h>
 #include <nvgpu/enabled.h>
+#include <nvgpu/vidmem.h>
 #include <nvgpu/posix/posix-fault-injection.h>
+#include <nvgpu/posix/posix_vidmem.h>
 #include "os_posix.h"
 
 _Thread_local struct nvgpu_posix_fault_inj dma_fi = {
@@ -40,6 +45,19 @@ _Thread_local struct nvgpu_posix_fault_inj dma_fi = {
 struct nvgpu_posix_fault_inj *nvgpu_dma_alloc_get_fault_injection(void)
 {
 	return &dma_fi;
+}
+
+static u64 __nvgpu_dma_alloc(struct nvgpu_allocator *allocator, u64 at,
+				size_t size)
+{
+	u64 addr = 0;
+
+	if (at)
+		addr = nvgpu_alloc_fixed(allocator, at, size, 0);
+	else
+		addr = nvgpu_alloc(allocator, size);
+
+	return addr;
 }
 
 /*
@@ -90,12 +108,108 @@ int nvgpu_dma_alloc_flags_sys(struct gk20a *g, unsigned long flags,
 	return __nvgpu_do_dma_alloc(g, flags, size, mem, APERTURE_SYSMEM);
 }
 
+static size_t mock_fb_get_vidmem_size(struct gk20a *g)
+{
+	return SZ_4G;
+}
+
+/* In userspace, vidmem requires only a few fields populated */
 int nvgpu_dma_alloc_flags_vid_at(struct gk20a *g, unsigned long flags,
 				 size_t size, struct nvgpu_mem *mem, u64 at)
 {
-	BUG();
+	u64 addr;
+	int err;
+
+	if (nvgpu_posix_fault_injection_handle_call(&dma_fi)) {
+		return -ENOMEM;
+	}
+
+	g->ops.fb.get_vidmem_size = mock_fb_get_vidmem_size;
+
+	nvgpu_set_enabled(g, NVGPU_MM_UNIFIED_MEMORY, false);
+
+	/* Initialize nvgpu_allocators */
+	err = nvgpu_vidmem_init(&g->mm);
+	if (err != 0) {
+		nvgpu_err(g, "vidmem init failed with err=%d", err);
+		return err;
+	}
+
+
+	struct nvgpu_allocator *vidmem_alloc = g->mm.vidmem.cleared ?
+		&g->mm.vidmem.allocator :
+		&g->mm.vidmem.bootstrap_allocator;
+
+	if (nvgpu_mem_is_valid(mem)) {
+		nvgpu_warn(g, "memory leak !!");
+		WARN_ON(1);
+	}
+
+	mem->size = size;
+	size = PAGE_ALIGN(size);
+
+	if (!nvgpu_alloc_initialized(&g->mm.vidmem.allocator)) {
+		err = -ENOSYS;
+		nvgpu_err(g, "nvgpu alloc not initialized\n");
+		goto dma_err;
+	}
+
+	/*
+	 * Our own allocator doesn't have any flags yet, and we can't
+	 * kernel-map these, so require explicit flags.
+	 */
+	WARN_ON(flags != NVGPU_DMA_NO_KERNEL_MAPPING);
+
+	addr = __nvgpu_dma_alloc(vidmem_alloc, at, size);
+	if (!addr) {
+		/*
+		 * If memory is known to be freed soon, let the user know that
+		 * it may be available after a while.
+		 */
+		err = -ENOMEM;
+		nvgpu_err(g, " not initialized\n");
+		goto dma_err;
+	}
+
+	if (at)
+		mem->mem_flags |= NVGPU_MEM_FLAG_FIXED;
+
+	/* POSIX doesn't have sg_table struct. Allocate memory for nvgpu_sgt */
+	mem->priv.sgt = nvgpu_kzalloc(g, sizeof(struct nvgpu_sgt));
+	if (!mem->priv.sgt) {
+		err = -ENOMEM;
+		goto fail_sgtfree;
+	}
+
+	/* Allocate memory for sgl */
+	mem->priv.sgt->sgl = (struct nvgpu_sgl *)
+		nvgpu_kzalloc(g, sizeof(struct nvgpu_mem_sgl));
+	if (mem->priv.sgt->sgl == NULL) {
+		nvgpu_err(g, "sgl allocation failed\n");
+		err = -ENOMEM;
+		goto fail_sglfree;
+	}
+
+	nvgpu_vidmem_set_page_alloc(
+		(struct nvgpu_mem_sgl *)mem->priv.sgt->sgl, addr);
+
+	mem->aligned_size = size;
+	mem->aperture = APERTURE_VIDMEM;
+	mem->vidmem_alloc = (struct nvgpu_page_alloc *)(uintptr_t)addr;
+	mem->allocator = vidmem_alloc;
 
 	return 0;
+
+fail_sglfree:
+	nvgpu_kfree(g, mem->priv.sgt);
+fail_sgtfree:
+	nvgpu_free(&g->mm.vidmem.allocator, addr);
+	mem->size = 0;
+dma_err:
+	nvgpu_vidmem_destroy(g);
+	nvgpu_cond_destroy(&g->mm.vidmem.clearing_thread_cond);
+	nvgpu_thread_stop_graceful(&g->mm.vidmem.clearing_thread, NULL, NULL);
+	return err;
 }
 
 void nvgpu_dma_free_sys(struct gk20a *g, struct nvgpu_mem *mem)
@@ -109,5 +223,18 @@ void nvgpu_dma_free_sys(struct gk20a *g, struct nvgpu_mem *mem)
 
 void nvgpu_dma_free_vid(struct gk20a *g, struct nvgpu_mem *mem)
 {
-	BUG();
+
+	nvgpu_memset(g, mem, 0, 0, mem->aligned_size);
+
+	nvgpu_free(mem->allocator, (u64)nvgpu_vidmem_get_page_alloc(
+			(struct nvgpu_mem_sgl *)mem->priv.sgt->sgl));
+	nvgpu_kfree(g, mem->priv.sgt);
+
+	mem->size = 0;
+	mem->aligned_size = 0;
+	mem->aperture = APERTURE_INVALID;
+
+	nvgpu_vidmem_destroy(g);
+	nvgpu_cond_destroy(&g->mm.vidmem.clearing_thread_cond);
+	nvgpu_thread_stop_graceful(&g->mm.vidmem.clearing_thread, NULL, NULL);
 }
