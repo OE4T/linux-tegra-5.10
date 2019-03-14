05c14274b4070aaa0fd02f07166554d07ede85ec
Ziqi Qing <zqing@nvidia.com>
2019-03-14 16:46:21 -0700

    nvcapture: capture buffer management
    Buffer management in KMD allow client to pin and setup a set of surface
    before any capture request. Also keep the compatibility to pin the
    surface when it is not registered before.
    
    - Add ioctl to handle buffer pin request.
    - Add capture buffer table.
    
    Bug 2462034
    
    Change-Id: I71021a241aab4e61d27bd504fb5657b169e56643
    Signed-off-by: Ziqi Qing <zqing@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1972655
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sudhir Vyas <svyas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/media/platform/tegra/camera/vi/capture.c
+++ drivers/media/platform/tegra/camera/vi/capture.c
@@ -201,6 +201,7 @@ void vi_capture_shutdown(struct tegra_vi_channel *chan)
 			vi_capture_request_unpin(chan, i);
 
 		capture_common_unpin_memory(&capture->requests);
+		destroy_buffer_table(capture->buf_ctx);
 		kfree(capture->unpins_list);
 	}
 	kfree(capture);
