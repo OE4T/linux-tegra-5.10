22589e63db823dabf5ec89ac052c058ea379ace0
Nicolin Chen <nicolinc@nvidia.com>
2019-04-15 13:11:23 -0700

    platform: nvadsp: Remove iova lucky check
    The logic of the nvadsp_dma_alloc_and_map_at() function is to
    firstly reserve the target iova and secondly allocate a memory
    with another dynamical iova.
    
    Since the target iova is reserved, the dynamical iova won't be
    possible to be identical. And this lucky check lacks of an exit
    routine either.
    
    So this patch just drops it.
    
    Bug 200444660
    
    Change-Id: I5c45e72b1e7315dacd8708fc8facf6bda41c51e5
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2098219
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/platform/tegra/nvadsp/os.c
+++ drivers/platform/tegra/nvadsp/os.c
@@ -645,10 +645,6 @@ static void *nvadsp_dma_alloc_and_map_at(struct platform_device *pdev,
 	if (!cpu_va)
 		goto fail_dma_alloc;
 
-	/* Luckily hitting the target iova, no need to remap */
-	if (tmp_iova == iova)
-		return cpu_va;
-
 	/* Use tmp_iova to remap non-contiguous pages to the desired iova */
 	for (offset = 0; offset < size; offset += mp_size) {
 		dma_addr_t cur_iova = tmp_iova + offset;
