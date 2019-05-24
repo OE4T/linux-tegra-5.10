872e4f99a40ea80d7cf5ac3cba367154a74e4644
Deepak Nibade <dnibade@nvidia.com>
2019-05-24 16:01:31 +0530

    gpu: nvgpu: remove GV100 hal support
    GV100 is deprecated and hence remove hal support files for it
    
    Bug 200496768
    
    Change-Id: If898974f98a2905dd94fb5577e82f06b602f3990
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2124913
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/hal/init/hal_init.c
+++ drivers/gpu/nvgpu/hal/init/hal_init.c
@@ -32,7 +32,6 @@
 #include "hal_gp10b.h"
 #include "hal_gv11b.h"
 #ifdef NVGPU_DGPU_SUPPORT
-#include "hal_gv100.h"
 #include "hal_tu104.h"
 #endif
 
@@ -65,11 +64,6 @@ int nvgpu_init_hal(struct gk20a *g)
 		}
 		break;
 #ifdef NVGPU_DGPU_SUPPORT
-	case NVGPU_GPUID_GV100:
-		if (gv100_init_hal(g) != 0) {
-			return -ENODEV;
-		}
-		break;
 #if defined(CONFIG_NVGPU_SUPPORT_TURING)
 	case NVGPU_GPUID_TU104:
 		if (tu104_init_hal(g) != 0) {
