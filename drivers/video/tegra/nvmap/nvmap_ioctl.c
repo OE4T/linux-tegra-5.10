CTCaer/switch-l4t-kernel-nvidia

video: tegra: nvmap: Use copy_from_user to cache data
A user buffer may be paged out while memcpy() does not have a
capability to handle page faults like copy_{from,to}_user().

This page fault may cause a kernel panic due to data abort:
    Unable to handle kernel paging request at virtual address 2fad123000
    Mem abort info:
      ESR = 0x9600000b
      Exception class = DABT (current EL), IL = 32 bits
      SET = 0, FnV = 0
      EA = 0, S1PTW = 0
    Data abort info:
      ISV = 0, ISS = 0x0000000b
      CM = 0, WnR = 0
    [0000002fad123000] *pgd=000000012655f003, *pud=000000012655f003,
                       *pmd=000000010abb1003, *pte=0068000110424bd3
    Internal error: Oops: 9600000b [#1] PREEMPT SMP
    PC is at __memcpy_toio+0x6c/0xc0
    LR is at nvmap_ioctl_rw_handle+0x518/0x9c8
    x21: 0000002fad123000
    x1 : 0000002fad123000
    Kernel panic - not syncing: Fatal exception

Note that the kernel crash log doesn't really show that a page
out according to the PTE value, as the PTE value was dumped at
CPU data abort routine which might not accurately reflect the
status where this page out happened. Yet, it's well-known that
memcpy() doesn't have the protection for page-out. Therefore,
a solution here is to use copy_from_user() instead.

The reason of using memcpy_toio() here was to handle a target
address in the device space. So this patch keeps the memcpy()
call but adds an extra copy_from_user() to cache the data.

Note that adding a copy_from_user() caching may increase some
memcpy overhead, but it's a necessary compromise to choose it
over the performance due to the statbility concern.

Bug 200513687

Change-Id: I9edba9f74da50c3404c5e2bef183416fa84545e5
Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/2156583
Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
Reviewed-by: Manish Tuteja <mtuteja@nvidia.com>

drivers/video/tegra/nvmap/nvmap_ioctl.c
@@ -3,7 +3,7 @@
  *
  * User-space interface to nvmap
  *
- * Copyright (c) 2011-2018, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2011-2019, NVIDIA CORPORATION. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -473,6 +473,9 @@ static ssize_t rw_handle(struct nvmap_client *client, struct nvmap_handle *h,
 			 unsigned long count)
 {
 	ssize_t copied = 0;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+	void *tmp = NULL;
+#endif
 	void *addr;
 	int ret = 0;
 
@@ -508,6 +511,15 @@ static ssize_t rw_handle(struct nvmap_client *client, struct nvmap_handle *h,
 
 	addr = h->vaddr + h_offs;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+	/* Allocate buffer to cache data for VPR write */
+	if (!is_read && h->heap_type == NVMAP_HEAP_CARVEOUT_VPR) {
+		tmp = vmalloc(elem_size);
+		if (!tmp)
+			return -ENOMEM;
+	}
+#endif
+
 	while (count--) {
 		if (h_offs + elem_size > h->size) {
 			pr_warn("read/write outside of handle\n");
@@ -524,10 +536,10 @@ static ssize_t rw_handle(struct nvmap_client *client, struct nvmap_handle *h,
 		else {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
 			if (h->heap_type == NVMAP_HEAP_CARVEOUT_VPR) {
-				uaccess_enable();
-				memcpy_toio(addr, (void *)sys_addr, elem_size);
-				uaccess_disable();
-				ret = 0;
+				ret = copy_from_user(tmp, (void *)sys_addr,
+						     elem_size);
+				if (!ret)
+					memcpy_toio(addr, tmp, elem_size);
 			} else
 #endif
 				ret = copy_from_user(addr, (void *)sys_addr, elem_size);
@@ -547,6 +559,12 @@ static ssize_t rw_handle(struct nvmap_client *client, struct nvmap_handle *h,
 		h_offs += h_stride;
 		addr += h_stride;
 	}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+	/* Release the buffer used for VPR write */
+	if (!is_read && h->heap_type == NVMAP_HEAP_CARVEOUT_VPR && tmp)
+		vfree(tmp);
+#endif
 
 	return ret ?: copied;
 }

OE4T/linux-tegra-5.10

Reviewed-on: https://git-master.nvidia.com/r/2155968
(cherry picked from commit d7d26f3fd642e9f6103a0cc58b7356b8091a77b3)
Reviewed-on: https://git-master.nvidia.com/r/2166415
GVS: Gerrit_Virtual_Submit
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/video/tegra/nvmap/nvmap_ioctl.c
@@ -430,6 +430,9 @@ static ssize_t rw_handle(struct nvmap_client *client, struct nvmap_handle *h,
 			 unsigned long count)
 {
 	ssize_t copied = 0;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+	void *tmp = NULL;
+#endif
 	void *addr;
 	int ret = 0;
 
@@ -465,6 +468,15 @@ static ssize_t rw_handle(struct nvmap_client *client, struct nvmap_handle *h,
 
 	addr = h->vaddr + h_offs;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+	/* Allocate buffer to cache data for VPR write */
+	if (!is_read && h->heap_type == NVMAP_HEAP_CARVEOUT_VPR) {
+		tmp = vmalloc(elem_size);
+		if (!tmp)
+			return -ENOMEM;
+	}
+#endif
+
 	while (count--) {
 		if (h_offs + elem_size > h->size) {
 			pr_warn("read/write outside of handle\n");
@@ -481,11 +493,10 @@ static ssize_t rw_handle(struct nvmap_client *client, struct nvmap_handle *h,
 		else {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
 			if (h->heap_type == NVMAP_HEAP_CARVEOUT_VPR) {
-				uaccess_enable();
-				kasan_memcpy_toio(addr, (void *)sys_addr,
-				elem_size);
-				uaccess_disable();
-				ret = 0;
+				ret = copy_from_user(tmp, (void *)sys_addr,
+						     elem_size);
+				if (!ret)
+					kasan_memcpy_toio(addr, tmp, elem_size);
 			} else
 #endif
 				ret = copy_from_user(addr, (void *)sys_addr, elem_size);
@@ -505,6 +516,12 @@ static ssize_t rw_handle(struct nvmap_client *client, struct nvmap_handle *h,
 		h_offs += h_stride;
 		addr += h_stride;
 	}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+	/* Release the buffer used for VPR write */
+	if (!is_read && h->heap_type == NVMAP_HEAP_CARVEOUT_VPR && tmp)
+		vfree(tmp);
+#endif
 
 	return ret ?: copied;
 }