3f97fe1f141e0f708bdbf2705ad58368c2144f00
Bruce Xu <brucex@nvidia.com>
2019-08-08 18:21:57 +0800

    [hmm]: hmm test failed
    hmm needs support compact ioctl
    
    Bug 200541474
    
    Change-Id: I48ba5d98eac814efda2084a901f77e51c7c42f11
    Signed-off-by: Bruce Xu <brucex@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2170970
    (cherry picked from commit 71fe2035a86236505c50fbf4152648806e51370e)
    Reviewed-on: https://git-master.nvidia.com/r/2176623
    Reviewed-by: Vinayak Pane <vpane@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/char/hmm_dmirror.c
+++ drivers/char/hmm_dmirror.c
@@ -1010,6 +1010,9 @@ static const struct file_operations dmirror_fops = {
 	.open		= dummy_fops_open,
 	.release	= dummy_fops_release,
 	.unlocked_ioctl = dummy_fops_unlocked_ioctl,
+#ifdef CONFIG_COMPAT
+	.compat_ioctl	= dummy_fops_unlocked_ioctl,
+#endif
 	.llseek		= default_llseek,
 	.owner		= THIS_MODULE,
 };
