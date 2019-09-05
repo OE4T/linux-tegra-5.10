c0566ce14dcf753fd01857cdfd3037e1bc5b6640
JC Kuo <jckuo@nvidia.com>
2019-09-05 16:28:29 +0800

    phy: tegra: xusb: Add sleepwalk and suspend/resume
    This commit adds sleepwalk/wake and suspend/resume interfaces
    to Tegra XUSB PHY driver.
    
    Tegra XUSB host controller driver makes use of sleepwalk functions
    to enable/disable sleepwalk circuit which is in always-on partition
    and can respond to USB resume signals when controller is not powered.
    Sleepwalk can be enabled/disabled for any USB UPHY individually.
    
      - tegra_xusb_padctl_enable_phy_sleepwalk()
      - tegra_xusb_padctl_disable_phy_sleepwalk()
    
    Tegra XUSB host controller driver makes use of wake functions to
    enable/disable/query wake circuit which is in always-on partition
    can wake system up when USB resume happens.
    Wake circuit can be enabled/disabled for any USB PHY individually.
    
      - tegra_xusb_padctl_enable_phy_wake()
      - tegra_xusb_padctl_disable_phy_wake()
      - tegra_xusb_padctl_remote_wake_detected()
    
    This commit also adds two system suspend stubs that can be used to
    save and restore XUSB PADCTL context during system suspend and
    resume.
      - tegra_xusb_padctl_suspend_noirq()
      - tegra_xusb_padctl_resume_noirq()
    
    Change-Id: Ida6685c64ebe23e9bd471f126a7e7faab08feae7
    Signed-off-by: JC Kuo <jckuo@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2418403
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- include/linux/phy/tegra/xusb.h
+++ include/linux/phy/tegra/xusb.h
@@ -8,6 +8,7 @@
 
 struct tegra_xusb_padctl;
 struct device;
+enum usb_device_speed;
 
 struct tegra_xusb_padctl *tegra_xusb_padctl_get(struct device *dev);
 void tegra_xusb_padctl_put(struct tegra_xusb_padctl *padctl);
@@ -31,4 +32,11 @@ void tegra_xusb_padctl_enable_clamp_en_early(struct tegra_xusb_padctl *padctl,
 						struct phy *phy);
 void tegra_xusb_padctl_disable_clamp_en_early(struct tegra_xusb_padctl *padctl,
 						struct phy *phy);
+int tegra_xusb_padctl_enable_phy_sleepwalk(struct tegra_xusb_padctl *padctl, struct phy *phy,
+					   enum usb_device_speed speed);
+int tegra_xusb_padctl_disable_phy_sleepwalk(struct tegra_xusb_padctl *padctl, struct phy *phy);
+int tegra_xusb_padctl_enable_phy_wake(struct tegra_xusb_padctl *padctl, struct phy *phy);
+int tegra_xusb_padctl_disable_phy_wake(struct tegra_xusb_padctl *padctl, struct phy *phy);
+bool tegra_xusb_padctl_remote_wake_detected(struct tegra_xusb_padctl *padctl, struct phy *phy);
+
 #endif /* PHY_TEGRA_XUSB_H */
