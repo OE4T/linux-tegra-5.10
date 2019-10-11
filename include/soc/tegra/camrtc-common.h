38afb490e3f225cafaad5ced944964e0ad5f7115
Pekka Pessi <ppessi@nvidia.com>
2019-10-11 16:09:31 +0300

    include: soc: tegra: add MKxxx() macros
    Add MKxxx() macros for defining the camera firmware API constants.
    
    If required, the type of constants can be changed by predefining the
    MKxxx() macros.
    
    Use "UL" suffix with the U32_C() macro, ULL suffix with the U64_C().
    
    Jira CAMASIL-1298
    
    Change-Id: If61f2af1a278951fa55cc3c70dcefe20e4951966
    Signed-off-by: Pekka Pessi <ppessi@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2217660
    Reviewed-by: Bhushan Rayrikar <brayrikar@nvidia.com>
    Reviewed-by: Sudhir Vyas <svyas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- include/soc/tegra/camrtc-common.h
+++ include/soc/tegra/camrtc-common.h
@@ -30,10 +30,10 @@
 #define __aligned(_n) __attribute__((aligned(_n)))
 #endif
 #ifndef U64_C
-#define U64_C(_x_) (uint64_t)(_x_##U)
+#define U64_C(_x_) (uint64_t)(_x_##ULL)
 #endif
 #ifndef U32_C
-#define U32_C(_x_) (uint32_t)(_x_##U)
+#define U32_C(_x_) (uint32_t)(_x_##UL)
 #endif
 #ifndef U16_C
 #define U16_C(_x_) (uint16_t)(_x_##U)
@@ -42,5 +42,50 @@
 #define U8_C(_x_) (uint8_t)(_x_##U)
 #endif
 #endif
+
+/**
+ * @defgroup MK_xxx Macros for defining constants
+ *
+ * These macros are used to define constants in the camera/firmware-api
+ * headers.
+ *
+ * The user of the header files can predefine them and override the
+ * types of the constants.
+ *
+ * @{
+ */
+#ifndef MK_U64
+#define MK_U64(_x_)	U64_C(_x_)
+#endif
+
+#ifndef MK_U32
+#define MK_U32(_x_)	U32_C(_x_)
+#endif
+
+#ifndef MK_U16
+#define MK_U16(_x_)	U16_C(_x_)
+#endif
+
+#ifndef MK_U8
+#define MK_U8(_x_)	U8_C(_x_)
+#endif
+
+#ifndef MK_BIT32
+#define MK_BIT32(_x_)	(MK_U32(1) << MK_U32(_x_))
+#endif
+
+#ifndef MK_BIT64
+#define MK_BIT64(_x_)	(MK_U64(1) << MK_U64(_x_))
+#endif
+
+#ifndef MK_ALIGN
+#define MK_ALIGN(_x_)	_x_
+#endif
+
+#ifndef MK_SIZE
+#define MK_SIZE(_x_)	MK_U32(_x_)
+#endif
+
+/** @} */
 
 #endif /* INCLUDE_CAMRTC_COMMON_H */
