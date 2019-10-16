9fdc70c22e36d5e49d0afe199d12b60c56805e4d
Nitin Kumbhar <nkumbhar@nvidia.com>
2019-10-16 16:55:07 +0530

    gpu: nvgpu: add doxygen for common.defaults
    Update defaults header to include doxygen documentation
    for various default values used in NvGPU.
    
    Currently, the header defines default values only for
    timeouts.
    
    JIRA NVGPU-2418
    
    Change-Id: I4a29024dc6bf19c32a5070d0d75aa84ddebc3379
    Signed-off-by: Nitin Kumbhar <nkumbhar@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2219271
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/include/nvgpu/defaults.h
+++ drivers/gpu/nvgpu/include/nvgpu/defaults.h
@@ -23,13 +23,35 @@
 #ifndef NVGPU_DEFAULTS_H
 #define NVGPU_DEFAULTS_H
 
-/*
- * Default timeout used for channel watchdog and ctxsw timeout.
+/**
+ * @defgroup NVGPU_COMMON_DEFAULTS
+ *
+ * Default values used in NvGPU for timeouts, inits, etc.
+ */
+
+/**
+ * @ingroup NVGPU_COMMON_DEFAULTS
+ *
+ * The default timeout value defined in msec and used on a silicon
+ * platform. This timeout value is used for channel watchdog, ctxsw timeouts, gr
+ * timeouts, etc.
  */
 #define NVGPU_DEFAULT_POLL_TIMEOUT_MS	3000
 
+/**
+ * @ingroup NVGPU_COMMON_DEFAULTS
+ *
+ * The default timeout value defined in msec and used for railgate delay. This
+ * defines a value for auto-suspend delay.
+ */
 #define NVGPU_DEFAULT_RAILGATE_IDLE_TIMEOUT 500
 
+/**
+ * @ingroup NVGPU_COMMON_DEFAULTS
+ *
+ * The default timeout value defined in msec and used on FPGA platform. This
+ * timeout value is used at places similar to NVGPU_DEFAULT_POLL_TIMEOUT_MS.
+ */
 #define NVGPU_DEFAULT_FPGA_TIMEOUT_MS 100000U /* 100 sec */
 
 #endif /* NVGPU_DEFAULTS_H */
