700aeb868e373113c52214af9aedb13f82eb2c69
Nagarjuna Kristam <nkristam@nvidia.com>
2019-10-18 15:08:08 +0530

    phy: tegra: xusb: Add vbus override support on Tegra210
    Tegra XUSB device control driver needs to control vbus override
    during its operations, add API for the support.
    
    Bug 200592691
    
    Change-Id: I2c36470e51fd14a9310717c98fe0964c7d8d674c
    Signed-off-by: Nagarjuna Kristam <nkristam@nvidia.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    (cherry picked from commit 90767cdfea89c3ac7012035d66633b9ba839091a)
    Signed-off-by: Henry Lin <henryl@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.4/+/2306377
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: EJ Hsu <ejh@nvidia.com>
    Reviewed-by: Wayne Chang <waynec@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- include/linux/phy/tegra/xusb.h
+++ include/linux/phy/tegra/xusb.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2016-2020, NVIDIA CORPORATION.  All rights reserved.
  */
 
 #ifndef PHY_TEGRA_XUSB_H
@@ -18,5 +18,8 @@ int tegra_xusb_padctl_hsic_set_idle(struct tegra_xusb_padctl *padctl,
 				    unsigned int port, bool idle);
 int tegra_xusb_padctl_usb3_set_lfps_detect(struct tegra_xusb_padctl *padctl,
 					   unsigned int port, bool enable);
+int tegra_xusb_padctl_set_vbus_override(struct tegra_xusb_padctl *padctl,
+							bool val);
+int tegra_phy_xusb_utmi_port_reset(struct phy *phy);
 
 #endif /* PHY_TEGRA_XUSB_H */
