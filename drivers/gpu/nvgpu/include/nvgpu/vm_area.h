c4120eacbb3d89cce4f2c23bd4991e2e44bc1bf1
dinesh <dt@nvidia.com>
2019-10-18 13:08:19 +0530

    gpu: nvgpu: Doxygen for vm
    This adds doxygen documentation for commom mm.vm.
    
    Change-Id: Ice8c36a05f564730d01bf6efb4efb7456fce668d
    Signed-off-by: dinesh <dt@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2220848
    Reviewed-by: Lakshmanan M <lm@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Vijayakumar Subbu <vsubbu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/include/nvgpu/vm_area.h
+++ drivers/gpu/nvgpu/include/nvgpu/vm_area.h
@@ -30,22 +30,39 @@ struct vm_gk20a;
 struct gk20a_as_share;
 struct nvgpu_as_alloc_space_args;
 struct nvgpu_as_free_space_args;
-
+/**
+ * Carve out virtual address space from virtual memory context.
+ * This is needed for fixed address mapping.
+ */
 struct nvgpu_vm_area {
-	/*
-	 * Entry into the list of VM areas owned by a VM.
+	/**
+	 * Entry into the list of VM areas owned by a virtual
+	 * memory context.
 	 */
 	struct nvgpu_list_node vm_area_list;
-
-	/*
+	/**
 	 * List of buffers mapped into this vm_area.
 	 */
 	struct nvgpu_list_node buffer_list_head;
-
+	/**
+	 * Flags used for allocation of vm area.
+	 */
 	u32 flags;
+	/**
+	 * Page size to be used for GPU mapping.
+	 */
 	u32 pgsz_idx;
+	/**
+	 * The base address of the vm_area.
+	 */
 	u64 addr;
+	/**
+	 * size of the vm_area.
+	 */
 	u64 size;
+	/**
+	 * The GPU unmapping needed before using the vm_area.
+	 */
 	bool sparse;
 };
 
@@ -57,17 +74,93 @@ nvgpu_vm_area_from_vm_area_list(struct nvgpu_list_node *node)
 					    vm_area_list));
 };
 
-/*
- * Alloc space flags.
+/**
+ * Allocation of vm area at fixed address.
  */
 #define NVGPU_VM_AREA_ALLOC_FIXED_OFFSET		BIT32(0)
+
+/**
+ * Allocation of vm_area at random address.
+ */
 #define NVGPU_VM_AREA_ALLOC_SPARSE			BIT32(1)
 
+/**
+ * @brief Allocate a virtual memory area with given size and start.
+ *
+ * @param vm [in]	Pointer virtual memory context from the range to be
+ *			allocated.
+ * @param pages [in]	Number of pages to be allocated.
+ * @param page_size [in]Page size to be used for allocation.
+ * @param addr [in/out]	Start address of the range.
+ * @param flags [in]	Flags used for allocation.
+ *			Flags are
+ *			 NVGPU_VM_AREA_ALLOC_FIXED_OFFSET,
+ *			 NVGPU_VM_AREA_ALLOC_SPARSE
+ *
+ * - Check the input flags.
+ * - Find the page size index for the given @page_size.
+ * - Find the page allocator for for the computed index.
+ * - Initialise the memory for vm area and call #nvgpu_vm_area_alloc_memory()
+ *   to allocate vm area.
+ * - Fill the details in allocated vm_area.
+ * - Add the vm_area to the list of vm areas in virtual memory context.
+ * - If NVGPU_VM_AREA_ALLOC_SPARSE is set, unmap the allocated vm_area
+ *   from the GPU.
+ *
+ * @return		Zero, for successful allocation.
+ *			Suitable error code for failures.
+ */
 int nvgpu_vm_area_alloc(struct vm_gk20a *vm, u32 pages, u32 page_size,
 			u64 *addr, u32 flags);
+
+/**
+ * @brief Free the virtual memory area.
+ *
+ * @param vm [in]	Pointer virtual memory context from
+ *			the range to be allocated.
+ * @param addr [in/out]	Start address of the vm area to be freed.
+ *
+ * - Find the vm_area by calling #nvgpu_vm_area_find().
+ * - Remove the vm_area from the list.
+ * - Remove and unmap the buffers associated with the vm_area by
+ *   walking the list of buffers associated with it.
+ * - Free the vm_area.
+ *
+ * @return		Zero.
+ */
 int nvgpu_vm_area_free(struct vm_gk20a *vm, u64 addr);
 
+/**
+ * @brief Find the virtual memory area from vm context.
+ *
+ * @param vm [in]	Pointer virtual memory context.
+ * @param addr [in]	Base address of the vm area.
+ *
+ * - Walk the vm.vm_area_list, find the corresponding
+ *   #nvgpu_vm_area struct.
+ *
+ * @return		#nvgpu_vm_area struct, for success.
+ *			NULL, if it fails to fine the vm_area.
+ */
 struct nvgpu_vm_area *nvgpu_vm_area_find(struct vm_gk20a *vm, u64 addr);
+
+/**
+ * @brief Find and validate the virtual memory area. This helps
+ *  to validate the fixed offset buffer while GPU unmapping.
+ *
+ * @param vm [in]		Pointer virtual memory context.
+ * @param addr [in]		Address of the mapped buffer.
+ * @param map_size [in] 	Size of the mapping.
+ * @param pgsz_idx [in]		Page size index used for mapping.
+ * @param pvm_area [out]	#nvgpu_vm_area struct.
+ *
+ * - Find the vm_area from the vm context.
+ * - Validate the buffer mapping is not overlapping with other
+ *   mappings in the vm_area.
+ *
+ * @return			#nvgpu_vm_area struct, for success.
+ *				Suitable error code for failures.
+ */
 int nvgpu_vm_area_validate_buffer(struct vm_gk20a *vm,
 				  u64 map_addr, u64 map_size, u32 pgsz_idx,
 				  struct nvgpu_vm_area **pvm_area);
