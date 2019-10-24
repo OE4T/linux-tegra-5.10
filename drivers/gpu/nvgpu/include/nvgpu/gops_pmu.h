7df3591e13403aa70cebd3802f4232930b957843
Mahantesh Kumbar <mkumbar@nvidia.com>
2019-10-24 17:49:17 +0530

    gpu: nvgpu: gops_pmu doxygen update
    Added detailed description for all ops of
    gops_pmu for doxygen update.
    
    JIRA NVGPU-2457
    
    Change-Id: Ia9e0947afdcbc9958916ce0185b3e7be56480f01
    Signed-off-by: Mahantesh Kumbar <mkumbar@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2225048
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Vijayakumar Subbu <vsubbu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/include/nvgpu/gops_pmu.h
+++ drivers/gpu/nvgpu/include/nvgpu/gops_pmu.h
@@ -30,33 +30,241 @@ struct nvgpu_pmu;
 struct nvgpu_hw_err_inject_info_desc;
 
 /**
- * PMU unit & engine HAL operations.
+ * PMU unit and engine HAL operations.
  *
- * This structure stores the PMU unit & engine HAL function pointers.
+ * This structure stores the PMU unit and engine HAL function pointers.
  *
  * @see gpu_ops
  */
 struct gops_pmu {
+	/**
+	 * @brief PMU early initialization to allocate memory for PMU unit,
+	 *        set PMU Engine h/w properties and set supporting data structs.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * Initializes PMU unit data structs in the GPU driver based on detected
+	 * chip.
+	 *  + Allocate memory for #nvgpu_pmu data struct.
+	 *  + set PMU Engine h/w properties.
+	 *  + set PMU RTOS supporting data structs.
+	 *  + set sub-unit's data structs.
+	 *  + ops of the PMU unit.
+	 *
+	 * @return 0 in case of success, < 0 in case of failure.
+	 * @retval -ENOMEM if memory allocation fail for any unit.
+	 */
 	int (*pmu_early_init)(struct gk20a *g);
+
+	/** @cond DOXYGEN_SHOULD_SKIP_THIS */
+#ifdef CONFIG_NVGPU_LS_PMU
 	int (*pmu_rtos_init)(struct gk20a *g);
 	int (*pmu_destroy)(struct gk20a *g, struct nvgpu_pmu *pmu);
 	int (*pmu_pstate_sw_setup)(struct gk20a *g);
 	int (*pmu_pstate_pmu_setup)(struct gk20a *g);
+#endif
+
 	struct nvgpu_hw_err_inject_info_desc * (*get_pmu_err_desc)
 		(struct gk20a *g);
+	/** @endcond */
+
+	/**
+	 * @brief To know PMU Engine complete support is required or not.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * For some build complete PMU Engine enable/support is not required.
+	 * Below are the cases explaining where complete PMU Engine support
+	 * is required and not required.
+	 *  + For PMU RTOS support complete PMU Engine is required.
+	 *  + For iGPU FUSA ACR, PMU Engine Falcon is enough.
+	 *
+	 * On GV11b FUSA, iGPU FUSA ACR is supported and only PMU Falcon
+	 * support is enabled from PMU unit.
+	 *
+	 * + True  - Support the complete PMU Engine and PMU RTOS support.
+	 * + False - Only PMU Engine Falcon is supported.
+	 *
+	 * @return True for complete PMU Engine and PMU RTOS support.
+	 *         False for PMU Engine Falcon support only.
+	 */
 	bool (*is_pmu_supported)(struct gk20a *g);
+
+	/**
+	 * @brief Fetch base address of PMU Engine Falcon.
+	 *
+	 * @param void
+	 *
+	 * @return address.
+	 */
 	u32 (*falcon_base_addr)(void);
-	/* reset */
+
+	/**
+	 * @brief Reset the PMU Engine.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * Does the PMU Engine reset to bring into good known state.
+	 * The reset sequence also configures PMU Engine clock gating
+	 * and interrupts if interrupt support is enabled.
+	 *
+	 * @return 0 in case of success, < 0 in case of failure.
+	 */
 	int (*pmu_reset)(struct gk20a *g);
+
+	/**
+	 * @brief Change the PMU Engine reset state.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * PMU Engine reset state change as per input parameter.
+	 *  + True  - Bring PMU engine out of reset.
+	 *  + False - Keep PMU falcon/engine in reset.
+	 */
 	void (*reset_engine)(struct gk20a *g, bool do_reset);
+
+	/**
+	 * @brief Query the PMU Engine reset state.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * PMU Engine reset state is read and return as below,
+	 *  + True  - If PMU engine in reset.
+	 *  + False - If PMU engine is out of reset.
+	 *
+	 * @return True if in reset else False.
+	 */
 	bool (*is_engine_in_reset)(struct gk20a *g);
-	/* secure boot */
+
+	/**
+	 * @brief Setup the normal PMU apertures for standardized access.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * Creates a memory aperture that the PMU may use to access memory in
+	 * a specific address-space or mapped into the PMU's virtual-address
+	 * space. The aperture is identified using a unique index that will
+	 * correspond to a single dmaidx in the PMU framebuffer interface.
+	 */
 	void (*setup_apertures)(struct gk20a *g);
+
+	/**
+	 * @brief Setup DMA transfer base address.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * Setup DMA transfer base address as required for chip.
+	 */
 	void (*write_dmatrfbase)(struct gk20a *g, u32 addr);
+
+	/**
+	 * @brief Checks if PMU DEBUG fuse is blown or not
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * DEBUG_MODE bit is checked to know what type signature needs to be
+	 * used for Falcon ucode verification. DEBUG_MODE bit indicate that
+	 * PMU DEBUG fuse is blown and Debug Signal going to the SCP.
+	 *  + True  - Use debug signature.
+	 *  + False - use production signature.
+	 *
+	 * @return True if debug else False.
+	 */
 	bool (*is_debug_mode_enabled)(struct gk20a *g);
+
+	/**
+	 * @brief Start PMU falcon CPU in secure mode.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * Start PMU falcon CPU in secure mode by writing true to
+	 * CPUCTL_ALIAS.
+	 */
 	void (*secured_pmu_start)(struct gk20a *g);
+
+	/**
+	 * @brief Setup required configuration for PMU Engine Falcon boot.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * Setup required configuration for PMU Engine Falcon boot by
+	 * following belwo steps.
+	 *  + setup apertures.
+	 *  + Clearing mailbox register used for status.
+	 *  + Enable the context interface.
+	 *  + The instance block setup.
+	 */
 	void (*flcn_setup_boot_config)(struct gk20a *g);
+
+	/**
+	 * @brief Validate IMEM/DMEM memory integrity.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * Validate IMEM/DMEM memory integrity by checking ECC status
+	 * followed IMEM/DEME error correction status check.
+	 *
+	 *	return True if corrected else False.
+	 */
 	bool (*validate_mem_integrity)(struct gk20a *g);
+
+	/**
+	 * @brief Clears the PMU BAR0 error status.
+	 *
+	 * @param g   [in] The GPU driver struct.
+	 *
+	 * Clears the PMU BAR0 error status by reading status
+	 * and writing back.
+	 */
+	void (*pmu_clear_bar0_host_err_status)(struct gk20a *g);
+
+	/**
+	 * @brief Check for the PMU BAR0 error status.
+	 *
+	 * @param g           [in] The GPU driver struct.
+	 * @param bar0_status [out] The status read from register.
+	 * @param etype       [out] Specific error listed below.
+	 *
+	 * etype error:
+	 * + #PMU_BAR0_SUCCESS
+	 * + PMU_BAR0_HOST_READ_TOUT
+	 * + PMU_BAR0_HOST_WRITE_TOUT
+	 * + PMU_BAR0_FECS_READ_TOUT
+	 * + PMU_BAR0_FECS_WRITE_TOUT
+	 * + PMU_BAR0_CMD_READ_HWERR
+	 * + PMU_BAR0_CMD_WRITE_HWERR
+	 * + PMU_BAR0_READ_HOSTERR
+	 * + PMU_BAR0_WRITE_HOSTERR
+	 * + PMU_BAR0_READ_FECSERR
+	 * + PMU_BAR0_WRITE_FECSERR
+	 *
+	 * Reads the PMU BAR0 status register and check for error if read
+	 * value is not equal to 0x0, below are the different error
+	 * listed.
+	 *  + TIMEOUT_HOST
+	 *    Indicate that HOST does not respond the PRI request from
+	 *    falcon2csb interface.
+	 *  + TIMEOUT_FECS
+	 *    Indicate that FECS does not respond the PRI request from
+	 *    falcon2csb interface.
+	 *  + CMD_HWERR
+	 *    CMD_HWERR error is generated when SW or FW attempts to
+	 *    write the DATA, ADDR, or CTL registers to issue a new PRI
+	 *    request but the previous PRI request from falcon2csb is
+	 *    still busy or bar0master is disabled.
+	 *  + HOSTERR
+	 *    Indicate that HOST return ERROR back to BAR0MASTER for
+	 *    transaction error caused by falcon2csb request.
+	 *  + FECSERR
+	 *    Indicate that FECS return ERROR back to BAR0MASTER for
+	 *    transaction error caused by falcon2csb request.
+	 *
+	 * @return 0 in case of success, -EIO in case of failure.
+	 */
+	int (*bar0_error_status)(struct gk20a *g, u32 *bar0_status,
+		u32 *etype);
+
+	/** @cond DOXYGEN_SHOULD_SKIP_THIS */
 #ifdef CONFIG_NVGPU_LS_PMU
 	/* ISR */
 	void (*pmu_enable_irq)(struct nvgpu_pmu *pmu, bool enable);
@@ -103,9 +311,7 @@ struct gops_pmu {
 	void (*pmu_dump_falcon_stats)(struct nvgpu_pmu *pmu);
 	void (*dump_secure_fuses)(struct gk20a *g);
 #endif
-	void (*pmu_clear_bar0_host_err_status)(struct gk20a *g);
-	int (*bar0_error_status)(struct gk20a *g, u32 *bar0_status,
-		u32 *etype);
+	/** @endcond */
 };
 
 #endif
