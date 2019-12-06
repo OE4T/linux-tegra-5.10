93af48e238da0718867032be9da41fca99c64361
Sumit Gupta <sumitg@nvidia.com>
2019-12-06 19:29:49 +0530

    scripts: create defconfig copy and symlink in src-rt
    Create a copy of defconfig in src-rt patch within $OUT
    and a new symbolic link to that defconfig. This is done
    to avoid updating of defconfig in main kernel source path
    due to updation of defconfig symlink at src-rt.
    
    Bug 2776004
    Bug 200587200
    
    Change-Id: I7320451ee8fe1be3fc91a6b75d761ea217d12d21
    Signed-off-by: Sumit Gupta <sumitg@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2256888
    (cherry picked from commit f11a4301cd6f26998ec17d4446dade840d96a42e)
    (bbiswas update commit to match k5.4 defconfig)
    Signed-off-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.4/+/2297945
    (cherry picked from commit 056812bb0562f1803e557bfe81b694c98c20b68d)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2353481
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Manish Bhardwaj <mbhardwaj@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- scripts/rt-patch.sh
+++ scripts/rt-patch.sh
@@ -11,39 +11,48 @@
 any_failure=0
 apply_rt_patches()
 {
-	if [ -f ../arch/arm64/configs/.tmp.defconfig ]; then
+	if [ -f $PWD/../arch/arm64/configs/.orig.defconfig ]; then
 		echo "The PREEMPT RT patches are already applied to the kernel!"
 	else
-		file_list=`find ../rt-patches -name \*.patch -type f | sort`
+		file_list=`find $PWD/../rt-patches -name \*.patch -type f | sort`
 		for p in $file_list; do
 			# set flag in case of failure and continue
 			patch -s -d .. -p1 < $p || any_failure=1
 		done
 		#make temporary copy of the defconfig file
-		cp ../arch/arm64/configs/defconfig\
-			../arch/arm64/configs/.tmp.defconfig
-		./config --file ../arch/arm64/configs/defconfig\
+		cp -f "$PWD/../arch/arm64/configs/defconfig"\
+			"$PWD/../arch/arm64/configs/.updated.defconfig"
+
+		$PWD/config --file "$PWD/../arch/arm64/configs/.updated.defconfig"\
 			--enable PREEMPT_RT_FULL  --disable DEBUG_PREEMPT\
 			--disable CPU_IDLE_TEGRA18X\
 			--disable CPU_FREQ_GOV_INTERACTIVE\
 			--disable CPU_FREQ_TIMES \
 			--disable FAIR_GROUP_SCHED || any_failure=1
+
+		rm "$PWD/../arch/arm64/configs/defconfig"
+		cp -fnrs "$PWD/../arch/arm64/configs/.updated.defconfig"\
+				"$PWD/../arch/arm64/configs/defconfig"
+
 		echo "The PREEMPT RT patches have been successfully applied!"
 	fi
 }
 
 revert_rt_patches()
 {
-	if [ -f ../arch/arm64/configs/.tmp.defconfig ]; then
-		file_list=`find ../rt-patches -name \*.patch -type f | sort -r`
+	if [ -f $PWD/../arch/arm64/configs/.orig.defconfig ]; then
+		file_list=`find $PWD/../rt-patches -name \*.patch -type f | sort -r`
 		for p in $file_list; do
 			# set flag in case of failure and continue
 			patch -s -R -d .. -p1 < $p || any_failure=1
 		done
-		#  CPU_FREQ_GOV_INTERACTIVE need to keep disable for Automotive
-		cp ../arch/arm64/configs/.tmp.defconfig\
-			../arch/arm64/configs/defconfig
-		rm -rf ../arch/arm64/configs/.tmp.defconfig
+
+		rm "$PWD/../arch/arm64/configs/defconfig"
+		cp $PWD/../arch/arm64/configs/.orig.defconfig\
+			$PWD/../arch/arm64/configs/defconfig
+
+		rm -rf $PWD/../arch/arm64/configs/.orig.defconfig
+		rm -rf $PWD/../arch/arm64/configs/.updated.defconfig
 		echo "The PREEMPT RT patches have been successfully reverted!"
 	else
 		echo "The PREEMPT RT patches are not applied to the kernel!"
