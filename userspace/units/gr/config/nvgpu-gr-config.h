c25ccbb13092e33fb3871a919804d463741c89f6
vinodg <vinodg@nvidia.com>
2019-12-13 16:52:49 -0800

    gpu: nvgpu: Add negative test for gr.config unit
    Add test to coverage the error injections in gr.config unit.
    required_tests is updated with new test for gr.config and
    missing test for gr.setup unit.
    
    Jira NVGPU-4531
    
    Change-Id: Idf089af5fec1e653793a620b4e7f7bd5d96210ba
    Signed-off-by: vinodg <vinodg@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2262230
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: Deepak Nibade <dnibade@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- userspace/units/gr/config/nvgpu-gr-config.h
+++ userspace/units/gr/config/nvgpu-gr-config.h
@@ -44,6 +44,8 @@ struct unit_module;
  *
  * Input: None
  *
+ * Targets: #nvgpu_gr_config_init.
+ *
  * Steps:
  * -  Call nvgpu_gr_config_init
  *
@@ -59,7 +61,10 @@ int test_gr_config_init(struct unit_module *m, struct gk20a *g, void *args);
  *
  * Test Type: Feature based.
  *
- * Input: test_gr_config_init must have been executed successfully.
+ * Targets: #nvgpu_gr_config_deinit.
+ *
+ * Input: #test_gr_init_setup and #test_gr_config_init
+ *        must have been executed successfully.
  *
  * Steps:
  * -  Call nvgpu_gr_config_deinit
@@ -78,7 +83,25 @@ int test_gr_config_deinit(struct unit_module *m, struct gk20a *g, void *args);
  *
  * Test Type: Feature based, Error guessing.
  *
- * Input: test_gr_config_init must have been executed successfully.
+ * Input: #test_gr_init_setup and #test_gr_config_init
+ *        must have been executed successfully.
+ *
+ * Targets: #nvgpu_gr_config_get_max_gpc_count,
+ *          #nvgpu_gr_config_get_max_tpc_count,
+ *          #nvgpu_gr_config_get_max_tpc_per_gpc_count,
+ *          #nvgpu_gr_config_get_gpc_count,
+ *          #nvgpu_gr_config_get_tpc_count,
+ *          #nvgpu_gr_config_get_ppc_count,
+ *          #nvgpu_gr_config_get_pe_count_per_gpc,
+ *          #nvgpu_gr_config_get_sm_count_per_tpc,
+ *          #nvgpu_gr_config_get_gpc_mask,
+ *          #nvgpu_gr_config_get_gpc_ppc_count,
+ *          #nvgpu_gr_config_get_gpc_skip_mask,
+ *          #nvgpu_gr_config_get_gpc_tpc_count,
+ *          #nvgpu_gr_config_get_pes_tpc_count,
+ *          #nvgpu_gr_config_get_pes_tpc_mask,
+ *          #nvgpu_gr_config_get_gpc_tpc_mask_base,
+ *          #nvgpu_gr_config_get_gpc_tpc_count_base.
  *
  * Steps:
  * -  Read configuration count and mask informations from the driver
@@ -99,7 +122,16 @@ int test_gr_config_count(struct unit_module *m, struct gk20a *g, void *args);
  *
  * Test Type: Feature based, Error guessing
  *
- * Input: nvgpu_gr_config_init must have been executed successfully.
+ * Targets: #nvgpu_gr_config_set_no_of_sm,
+ *          #nvgpu_gr_config_get_sm_info,
+ *          #nvgpu_gr_config_set_sm_info_tpc_index,
+ *          #nvgpu_gr_config_set_sm_info_global_tpc_index,
+ *          #nvgpu_gr_config_set_sm_info_sm_index,
+ *          #nvgpu_gr_config_set_gpc_tpc_mask,
+ *          #nvgpu_gr_config_get_gpc_tpc_mask.
+ *
+ * Input: #test_gr_init_setup and #test_gr_config_init
+ *        must have been executed successfully.
  *
  * Steps:
  * -  Random values are set for various configuration and read back to
@@ -110,6 +142,33 @@ int test_gr_config_count(struct unit_module *m, struct gk20a *g, void *args);
  */
 int test_gr_config_set_get(struct unit_module *m, struct gk20a *g, void *args);
 
+/**
+ * Test specification for: test_gr_config_error_injection.
+ *
+ * Description: This test helps to verify whether the kernel handles all
+ *              possible error conditions for memory allocation failure. Also
+ *		provide different configurations in common.gr unit.
+ *
+ * Test Type: Feature based, Error guessing
+ *
+ * Input: #test_gr_init_setup must have been executed successfully.
+ *
+ * Targets: #nvgpu_gr_config_init,
+ *          #nvgpu_gr_config_deinit,
+ *
+ * Steps:
+ * -  Force memory allocation failures for various structures within
+ *    nvgpu_gr_config_init call.
+ * -  Set for various configuration like pes_tpc_count, gpc_tpc_mask,
+ *    gpc_count by adding stub function for various gr.config hal and
+ *    call nvgpu_gr_config_init.
+ * -  Force memory allocation failures with
+ *    g->ops.gr.config.init_sm_id_table call.
+ *
+ * Output: Returns PASS if the steps above were executed successfully. FAIL
+ * otherwise.
+ */
+int test_gr_config_error_injection(struct unit_module *m, struct gk20a *g, void *args);
 #endif /* UNIT_NVGPU_GR_CONFIG_H */
 
 /**
