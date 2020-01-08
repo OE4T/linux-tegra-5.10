3d28d40f4c32c96388f6723c5b0e1e24604027f7
Rohit Vaswani <rvaswani@nvidia.com>
2020-01-08 12:39:11 -0800

    Revert "t23x: Add support for 8x CCQ submission"
    This reverts commit b0ca346559fe7f7aca5eb3873a9c6e85e4fb0012.
    
    Change-Id: I5b7dffc354b73c70fc559ff6a9847b752c7978ec
    Signed-off-by: Rohit Vaswani <rvaswani@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/2276046
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Tested-by: Krishna Reddy <vdumpa@nvidia.com>

--- drivers/video/tegra/host/pva/pva_isr_t23x.c
+++ drivers/video/tegra/host/pva/pva_isr_t23x.c
@@ -24,89 +24,26 @@
 #include "pva_regs.h"
 #include "dev.h"
 #include "pva.h"
-#include "pva_ccq_t23x.h"
-
-#define PVA_MASK_LOW_16BITS 0xff
 
 irqreturn_t pva_ccq_isr(int irq, void *dev_id)
 {
+	/* This ISR is a placeholder which just clears any pending interrupts */
 	uint32_t clear_int = (0x1U << 20) | (0x1U << 24) | (0x1U << 28);
-	uint32_t int_status = 0, isr_status = 0, aisr_status = 0;
-	unsigned int queue_id = MAX_PVA_QUEUE_COUNT + 1;
+	uint32_t int_status;
 	int i;
 	struct pva *pva = dev_id;
 	struct platform_device *pdev = pva->pdev;
-	bool recover = false;
 
-	for (i = 1; i < MAX_PVA_IRQS; i++) {
-		if (pva->irq[i] == irq) {
-			queue_id = i - 1;
-			break;
+	nvhost_dbg_info("Received ISR from CCQ block, IRQ: %d", irq);
+	for (i = 0; i < MAX_PVA_QUEUE_COUNT; i++) {
+		int_status = host1x_readl(pdev,
+					  cfg_ccq_status_r(pva->version, i, 2))
+					  & ~0xff;
+		if (int_status != 0x0) {
+			host1x_writel(pdev,
+				      cfg_ccq_status_r(pva->version, i, 2),
+				      clear_int);
 		}
 	}
-	if (queue_id == MAX_PVA_QUEUE_COUNT + 1) {
-		printk("Invalid IRQ received. Returning from ISR");
-		return IRQ_HANDLED;
-	}
-	nvhost_dbg_info("Recieved ISR from CCQ block, IRQ: %d", irq);
-	int_status = host1x_readl(pdev, cfg_ccq_status_r(pva->version,
-				queue_id, PVA_CCQ_STATUS2_INDEX))
-				& ~PVA_MASK_LOW_16BITS;
-	if (int_status & PVA_VALID_CCQ_ISR) {
-		isr_status = host1x_readl(pdev, cfg_ccq_status_r(pva->version,
-					queue_id, PVA_CCQ_STATUS7_INDEX));
-	}
-	if (int_status & PVA_VALID_CCQ_AISR) {
-		aisr_status = host1x_readl(pdev, cfg_ccq_status_r(pva->version,
-					queue_id, PVA_CCQ_STATUS8_INDEX));
-	}
-	if (aisr_status & PVA_AISR_INT_PENDING) {
-		nvhost_dbg_info("PVA CCQ AISR (%x)", aisr_status);
-
-		/* For now, just log the errors */
-
-		if (aisr_status & PVA_AISR_TASK_ERROR)
-			nvhost_warn(&pdev->dev,
-				    "PVA AISR: \
-				    PVA_AISR_TASK_ERROR for queue id = %d",
-				    queue_id);
-		if (aisr_status & PVA_AISR_THRESHOLD_EXCEEDED)
-			nvhost_warn(&pdev->dev, "PVA AISR: \
-				PVA_AISR_THRESHOLD_EXCEEDED for queue id = %d",
-				queue_id);
-		if (aisr_status & PVA_AISR_LOGGING_OVERFLOW)
-			nvhost_warn(&pdev->dev, "PVA AISR: \
-				PVA_AISR_LOGGING_OVERFLOW for queue id = %d",
-				queue_id);
-		if (aisr_status & PVA_AISR_PRINTF_OVERFLOW)
-			nvhost_warn(&pdev->dev, "PVA AISR: \
-				PVA_AISR_PRINTF_OVERFLOW for queue id = %d",
-				queue_id);
-		if (aisr_status & PVA_AISR_CRASH_LOG)
-			nvhost_warn(&pdev->dev, "PVA AISR: \
-				PVA_AISR_CRASH_LOG for queue id = %d",
-				queue_id);
-		if (aisr_status & PVA_AISR_ABORT) {
-			nvhost_warn(&pdev->dev, "PVA AISR: \
-				PVA_AISR_ABORT for queue id = %d",
-				queue_id);
-			nvhost_warn(&pdev->dev, "Checkpoint value: 0x%08x",
-				    aisr_status);
-			recover = true;
-		}
-	}
-	if (isr_status & PVA_INT_PENDING) {
-		pva_ccq_isr_handler(pva, queue_id);
-	}
-	if (int_status != 0x0) {
-		nvhost_dbg_info("Clear ccq interrrupt for %d, \
-				current status: 0x%x",
-				queue_id, int_status);
-		host1x_writel(pdev, cfg_ccq_status_r(pva->version,
-				queue_id, PVA_CCQ_STATUS2_INDEX), clear_int);
-	}
-	if (recover)
-		pva_abort(pva);
-
 	return IRQ_HANDLED;
 }
