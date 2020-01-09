8672825fb9680a73b89e4b7b7e1fb85453d665ea
Omar Nemri <onemri@nvidia.com>
2020-01-09 10:40:54 -0800

    Revert "Revert "t23x: Add support for 8x CCQ submission""
    This reverts commit c3a7677c449433fbc235d49ed36f77690e001163.
    Signed-off-by: Omar Nemri <onemri@nvidia.com>
    
    Change-Id: I3b5055db976cd57cf6f375f6b88af2c99edb34fe
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-t23x/+/2276750
    Tested-by: Omar Nemri <onemri@nvidia.com>
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/host/pva/pva_version_config_t23x.c
+++ drivers/video/tegra/host/pva/pva_version_config_t23x.c
@@ -16,10 +16,14 @@
 
 #include "pva_mailbox_t23x.h"
 #include "pva_interface_regs_t23x.h"
+#include "pva_ccq_t23x.h"
 
 struct pva_version_config pva_t23x_config = {
 	.read_mailbox = pva_read_mailbox_t23x,
 	.write_mailbox = pva_write_mailbox_t23x,
 	.read_status_interface = read_status_interface_t23x,
+	.ccq_send_task = pva_ccq_send_task_t23x,
+	.submit_cmd_sync_locked = pva_send_cmd_sync_locked,
+	.submit_cmd_sync = pva_send_cmd_sync,
 	.irq_count = 9,
 };
