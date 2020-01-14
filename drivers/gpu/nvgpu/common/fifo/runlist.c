858905aeae18f515f67745e7e4df408e080902ca
Vedashree Vidwans <vvidwans@nvidia.com>
2020-01-14 13:18:58 -0800

    gpu: nvgpu: fifo: remove runlist.c dead code
    Currenly, nvgpu_runlist_cleanup_sw() includes a condition to check if
    nvgpu_fifo struct in GPU structure is NULL. However, as nvgpu_fifo is
    not included as a nvgpu_fifo pointer, it is not possible to set
    nvgpu_fifo member as NULL. So, this patch deletes this condition.
    
    Jira NVGPU-4817
    
    Change-Id: I3484f74064450ad031bfa0beea9bbd1a49165f72
    Signed-off-by: Vedashree Vidwans <vvidwans@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvgpu/+/2279112
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-misra <svc-mobile-misra@nvidia.com>
    Reviewed-by: svc-mobile-cert <svc-mobile-cert@nvidia.com>
    Reviewed-by: Thomas Fleury <tfleury@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Vinod Gopalakrishnakurup <vinodg@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/common/fifo/runlist.c
+++ drivers/gpu/nvgpu/common/fifo/runlist.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2020, NVIDIA CORPORATION.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -648,8 +648,7 @@ void nvgpu_runlist_cleanup_sw(struct gk20a *g)
 	u32 i, j;
 	struct nvgpu_runlist_info *runlist;
 
-	if ((f == NULL) || (f->runlist_info == NULL) ||
-					(f->active_runlist_info == NULL)) {
+	if ((f->runlist_info == NULL) || (f->active_runlist_info == NULL)) {
 		return;
 	}
 
