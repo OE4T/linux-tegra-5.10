madisongh/linux-tegra

ASoC: tegra-alt: Fix system suspend handling
There are a couple issues with system suspend handling in the Tegra
ASoC codec drivers which are:

1. Most drivers the system suspend callback does nothing and this
   means that if the codec device is active when suspend occurs then
   the codec will not be suspended correctly. This has been seen to
   cause system crashes in the I2S driver when the system is suspended
   and resuming while audio playback is active.

2. The codec suspend handlers may be called twice when entering
   suspend. Suspend handlers should only be called once, but currently
   they are called twice; once by the PM core and once when the APE
   power-domain is turned off (if it is on when suspend occurs).

Fix the above two problems by:

a). Adding proper system suspend and resume handlers for all codec
    drivers, to suspend and resume the device if not runtime PM
    suspended on entering suspend.
b). Remove the code to call the PM callbacks from the APE power-domain
    code.

Finally, make the system suspend callbacks for these codec drivers
late suspend callbacks to ensure that the sound core has suspended
any on-going activity, before we attempt to suspend these codec
devices.

Bug 200275736

Change-Id: I09d7e570be4e1866683873591ff76be63fdaf3fb
Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
Reviewed-on: http://git-master/r/1488953
GVS: Gerrit_Virtual_Submit
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Bibek Basu <bbasu@nvidia.com>
Tested-by: Bibek Basu <bbasu@nvidia.com>

sound/soc/tegra-alt/tegra210_admaif_alt.c
@@ -246,7 +246,18 @@ static int tegra210_admaif_runtime_resume(struct device *dev)
 #ifdef CONFIG_PM_SLEEP
 static int tegra210_admaif_suspend(struct device *dev)
 {
-	return 0;
+	if (pm_runtime_status_suspended(dev))
+		return 0;
+
+	return tegra210_admaif_runtime_suspend(dev);
+}
+
+static int tegra210_admaif_resume(struct device *dev)
+{
+	if (pm_runtime_status_suspended(dev))
+		return 0;
+
+	return tegra210_admaif_runtime_resume(dev);
 }
 #endif
 
@@ -1210,7 +1221,8 @@ static int tegra210_admaif_remove(struct platform_device *pdev)
 static const struct dev_pm_ops tegra210_admaif_pm_ops = {
 	SET_RUNTIME_PM_OPS(tegra210_admaif_runtime_suspend,
 			   tegra210_admaif_runtime_resume, NULL)
-	SET_SYSTEM_SLEEP_PM_OPS(tegra210_admaif_suspend, NULL)
+	SET_LATE_SYSTEM_SLEEP_PM_OPS(tegra210_admaif_suspend,
+				     tegra210_admaif_resume)
 };
 
 static struct platform_driver tegra210_admaif_driver = {

OE4T/linux-tegra-5.10

(cherry picked from commit 610f30df70a790733ed25a1be250e08cce19f368)
Change-Id: Ia0893c8373721ed16787a6c8243dca49f9672d34
Reviewed-on: https://git-master/r/1509428
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

sound/soc/tegra-alt/tegra210_admaif_alt.c
@@ -259,7 +259,18 @@ static int tegra_admaif_runtime_resume(struct device *dev)
 #ifdef CONFIG_PM_SLEEP
 static int tegra_admaif_suspend(struct device *dev)
 {
-	return 0;
+	if (pm_runtime_status_suspended(dev))
+		return 0;
+
+	return tegra_admaif_runtime_suspend(dev);
+}
+
+static int tegra_admaif_resume(struct device *dev)
+{
+	if (pm_runtime_status_suspended(dev))
+		return 0;
+
+	return tegra_admaif_runtime_resume(dev);
 }
 #endif
 
@@ -1337,7 +1348,7 @@ static int tegra_admaif_remove(struct platform_device *pdev)
 static const struct dev_pm_ops tegra_admaif_pm_ops = {
 	SET_RUNTIME_PM_OPS(tegra_admaif_runtime_suspend,
 			   tegra_admaif_runtime_resume, NULL)
-	SET_SYSTEM_SLEEP_PM_OPS(tegra_admaif_suspend, NULL)
+	SET_LATE_SYSTEM_SLEEP_PM_OPS(tegra_admaif_suspend, tegra_admaif_resume)
 };
 
 static struct platform_driver tegra_admaif_driver = {