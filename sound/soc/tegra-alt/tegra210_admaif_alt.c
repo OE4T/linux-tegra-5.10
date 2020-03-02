madisongh/linux-tegra

ASoC: tegra-alt: Avoid reg access after shutdown
Below are the hypothetical scenarios
- Drivers are in suspend state while reboot and PCM Open call from
  userspace is received after driver shutdown [APE is power gated
  already]
- PM domain handling doesn't ensure proper functionality after driver
  shutdown is called

The change handles with the below fix
- ALSA PCM Open API first executes runtime resume of each driver.
  So need to prevent any reg access in runtime resume of drivers.
- ALSA PCM Open executes ADMAIF startup after runtime resume of
  each driver. If ADMAIF startup returns failure based on shutdown,
  PCM Open operation will fail, this will break PCM Open path and
  will ensure no further ALSA APIs/Callbacks are called.

Bug 200289815

Change-Id: Ia03e035569375f37ae4b0faa1a4593ce121d2354
Signed-off-by: Mohan Kumar <mkumard@nvidia.com>
Reviewed-on: http://git-master/r/1475208
Reviewed-by: Automatic_Commit_Validation_User
GVS: Gerrit_Virtual_Submit
Reviewed-by: Sharad Gupta <sharadg@nvidia.com>
Reviewed-by: Dipesh Gandhi <dipeshg@nvidia.com>
Reviewed-by: Ravindra Lokhande <rlokhande@nvidia.com>

sound/soc/tegra-alt/tegra210_admaif_alt.c
@@ -1,7 +1,7 @@
 /*
  * tegra210_admaif_alt.c - Tegra ADMAIF driver
  *
- * Copyright (c) 2014-2016 NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2017 NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -245,7 +245,8 @@ static int tegra210_admaif_runtime_resume(struct device *dev)
 	}
 
 	regcache_cache_only(admaif->regmap, false);
-	regcache_sync(admaif->regmap);
+	if (!admaif->is_shutdown)
+		regcache_sync(admaif->regmap);
 
 	return 0;
 }
@@ -303,6 +304,11 @@ static int tegra210_admaif_prepare(struct snd_pcm_substream *substream,
 static int tegra210_admaif_startup(struct snd_pcm_substream *substream,
 				struct snd_soc_dai *dai)
 {
+	struct tegra210_admaif *admaif = snd_soc_dai_get_drvdata(dai);
+
+	if (admaif->is_shutdown)
+		return -ENODEV;
+
 	return 0;
 }
 
@@ -948,6 +954,7 @@ static int tegra210_admaif_probe(struct platform_device *pdev)
 	admaif->refcnt = 0;
 
 	admaif->soc_data = soc_data;
+	admaif->is_shutdown = false;
 
 	admaif->capture_dma_data = devm_kzalloc(&pdev->dev,
 			sizeof(struct tegra_alt_pcm_dma_params) *
@@ -1082,6 +1089,13 @@ err:
 	return ret;
 }
 
+static void tegra210_admaif_platform_shutdown(struct platform_device *pdev)
+{
+	struct tegra210_admaif *admaif = dev_get_drvdata(&pdev->dev);
+
+	admaif->is_shutdown = true;
+}
+
 static int tegra210_admaif_remove(struct platform_device *pdev)
 {
 	tegra_isomgr_adma_unregister();
@@ -1108,6 +1122,7 @@ static const struct dev_pm_ops tegra210_admaif_pm_ops = {
 static struct platform_driver tegra210_admaif_driver = {
 	.probe = tegra210_admaif_probe,
 	.remove = tegra210_admaif_remove,
+	.shutdown = tegra210_admaif_platform_shutdown,
 	.driver = {
 		.name = DRV_NAME,
 		.owner = THIS_MODULE,

OE4T/linux-tegra-5.10

(cherry picked from commit 3361e95850563f3009a428bb1c01941ed069a7b0)
Reviewed-on: http://git-master/r/1492664
Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>

sound/soc/tegra-alt/tegra210_admaif_alt.c
@@ -258,7 +258,8 @@ static int tegra_admaif_runtime_resume(struct device *dev)
 	}
 
 	regcache_cache_only(admaif->regmap, false);
-	regcache_sync(admaif->regmap);
+	if (!admaif->is_shutdown)
+		regcache_sync(admaif->regmap);
 
 	return 0;
 }
@@ -319,6 +320,11 @@ static int tegra_admaif_prepare(struct snd_pcm_substream *substream,
 static int tegra_admaif_startup(struct snd_pcm_substream *substream,
 				struct snd_soc_dai *dai)
 {
+	struct tegra_admaif *admaif = snd_soc_dai_get_drvdata(dai);
+
+	if (admaif->is_shutdown)
+		return -ENODEV;
+
 	return 0;
 }
 
@@ -1048,6 +1054,7 @@ static int tegra_admaif_probe(struct platform_device *pdev)
 	admaif->refcnt = 0;
 
 	admaif->soc_data = (struct tegra_admaif_soc_data *)match->data;
+	admaif->is_shutdown = false;
 
 	admaif->capture_dma_data = devm_kzalloc(&pdev->dev,
 			sizeof(struct tegra_alt_pcm_dma_params) *
@@ -1230,6 +1237,13 @@ err:
 	return ret;
 }
 
+static void tegra_admaif_platform_shutdown(struct platform_device *pdev)
+{
+	struct tegra_admaif *admaif = dev_get_drvdata(&pdev->dev);
+
+	admaif->is_shutdown = true;
+}
+
 static int tegra_admaif_remove(struct platform_device *pdev)
 {
 	struct tegra_admaif *admaif = dev_get_drvdata(&pdev->dev);
@@ -1259,6 +1273,7 @@ static const struct dev_pm_ops tegra_admaif_pm_ops = {
 static struct platform_driver tegra_admaif_driver = {
 	.probe = tegra_admaif_probe,
 	.remove = tegra_admaif_remove,
+	.shutdown = tegra_admaif_platform_shutdown,
 	.driver = {
 		.name = DRV_NAME,
 		.owner = THIS_MODULE,