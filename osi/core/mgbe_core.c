4653d93db4c418fcf08ac5cf378c46d4797dcba2
narayanr <narayanr@nvidia.com>
2020-03-10 16:44:25 +0530

    nvethernetrm: mgbe: Add support to forward error pkts
    Added support to configure Rx MTL Queue to drop or
    forward the error packets to DMA or application
    
    Bug 200565898
    
    Change-Id: I5e5dbad01c07f7b6efd1a1a0b9c07fcafa9050e5
    Signed-off-by: narayanr <narayanr@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/kernel/nvethernetrm/+/2310097
    Reviewed-by: Mahesh Patil <maheshp@nvidia.com>
    Reviewed-by: Bhadram Varka <vbhadram@nvidia.com>

--- osi/core/mgbe_core.c
+++ osi/core/mgbe_core.c
@@ -30,6 +30,64 @@
 #include "mgbe_mmc.h"
 
 /**
+ * @brief mgbe_config_fw_err_pkts - Configure forwarding of error packets
+ *
+ * Algorithm: When FEP bit is reset, the Rx queue drops packets with
+ *	  error status (CRC error, GMII_ER, watchdog timeout, or overflow).
+ *	  When FEP bit is set, all packets except the runt error packets
+ *	  are forwarded to the application or DMA.
+ *
+ * @param[in] addr: Base address indicating the start of memory mapped IO
+ * region of the MAC.
+ * @param[in] qinx: Q index
+ * @param[in] enable_fw_err_pkts: Enable or Disable the forwarding of error
+ * packets
+ *
+ * @note MAC should be init and started. see osi_start_mac()
+ *
+ * @retval 0 on success
+ * @retval -1 on failure.
+ */
+static int mgbe_config_fw_err_pkts(struct osi_core_priv_data *osi_core,
+				   const unsigned int qinx,
+				   const unsigned int enable_fw_err_pkts)
+{
+	unsigned int val;
+
+	/* Check for valid enable_fw_err_pkts and qinx values */
+	if ((enable_fw_err_pkts!= OSI_ENABLE &&
+	    enable_fw_err_pkts != OSI_DISABLE) ||
+	    (qinx >= OSI_MGBE_MAX_NUM_CHANS)) {
+		return -1;
+	}
+
+	/* Read MTL RXQ Operation_Mode Register */
+	val = osi_readl((unsigned char *)osi_core->base +
+			MGBE_MTL_CHX_RX_OP_MODE(qinx));
+
+	/* enable_fw_err_pkts, 1 is for enable and 0 is for disable */
+	if (enable_fw_err_pkts == OSI_ENABLE) {
+		/* When enable_fw_err_pkts bit is set, all packets except
+		 * the runt error packets are forwarded to the application
+		 * or DMA.
+		 */
+		val |= MGBE_MTL_RXQ_OP_MODE_FEP;
+	} else {
+		/* When this bit is reset, the Rx queue drops packets with error
+		 * status (CRC error, GMII_ER, watchdog timeout, or overflow)
+		 */
+		val &= ~MGBE_MTL_RXQ_OP_MODE_FEP;
+	}
+
+	/* Write to FEP bit of MTL RXQ Operation Mode Register to enable or
+	 * disable the forwarding of error packets to DMA or application.
+	 */
+	osi_writel(val, (unsigned char *)osi_core->base +
+		   MGBE_MTL_CHX_RX_OP_MODE(qinx));
+
+	return 0;
+}
+/**
  * @brief mgbe_poll_for_swr - Poll for software reset (SWR bit in DMA Mode)
  *
  * Algorithm: CAR reset will be issued through MAC reset pin.
@@ -2130,7 +2188,7 @@ void mgbe_init_core_ops(struct core_ops *ops)
 	ops->config_mac_loopback = mgbe_config_mac_loopback;
 	ops->set_avb_algorithm = OSI_NULL;
 	ops->get_avb_algorithm = OSI_NULL;
-	ops->config_fw_err_pkts = OSI_NULL;
+	ops->config_fw_err_pkts = mgbe_config_fw_err_pkts;
 	ops->config_tx_status = OSI_NULL;
 	ops->config_rx_crc_check = OSI_NULL;
 	ops->config_flow_control = OSI_NULL;
