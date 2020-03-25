OE4T/linux-tegra-4.9

gpio: t18x: Add support for configuring pins in suspend
Add support to configure the pins in different configuration
in suspend than active/idle state of the system.

This helps in configuring pins in SC7 specific requirements
during suspend.

bug 1853610

Change-Id: I278ea65db67fae9c8b09fadee1134c470590ff6a
Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
Reviewed-on: http://git-master/r/1317131
Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
GVS: Gerrit_Virtual_Submit

drivers/gpio/gpio-tegra186.c
@@ -16,6 +16,7 @@
 #include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
 #include <linux/platform_device.h>
@@ -426,6 +427,13 @@ struct tegra_gpio_controller {
 	struct tegra_gpio_info *tgi;
 };
 
+struct tegra_gpio_saved_register {
+	bool restore_needed;
+	u32 val;
+	u32 conf;
+	u32 out;
+};
+
 struct tegra_gpio_info {
 	struct device *dev;
 	int nbanks;
@@ -436,6 +444,7 @@ struct tegra_gpio_info {
 	struct tegra_gpio_controller tg_contrlr[MAX_GPIO_CONTROLLERS];
 	struct gpio_chip gc;
 	struct irq_chip ic;
+	struct tegra_gpio_saved_register *gpio_rval;
 };
 
 static struct lock_class_key gpio_lock_class;
@@ -601,6 +610,29 @@ static int tegra_gpio_direction_output(struct gpio_chip *chip, unsigned offset,
 		dev_err(chip->parent, "Failed to set output direction: %d\n",
 			ret);
 	return ret;
+}
+
+static int tegra_gpio_suspend_configure(struct gpio_chip *chip, unsigned offset,
+					enum gpiod_flags dflags)
+{
+	struct tegra_gpio_info *tgi = gpiochip_get_data(chip);
+	struct tegra_gpio_saved_register *regs;
+
+	if (!gpio_is_accessible(tgi, offset))
+		return -EBUSY;
+
+	regs = &tgi->gpio_rval[offset];
+	regs->conf = tegra_gpio_readl(tgi, offset, GPIO_ENB_CONFIG_REG),
+	regs->out = tegra_gpio_readl(tgi, offset, GPIO_OUT_CTRL_REG),
+	regs->val = tegra_gpio_readl(tgi, offset, GPIO_OUT_VAL_REG),
+	pinctrl_gpio_save_config(tgi->gc.base + offset);
+	regs->restore_needed = true;
+
+	if (dflags & GPIOD_FLAGS_BIT_DIR_OUT)
+		return tegra_gpio_direction_output(chip, offset,
+					dflags & GPIOD_FLAGS_BIT_DIR_VAL);
+
+	return tegra_gpio_direction_input(chip, offset);
 }
 
 static int tegra_gpio_set_debounce(struct gpio_chip *chip, unsigned offset,
@@ -859,9 +891,15 @@ static int tegra_gpio_probe(struct platform_device *pdev)
 	tgi = devm_kzalloc(&pdev->dev, sizeof(*tgi), GFP_KERNEL);
 	if (!tgi)
 		return -ENOMEM;
+
 	tgi->dev = &pdev->dev;
 	tgi->nbanks = bank;
 	tgi->soc = of_device_get_match_data(&pdev->dev);
+
+	tgi->gpio_rval = devm_kzalloc(&pdev->dev, tgi->soc->nports * 8 *
+				      sizeof(*tgi->gpio_rval), GFP_KERNEL);
+	if (!tgi->gpio_rval)
+		return -ENOMEM;
 
 	tgi->gc.label			= tgi->soc->name;
 	tgi->gc.request			= tegra_gpio_request;
@@ -871,6 +909,7 @@ static int tegra_gpio_probe(struct platform_device *pdev)
 	tgi->gc.direction_output	= tegra_gpio_direction_output;
 	tgi->gc.set			= tegra_gpio_set;
 	tgi->gc.get_direction		= tegra_gpio_get_direction;
+	tgi->gc.suspend_configure	= tegra_gpio_suspend_configure;
 	tgi->gc.is_enabled		= tegra_gpio_is_enabled;
 	tgi->gc.to_irq			= tegra_gpio_to_irq;
 	tgi->gc.set_debounce		= tegra_gpio_set_debounce;
@@ -961,6 +1000,45 @@ static int tegra_gpio_probe(struct platform_device *pdev)
 	return 0;
 }
 
+#ifdef CONFIG_PM_SLEEP
+static int tegra_gpio_resume_early(struct device *dev)
+{
+	struct tegra_gpio_info *tgi = dev_get_drvdata(dev);
+	struct tegra_gpio_saved_register *regs;
+	int i;
+
+	for (i = 0; i < tgi->gc.ngpio; i++) {
+		regs = &tgi->gpio_rval[i];
+		if (!regs->restore_needed)
+			continue;
+
+		regs->restore_needed = false;
+
+		tegra_gpio_writel(tgi, regs->val, i, GPIO_OUT_VAL_REG);
+		tegra_gpio_writel(tgi, regs->out, i, GPIO_OUT_CTRL_REG);
+		tegra_gpio_writel(tgi, regs->conf, i, GPIO_ENB_CONFIG_REG);
+		pinctrl_gpio_restore_config(tgi->gc.base + i);
+	}
+
+	return 0;
+}
+
+static int tegra_gpio_suspend_late(struct device *dev)
+{
+	struct tegra_gpio_info *tgi = dev_get_drvdata(dev);
+
+	return of_gpiochip_suspend(&tgi->gc);
+}
+
+static const struct dev_pm_ops tegra_gpio_pm = {
+        .suspend_late = tegra_gpio_suspend_late,
+        .resume_early = tegra_gpio_resume_early,
+};
+#define TEGRA_GPIO_PM		&tegra_gpio_pm
+#else
+#define TEGRA_GPIO_PM		NULL
+#endif
+
 static const struct tegra_gpio_soc_info t186_gpio_soc = {
 	.name = "tegra-gpio",
 	.debug_fs_name = "tegra_gpio",
@@ -1005,6 +1083,7 @@ static struct platform_driver tegra_gpio_driver = {
 	.driver		= {
 		.name	= "gpio-tegra186",
 		.of_match_table = tegra_gpio_of_match,
+		.pm = TEGRA_GPIO_PM,
 	},
 	.probe		= tegra_gpio_probe,
 };

OE4T/linux-tegra-5.10

Change-Id: I78087bbd7332f6a79bf3dfc19b1489ca5c9a41c2
(cherry picked from commit fb3dc5babb7938f969d8c5cb0d484cce16d22421)
Signed-off-by: Vishwaroop A <va@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.4/+/2308139
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/gpio/gpio-tegra186.c
@@ -10,7 +10,9 @@
 #include <linux/irq.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
+#include <linux/gpio/consumer.h>
 #include <linux/platform_device.h>
+#include <linux/of_gpio.h>
 
 #include <dt-bindings/gpio/tegra186-gpio.h>
 #include <dt-bindings/gpio/tegra194-gpio.h>
@@ -173,6 +175,13 @@ struct tegra_gpio_soc {
 	int gte_npins;
 };
 
+struct tegra_gpio_saved_register {
+	bool restore_needed;
+	u32 val;
+	u32 conf;
+	u32 out;
+};
+
 struct tegra_gpio {
 	struct gpio_chip gpio;
 	struct irq_chip intc;
@@ -186,6 +195,7 @@ struct tegra_gpio {
 	void __iomem *secure;
 	void __iomem *base;
 	void __iomem *gte_regs;
+	struct tegra_gpio_saved_register *gpio_rval;
 };
 
 /*************************** GTE related code ********************/
@@ -195,7 +205,6 @@ struct tegra_gte_info {
 	uint32_t slice;
 	uint32_t slice_bit;
 };
-
 
 /* Structure to maintain all information about the AON GPIOs
  * that can be supported
@@ -471,6 +480,30 @@ static int tegra186_gpio_direction_output(struct gpio_chip *chip,
 	writel(value, base + TEGRA186_GPIO_ENABLE_CONFIG);
 
 	return 0;
+}
+
+static int tegra_gpio_suspend_configure(struct gpio_chip *chip, unsigned offset,
+					enum gpiod_flags dflags)
+{
+	struct tegra_gpio *gpio = gpiochip_get_data(chip);
+	struct tegra_gpio_saved_register *regs;
+	void __iomem *base;
+
+	base = tegra186_gpio_get_base(gpio, offset);
+	if (WARN_ON(base == NULL))
+		return -EINVAL;
+
+	regs = &gpio->gpio_rval[offset];
+	regs->conf = readl(base + TEGRA186_GPIO_ENABLE_CONFIG),
+	regs->out = readl(base + TEGRA186_GPIO_OUTPUT_CONTROL),
+	regs->val = readl(base + TEGRA186_GPIO_OUTPUT_VALUE),
+	regs->restore_needed = true;
+
+	if (dflags & GPIOD_FLAGS_BIT_DIR_OUT)
+		return tegra186_gpio_direction_output(chip, offset,
+					dflags & GPIOD_FLAGS_BIT_DIR_VAL);
+
+	return tegra186_gpio_direction_input(chip, offset);
 }
 
 static int tegra_gpio_timestamp_control(struct gpio_chip *chip, unsigned offset,
@@ -846,6 +879,11 @@ static int tegra186_gpio_probe(struct platform_device *pdev)
 	if (IS_ERR(gpio->base))
 		return PTR_ERR(gpio->base);
 
+	gpio->gpio_rval = devm_kzalloc(&pdev->dev, gpio->soc->num_ports * 8 *
+				      sizeof(*gpio->gpio_rval), GFP_KERNEL);
+	if (!gpio->gpio_rval)
+		return -ENOMEM;
+
 	np = pdev->dev.of_node;
 	if (!np) {
 		dev_err(&pdev->dev, "No valid device node, probe failed\n");
@@ -899,6 +937,7 @@ static int tegra186_gpio_probe(struct platform_device *pdev)
 	gpio->gpio.set_config = tegra186_gpio_set_config;
 	gpio->gpio.timestamp_control = tegra_gpio_timestamp_control;
 	gpio->gpio.timestamp_read = tegra_gpio_timestamp_read;
+	gpio->gpio.suspend_configure = tegra_gpio_suspend_configure;
 
 	gpio->gpio.base = -1;
 
@@ -975,6 +1014,50 @@ static int tegra186_gpio_probe(struct platform_device *pdev)
 
 	return 0;
 }
+
+#ifdef CONFIG_PM_SLEEP
+static int tegra_gpio_resume_early(struct device *dev)
+{
+	struct tegra_gpio *gpio = dev_get_drvdata(dev);
+	struct tegra_gpio_saved_register *regs;
+	unsigned offset = 0U;
+	void __iomem *base;
+	int i;
+
+	base = tegra186_gpio_get_base(gpio, offset);
+	if (WARN_ON(base == NULL))
+		return -EINVAL;
+
+	for (i = 0; i < gpio->gpio.ngpio; i++) {
+		regs = &gpio->gpio_rval[i];
+		if (!regs->restore_needed)
+			continue;
+
+		regs->restore_needed = false;
+
+		writel(regs->val,  base + TEGRA186_GPIO_OUTPUT_VALUE);
+		writel(regs->out,  base + TEGRA186_GPIO_OUTPUT_CONTROL);
+		writel(regs->conf, base + TEGRA186_GPIO_ENABLE_CONFIG);
+	}
+
+	return 0;
+}
+
+static int tegra_gpio_suspend_late(struct device *dev)
+{
+	struct tegra_gpio *gpio = dev_get_drvdata(dev);
+
+	return of_gpiochip_suspend(&gpio->gpio);
+}
+
+static const struct dev_pm_ops tegra_gpio_pm = {
+	.suspend_late = tegra_gpio_suspend_late,
+	.resume_early = tegra_gpio_resume_early,
+};
+#define TEGRA_GPIO_PM		&tegra_gpio_pm
+#else
+#define TEGRA_GPIO_PM		NULL
+#endif
 
 static int tegra186_gpio_remove(struct platform_device *pdev)
 {
@@ -1137,6 +1220,7 @@ static struct platform_driver tegra186_gpio_driver = {
 	.driver = {
 		.name = "tegra186-gpio",
 		.of_match_table = tegra186_gpio_of_match,
+		.pm = TEGRA_GPIO_PM,
 	},
 	.probe = tegra186_gpio_probe,
 	.remove = tegra186_gpio_remove,