24589aec417baf6f5253abf12767a1bca6edaa2a
puneet saxena <puneets@nvidia.com>
2020-03-31 18:22:00 +0530

    tegra: mc: support dram_type
    Export an interface to return dram_type.
    
    Jira TMM-2018
    
    Change-Id: I978e3c38846656c56dbc2ac3d71ad60b8840c922
    Signed-off-by: puneet saxena <puneets@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-t23x/+/2321365
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Sanjay Chandrashekara <sanjayc@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/platform/tegra/mc/mc_utils.c
+++ drivers/platform/tegra/mc/mc_utils.c
@@ -24,7 +24,37 @@
 #define CH_8_BYTES_PER_CLK	(BYTES_PER_CLK_PER_CH * CH_8)
 #define CH_4_BYTES_PER_CLK	(BYTES_PER_CLK_PER_CH * CH_4)
 
-int ch_num;
+/* EMC regs */
+#define MC_BASE 0x02c10000
+#define EMC_BASE 0x02c60000
+
+#define EMC_FBIO_CFG5_0 0x103C
+#define MC_EMEM_ADR_CFG_CHANNEL_ENABLE_0 0xdf8
+#define MC_EMEM_ADR_CFG_0 0x54
+#define MC_ECC_CONTROL_0 0x1880
+
+#define CH_MASK 0xFFFF /* Change bit counting if this mask changes */
+#define CH4 0xf
+#define CH2 0x3
+
+#define ECC_MASK 0x1 /* 1 = enabled, 0 = disabled */
+#define RANK_MASK 0x1 /* 1 = 2-RANK, 0 = 1-RANK */
+#define DRAM_MASK 0x3
+
+/* EMC_FBIO_CFG5_0(1:0) : DRAM_TYPE */
+#define DRAM_LPDDR4 0
+#define DRAM_LPDDR5 1
+#define DRAM_DDR3 2
+
+struct emc_params {
+	u32 rank;
+	u32 ecc;
+	u32 ch;
+	u32 dram;
+};
+
+static struct emc_params emc_param;
+static int ch_num;
 
 static unsigned long freq_to_bw(unsigned long freq)
 {
@@ -62,7 +92,159 @@ unsigned long emc_bw_to_freq(unsigned long bw)
 }
 EXPORT_SYMBOL_GPL(emc_bw_to_freq);
 
-void tegra_mc_utils_init(int channels)
+enum dram_types tegra_dram_types(void)
 {
-	ch_num = channels;
+	enum dram_types dram_type = DRAM_TYPE_NONE;
+
+	switch (emc_param.dram) {
+	case DRAM_LPDDR5:
+		if (emc_param.ecc) {
+			if (ch_num == 16) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR5_16CH_ECC_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR5_16CH_ECC_1RANK;
+			} else if (ch_num == 8) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR5_8CH_ECC_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR5_8CH_ECC_1RANK;
+			} else if (ch_num == 4) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR5_4CH_ECC_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR5_4CH_ECC_1RANK;
+			}
+		} else {
+			if (ch_num == 16) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR5_16CH_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR5_16CH_1RANK;
+			} else if (ch_num == 8) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR5_8CH_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR5_8CH_1RANK;
+			} else if (ch_num == 4) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR5_4CH_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR5_4CH_1RANK;
+			}
+		}
+
+		if (ch_num < 4) {
+			pr_err("DRAM_LPDDR5: Unknown memory channel configuration\n");
+			WARN_ON(true);
+		}
+
+		break;
+	case DRAM_LPDDR4:
+		if (emc_param.ecc) {
+			if (ch_num == 16) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR4_16CH_ECC_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR4_16CH_ECC_1RANK;
+			} else if (ch_num == 8) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR4_8CH_ECC_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR4_8CH_ECC_1RANK;
+			} else if (ch_num == 4) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR4_4CH_ECC_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR4_4CH_ECC_1RANK;
+			}
+		} else {
+			if (ch_num == 16) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR4_16CH_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR4_16CH_1RANK;
+			} else if (ch_num == 8) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR4_8CH_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR4_8CH_1RANK;
+			} else if (ch_num == 4) {
+				if (emc_param.rank)
+					dram_type =
+					DRAM_TYPE_LPDDR5_4CH_2RANK;
+				else
+					dram_type =
+					DRAM_TYPE_LPDDR5_4CH_1RANK;
+			}
+		}
+
+		if (ch_num < 4) {
+			pr_err("DRAM_LPDDR4: Unknown memory channel configuration\n");
+			WARN_ON(true);
+		}
+		break;
+	default:
+		pr_err("mc_util: ddr config not supported\n");
+		WARN_ON(true);
+	}
+
+	return dram_type;
+}
+
+void tegra_mc_utils_init(void)
+{
+	u32 dram, ch, ecc, rank;
+	void __iomem *emc_base;
+
+	emc_base = ioremap(EMC_BASE, 0x00010000);
+	if (is_tegra_safety_build())
+		dram = 0x1;
+	else
+		dram = readl(emc_base + EMC_FBIO_CFG5_0) & DRAM_MASK;
+
+	ch = mc_readl(MC_EMEM_ADR_CFG_CHANNEL_ENABLE_0) & CH_MASK;
+	ecc = mc_readl(MC_ECC_CONTROL_0) & ECC_MASK;
+	rank = mc_readl(MC_EMEM_ADR_CFG_0) & RANK_MASK;
+
+	iounmap(emc_base);
+
+	while (ch) {
+		if (ch & 1)
+			ch_num++;
+		ch >>= 1;
+	}
+
+	/* pre silicon use LPDDR4 16ch no ecc 1-rank config*/
+	if (tegra_platform_is_sim() || tegra_platform_is_fpga()) {
+		dram = DRAM_LPDDR4;
+		ch_num = 16;
+	}
+
+	emc_param.ch = ch;
+	emc_param.ecc = ecc;
+	emc_param.rank = rank;
+	emc_param.dram = dram;
 }
