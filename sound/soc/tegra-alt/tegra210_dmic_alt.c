e44fad82829b98cc4375e72bf8eddc954d163803
Mohan Kumar <mkumard@nvidia.com>
2020-04-23 15:26:50 +0530

    ASoC: tegra-alt: Support dynamic pinmux for DMIC
    Add driver changes to support dynamic pinmux for DMIC modules.
    
    Bug 200610724
    
    Change-Id: I51c6c613f7d34584563b970018745ac52bfe36b5
    Signed-off-by: Mohan Kumar <mkumard@nvidia.com>
    Signed-off-by: Viswanath L <viswanathl@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2358018
    (cherry picked from commit 2c62e612cdabd3bbb88c52557e7a5422914cdff0)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2406376
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sharad Gupta <sharadg@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- sound/soc/tegra-alt/tegra210_dmic_alt.c
+++ sound/soc/tegra-alt/tegra210_dmic_alt.c
@@ -35,6 +35,7 @@
 #include <sound/soc.h>
 #include <linux/of_device.h>
 #include <linux/pinctrl/pinconf-tegra.h>
+#include <linux/pinctrl/consumer.h>
 
 #include "tegra210_xbar_alt.h"
 #include "tegra210_dmic_alt.h"
@@ -125,7 +126,37 @@ static int tegra210_dmic_startup(struct snd_pcm_substream *substream,
 		}
 	}
 
+	if (!(tegra_platform_is_fpga())) {
+		if (!IS_ERR_OR_NULL(dmic->pin_active_state)) {
+			ret = pinctrl_select_state(dmic->pinctrl,
+						dmic->pin_active_state);
+			if (ret < 0) {
+				dev_err(dev,
+				"Setting dmic pinctrl active state failed\n");
+				return -EINVAL;
+			}
+		}
+	}
+
 	return 0;
+}
+
+static void tegra210_dmic_shutdown(struct snd_pcm_substream *substream,
+				   struct snd_soc_dai *dai)
+{
+	struct device *dev = dai->dev;
+	struct tegra210_dmic *dmic = snd_soc_dai_get_drvdata(dai);
+	int ret;
+
+	if (!(tegra_platform_is_fpga())) {
+		if (!IS_ERR_OR_NULL(dmic->pin_idle_state)) {
+			ret = pinctrl_select_state(
+				dmic->pinctrl, dmic->pin_idle_state);
+			if (ret < 0)
+				dev_err(dev,
+				"Setting dmic pinctrl idle state failed\n");
+		}
+	}
 }
 
 static int tegra210_dmic_hw_params(struct snd_pcm_substream *substream,
@@ -317,6 +348,7 @@ static int tegra210_dmic_put_control(struct snd_kcontrol *kcontrol,
 static struct snd_soc_dai_ops tegra210_dmic_dai_ops = {
 	.hw_params	= tegra210_dmic_hw_params,
 	.startup	= tegra210_dmic_startup,
+	.shutdown       = tegra210_dmic_shutdown,
 };
 
 static struct snd_soc_dai_driver tegra210_dmic_dais[] = {
@@ -602,6 +634,21 @@ static int tegra210_dmic_platform_probe(struct platform_device *pdev)
 		if (ret < 0)
 			dev_warn(&pdev->dev, "Failed to set %s setting\n",
 				 dmic->prod_name);
+	}
+
+	dmic->pinctrl = devm_pinctrl_get(&pdev->dev);
+	if (IS_ERR(dmic->pinctrl)) {
+		dev_dbg(&pdev->dev, "Missing pinctrl device\n");
+		return 0;
+	}
+
+	dmic->pin_active_state = pinctrl_lookup_state(dmic->pinctrl,
+							"dap_active");
+	dmic->pin_idle_state = pinctrl_lookup_state(dmic->pinctrl,
+							"dap_inactive");
+	if (IS_ERR(dmic->pin_active_state) && IS_ERR(dmic->pin_idle_state)) {
+		dev_dbg(&pdev->dev, "Pinctrl: No DAP states found\n");
+		devm_pinctrl_put(dmic->pinctrl);
 	}
 
 	return 0;
