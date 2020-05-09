ab433d9d62e053d5cace054e50b47a7e91f413d4
puneet saxena <puneets@nvidia.com>
2020-05-10 00:35:28 +0530

    dma-coherent: Add maximum alignment order
    Alignment order for coherent dma buffer is set by buffer size.
    For large buffer, it is a waste of address space.
    So using ARM_DMA_IOMMU_ALIGNMENT configurable parameter to limit
    maximum alignment order to reduce the waste during coherent memory
    allocation.
    This fixes alignment issue seen by gk20a
    
    Bug 200380854
    
    Change-Id: I7197f66fe67161137269ce64fdada8295bfc1686
    Signed-off-by: Vandana Salve <vsalve@nvidia.com>
    Reviewed-on: http://git-master/r/356347
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    (cherry picked from commit c3c725f94b00810185d1666ff463cc343a099c18)
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1688930
    Signed-off-by: puneet saxena <puneets@nvidia.com>
    (cherry picked from commit a99f7519ac3d86273e6d85d624fb7fd804db4d26)
    Signed-off-by: Prateek Patel <prpatel@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2362014
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- kernel/dma/coherent.c
+++ kernel/dma/coherent.c
@@ -11,6 +11,12 @@
 #include <linux/module.h>
 #include <linux/dma-mapping.h>
 
+#ifdef CONFIG_ARM_DMA_IOMMU_ALIGNMENT
+#define DMA_BUF_ALIGNMENT CONFIG_ARM_DMA_IOMMU_ALIGNMENT
+#else
+#define DMA_BUF_ALIGNMENT 8
+#endif
+
 struct dma_coherent_mem {
 	void		*virt_base;
 	dma_addr_t	device_base;
@@ -150,7 +156,11 @@ static void *__dma_alloc_from_coherent(struct device *dev,
 		align = 0;
 		count = PAGE_ALIGN(size) >> PAGE_SHIFT;
 	} else  {
-		align = (1 << order) - 1;
+		if (order > DMA_BUF_ALIGNMENT)
+			align = (1 << DMA_BUF_ALIGNMENT) - 1;
+		else
+			align = (1 << order) - 1;
+
 		count = 1 << order;
 	}
 
