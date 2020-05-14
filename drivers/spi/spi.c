InES-HPMM/linux-l4t

spi: Add spi_cs_low() callback
spi_cs_low() callback is added for spi client
users to set chip select state(low/high).

prototype:
int spi_cs_low(struct spi_device *spi, bool state);

Bug 1371286

Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
Change-Id: I4b2374202267338246262d902d6492d35dc755fb
Reviewed-on: http://git-master/r/303902
Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

drivers/spi/spi.c
@@ -1708,6 +1708,29 @@ int spi_write_then_read(struct spi_device *spi,
 }
 EXPORT_SYMBOL_GPL(spi_write_then_read);
 
+/**
+ * spi_cs_low - set chip select pin state
+ * @spi: device for which chip select pin state to be set
+ * state: if true chip select pin will be kept low else high
+ *
+ * The return value is zero for success, else
+ * errno status code.
+ */
+int spi_cs_low(struct spi_device *spi, bool state)
+{
+	struct spi_master *master = spi->master;
+	int ret = 0;
+
+	mutex_lock(&master->bus_lock_mutex);
+
+	if (master->spi_cs_low)
+		ret = master->spi_cs_low(spi, state);
+
+	mutex_unlock(&master->bus_lock_mutex);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(spi_cs_low);
+
 /*-------------------------------------------------------------------------*/
 
 static int __init spi_init(void)

OE4T/linux-tegra-5.10

Bug 200601675
(cherry picked from commit ec4ad1ec67dbf3ad22d95e82d735b535c8debefa)
Signed-off-by: Krishna Yarlagadda <kyarlagadda@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1747148
(cherry picked from commit d43c09520420a7c4187fe579c0b1192ff3b42e8a)
Signed-off-by: Vishwaroop A <va@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.4/+/2306627
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/spi/spi.c
@@ -3652,6 +3652,29 @@ int spi_write_then_read(struct spi_device *spi,
 }
 EXPORT_SYMBOL_GPL(spi_write_then_read);
 
+/**
+ * spi_cs_low - set chip select pin state
+ * @spi: device for which chip select pin state to be set
+ * state: if true chip select pin will be kept low else high
+ *
+ * The return value is zero for success, else
+ * errno status code.
+ */
+int spi_cs_low(struct spi_device *spi, bool state)
+{
+	struct spi_controller *ctrl = spi->master;
+	int ret = 0;
+
+	mutex_lock(&ctrl->bus_lock_mutex);
+
+	if (ctrl->spi_cs_low)
+		ret = ctrl->spi_cs_low(spi, state);
+
+	mutex_unlock(&ctrl->bus_lock_mutex);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(spi_cs_low);
+
 /*-------------------------------------------------------------------------*/
 
 #if IS_ENABLED(CONFIG_OF)