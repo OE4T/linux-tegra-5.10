7f1acd5ce11f34c7d26b9747e60d11c2eaf4c4d3
David Pu <dpu@nvidia.com>
2020-07-02 23:52:00 -0700

    t23x: psc: add data xfer ioctl
    This patch adds one new ioctl to transfer data between PSC and Linux
    using shared memory for testing purpose.
    
    Jira PSCFW-177
    
    Change-Id: I4772715b9b3804e8db0b4414a4beb68edcd73a59
    Signed-off-by: David Pu <dpu@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-t23x/+/2393376
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Marvin Hsu <marvinh@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/platform/tegra/psc/psc_debug.c
+++ drivers/platform/tegra/psc/psc_debug.c
@@ -12,22 +12,64 @@
  *
  */
 
+#define DEBUG
+#include <linux/ioctl.h>
+#include <linux/types.h>
 #include <linux/of_platform.h>
 #include <linux/mailbox_client.h>
 #include <linux/debugfs.h>
 #include <linux/fs.h>
+#include <linux/mutex.h>
 #include <linux/uaccess.h>
+#include <linux/dma-mapping.h>
+#include <linux/types.h>
 #include "tegra23x_psc.h"
+
 
 #define MBOX_MSG_LEN 64
 
+#define RX_READY 1
+#define RX_IDLE 0
+
+/* Max block period in ms before TX is assumed failure. */
+#define DEFAULT_TX_TIMEOUT 2000
+
+/* 4MB max size to use for dma_alloc* */
+#define MAX_SHARED_MEM (4 * 1024 * 1024U)
+
+struct xfer_info {
+	__u32 opcode[2];
+	void __user *tx_buf;
+	void __user *rx_buf;
+
+	__u32 tx_size;
+	__u32 rx_size;
+
+	__u8  out[MBOX_MSG_LEN];
+};
+
+union mbox_msg {
+	struct {
+		u32 opcode[2];
+		uint64_t tx_iova;
+		uint64_t rx_iova;
+		u32 tx_size;
+		u32 rx_size;
+	};
+	u32 data[16];
+};
+
+#define PSCDBG_IOCTL_BASE 'P'
+#define PSCIOC_XFER_DATA _IOWR(PSCDBG_IOCTL_BASE, 0, struct xfer_info)
+
 struct psc_debug_dev {
+	struct mutex lock;
 	struct platform_device *pdev;
 	struct mbox_client cl;
 	struct mbox_chan *chan;
 	wait_queue_head_t read_wait;
-	atomic_t open_cnt;
 	atomic_t data_ready;
+
 	u8 rx_msg[MBOX_MSG_LEN];
 };
 
@@ -37,11 +79,12 @@ static struct dentry *debugfs_root;
 static int psc_debug_open(struct inode *inode, struct file *file)
 {
 	struct mbox_chan *chan;
-	struct psc_debug_dev *dbg = &psc_debug;
+	struct psc_debug_dev *dbg = inode->i_private;
 	struct platform_device *pdev = dbg->pdev;
+	int ret = 0;
 
-	if (atomic_read(&dbg->open_cnt))
-		return -EBUSY;
+	if (mutex_lock_interruptible(&dbg->lock))
+		return -ERESTARTSYS;
 
 	file->private_data = dbg;
 
@@ -49,29 +92,31 @@ static int psc_debug_open(struct inode *inode, struct file *file)
 	if (IS_ERR(chan) && (PTR_ERR(chan) != -EPROBE_DEFER)) {
 		dev_err(&pdev->dev, "failed to get channel, err %lx\n",
 			PTR_ERR(chan));
-		return PTR_ERR(chan);
+		ret = PTR_ERR(chan);
+		goto return_unlock;
 	}
 
-	dev_info(&pdev->dev, "get mbox channel 0\n");
 	dbg->chan = chan;
-
-	atomic_set(&dbg->open_cnt, 0);
-	atomic_set(&dbg->data_ready, 0);
-
+	atomic_set(&dbg->data_ready, RX_IDLE);
 	nonseekable_open(inode, file);
 
-	return 0;
+return_unlock:
+	mutex_unlock(&dbg->lock);
+	return ret;
 }
 
 static int psc_debug_release(struct inode *inode, struct file *file)
 {
 	struct psc_debug_dev *dbg = file->private_data;
 
+	mutex_lock(&dbg->lock);
+
 	mbox_free_channel(dbg->chan);
 
-	atomic_set(&dbg->open_cnt, 0);
-	atomic_set(&dbg->data_ready, 0);
+	atomic_set(&dbg->data_ready, RX_IDLE);
 	file->private_data = NULL;
+
+	mutex_unlock(&dbg->lock);
 	return 0;
 }
 
@@ -82,20 +127,32 @@ static ssize_t psc_debug_read(struct file *file, char __user *buffer,
 	ssize_t ret;
 	loff_t pos = 0;
 
-	if (atomic_read(&dbg->data_ready) <= 0) {
-		if (file->f_flags & O_NONBLOCK)
-			return -EAGAIN;
+	if (count > MBOX_MSG_LEN)
+		return -EINTR;
+
+	mutex_lock(&dbg->lock);
+
+	if (atomic_read(&dbg->data_ready) != RX_READY) {
+		if (file->f_flags & O_NONBLOCK) {
+			ret = -EAGAIN;
+			goto return_unlock;
+		}
 
 		if (wait_event_interruptible(dbg->read_wait,
-					atomic_read(&dbg->data_ready) > 0))
-			return -EINTR;
+					atomic_cmpxchg(&dbg->data_ready,
+						RX_READY,
+						RX_IDLE) == RX_READY)) {
+			ret = -EINTR;
+			goto return_unlock;
+		}
 	}
 
 	ret = simple_read_from_buffer(buffer, count, &pos,
 			dbg->rx_msg, min_t(size_t, count, MBOX_MSG_LEN));
 	*ppos += pos;
-	atomic_set(&dbg->data_ready, 0);
 
+return_unlock:
+	mutex_unlock(&dbg->lock);
 	return ret;
 }
 
@@ -112,32 +169,163 @@ static ssize_t psc_debug_write(struct file *file, const char __user *buffer,
 		return -EINVAL;
 	}
 
+	mutex_lock(&dbg->lock);
 	if (copy_from_user(tx_buf, buffer, count)) {
 		dev_err(&pdev->dev, "copy_from_user() error!\n");
-		return -EFAULT;
+		ret = -EFAULT;
+		goto return_unlock;
 	}
 	ret = mbox_send_message(dbg->chan, tx_buf);
+
+return_unlock:
+	mutex_unlock(&dbg->lock);
 	return ret < 0 ? ret : count;
+}
+
+static long xfer_data(struct file *file, char __user *data)
+{
+	struct psc_debug_dev *dbg = file->private_data;
+	struct platform_device *pdev = dbg->pdev;
+	struct device *dev = &pdev->dev;
+	void *tx_virt = NULL;
+	void *rx_virt = NULL;
+	dma_addr_t tx_phys = 0;
+	dma_addr_t rx_phys = 0;
+	long ret = 0;
+	union mbox_msg msg = {0};
+	struct xfer_info info;
+
+	if (copy_from_user(&info, data, sizeof(struct xfer_info))) {
+		dev_err(&pdev->dev, "failed to copy data.\n");
+		ret = -EFAULT;
+	}
+
+	dev_dbg(dev, "opcode[%x %x]\n", info.opcode[0], info.opcode[1]);
+	dev_dbg(dev, "tx[%p, size:%u], rx[%p, size:%u]\n",
+		info.tx_buf, info.tx_size, info.rx_buf, info.rx_size);
+
+	if (info.tx_size > MAX_SHARED_MEM || info.rx_size > MAX_SHARED_MEM)
+		return -ENOMEM;
+
+	if (info.tx_buf && info.tx_size > 0) {
+
+		tx_virt = dma_alloc_coherent(dev, info.tx_size,
+					&tx_phys, GFP_KERNEL);
+		if (tx_virt == NULL || tx_phys == 0) {
+			dev_err(dev, "dma_alloc_coherent() failed!\n");
+			ret = -ENOMEM;
+			goto fail;
+		}
+
+		if (copy_from_user(tx_virt, info.tx_buf, info.tx_size)) {
+			dev_err(dev, "failed to copy data.\n");
+			ret = -EFAULT;
+			goto free_rx;
+		}
+		dma_sync_single_for_device(dev, tx_phys,
+				info.tx_size, DMA_TO_DEVICE);
+	}
+
+	if (info.rx_buf && info.rx_size > 0) {
+
+		rx_virt = dma_alloc_coherent(dev, info.rx_size,
+				&rx_phys, GFP_KERNEL);
+		if (rx_virt == NULL || rx_phys == 0) {
+			dev_err(dev, "dma_alloc_coherent() failed!\n");
+			ret = -ENOMEM;
+			goto free_tx;
+		}
+	}
+	dev_dbg(dev, "tx_virt:%p, tx_phys: %p\n", tx_virt, (void *)tx_phys);
+	dev_dbg(dev, "rx_virt:%p, rx_phys: %p\n", rx_virt, (void *)rx_phys);
+
+	msg.opcode[0] = info.opcode[0];
+	msg.opcode[1] = info.opcode[1];
+	msg.tx_iova = tx_phys;
+	msg.rx_iova = rx_phys;
+	msg.tx_size = info.tx_size;
+	msg.rx_size = info.rx_size;
+
+	ret = mbox_send_message(dbg->chan, &msg);
+	if (ret < 0) {
+		dev_err(dev, "mbox_send_message() failed: ret:%ld\n", ret);
+		goto free_rx;
+	}
+
+	if (atomic_read(&dbg->data_ready) != RX_READY &&
+		wait_event_interruptible(dbg->read_wait,
+				atomic_cmpxchg(&dbg->data_ready,
+					RX_READY,
+					RX_IDLE) == RX_READY)) {
+		ret = -EINTR;
+		goto free_rx;
+	}
+
+	dev_dbg(dev, "dbg->msg[%x %x %x %x]\n",
+		dbg->rx_msg[0], dbg->rx_msg[1], dbg->rx_msg[2], dbg->rx_msg[3]);
+
+	/* copy mbox payload */
+	memcpy(&info.out[0], &dbg->rx_msg[0], sizeof(dbg->rx_msg));
+
+	if (rx_phys && info.rx_size > 0) {
+		dma_sync_single_for_cpu(dev, rx_phys,
+				info.rx_size, DMA_BIDIRECTIONAL);
+
+		if (copy_to_user(info.rx_buf, rx_virt, msg.rx_size)) {
+			dev_err(dev, "failed to copy_to_user.\n");
+			ret = -EFAULT;
+			goto free_rx;
+		}
+	}
+
+free_rx:
+	if (rx_phys)
+		dma_free_coherent(dev, info.rx_size, rx_virt, rx_phys);
+free_tx:
+	if (tx_phys)
+		dma_free_coherent(dev, info.tx_size, tx_virt, tx_phys);
+fail:
+	return ret;
+}
+
+static long
+psc_debug_ioctl(struct file *file, unsigned int cmd, unsigned long data)
+{
+	struct psc_debug_dev *dbg = file->private_data;
+	long ret = -ENOIOCTLCMD;
+
+	mutex_lock(&dbg->lock);
+
+	switch (cmd) {
+	case PSCIOC_XFER_DATA:
+		ret = xfer_data(file, (char __user *)data);
+		break;
+	default:
+		break;
+	}
+
+	mutex_unlock(&dbg->lock);
+	return ret;
 }
 
 static const struct file_operations psc_debug_fops = {
 	.open		= psc_debug_open,
 	.read		= psc_debug_read,
 	.write		= psc_debug_write,
-	.llseek		= no_llseek,
 	.release	= psc_debug_release,
+	.unlocked_ioctl = psc_debug_ioctl,
 };
 
 static void psc_chan_rx_callback(struct mbox_client *c, void *msg)
 {
 	struct device *dev = c->dev;
 	struct psc_debug_dev *dbg = container_of(c, struct psc_debug_dev, cl);
-	uint32_t *data = msg;
 
-	dev_dbg(dev, "data: %08x %08x %08x\n", data[0], data[1], data[2]);
+	dev_dbg(dev, "%s\n", __func__);
+
 	memcpy(dbg->rx_msg, msg, MBOX_MSG_LEN);
 
-	atomic_set(&dbg->data_ready, 1);
+	atomic_set(&dbg->data_ready, RX_READY);
 	wake_up_interruptible(&dbg->read_wait);
 }
 
@@ -174,6 +362,7 @@ int psc_debugfs_create(struct platform_device *pdev)
 	dbg->pdev = pdev;
 
 	init_waitqueue_head(&dbg->read_wait);
+	mutex_init(&dbg->lock);
 
 	return 0;
 }
@@ -182,5 +371,6 @@ void psc_debugfs_remove(struct platform_device *pdev)
 {
 	dev_dbg(&pdev->dev, "%s\n", __func__);
 
+	mutex_destroy(&psc_debug.lock);
 	debugfs_remove_recursive(debugfs_root);
 }
