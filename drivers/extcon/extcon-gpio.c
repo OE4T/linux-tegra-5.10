OE4T/linux-tegra-4.9

extcon: gpio: add device tree support for extcon
Add Device tree support for extcon-gpio driver and
add support for default connection state based on
platform if there is no provision of connecting
through gpio.

Bug 200240416
Bug 1935963

Change-Id: I43ef21ebdb9dc23ee6f8aa404de9e06fb744a15c
Signed-off-by: Hyong Bin Kim <hyongbink@nvidia.com>
Reviewed-on: http://git-master/r/1231974
(cherry picked from commit 80ca8c90318fac8866d4624d16cfc2c50ad9103c)
Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
(cherry picked from commit 957fd3a12274736970f375481b79ba676d67f01f)
Reviewed-on: http://git-master/r/1495586
Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
GVS: Gerrit_Virtual_Submit
Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

drivers/extcon/extcon-gpio.c
@@ -25,11 +25,14 @@
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_gpio.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/workqueue.h>
 
 struct gpio_extcon_data {
+	struct device *dev;
 	struct extcon_dev *edev;
 	int irq;
 	struct delayed_work work;
@@ -42,15 +45,22 @@ struct gpio_extcon_data {
 static void gpio_extcon_work(struct work_struct *work)
 {
 	int state;
+	int ret;
 	struct gpio_extcon_data	*data =
 		container_of(to_delayed_work(work), struct gpio_extcon_data,
 			     work);
 
-	state = gpiod_get_value_cansleep(data->id_gpiod);
-	if (data->pdata->gpio_active_low)
-		state = !state;
-
-	extcon_set_state_sync(data->edev, data->pdata->extcon_id, state);
+	if (gpio_is_valid(data->pdata->gpio)) {
+		state = gpiod_get_value_cansleep(data->id_gpiod);
+		if (data->pdata->gpio_active_low)
+			state = !state;
+	} else {
+		state = data->pdata->default_state;
+	}
+	ret = extcon_set_state(data->edev,
+			       data->pdata->supported_cable[0], state);
+	if (ret < 0)
+		dev_err(data->dev, "cable state update failed\n");
 }
 
 static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
@@ -91,40 +101,105 @@ static int gpio_extcon_init(struct device *dev, struct gpio_extcon_data *data)
 	return 0;
 }
 
+static struct gpio_extcon_pdata *of_get_platform_data(
+		struct platform_device *pdev)
+{
+	struct gpio_extcon_pdata *pdata;
+	struct device_node *np = pdev->dev.of_node;
+	int gpio;
+	u32 pval;
+	int ret;
+
+	pdata = devm_kzalloc(&pdev->dev, sizeof(*pdata), GFP_KERNEL);
+	if (!pdata)
+		return ERR_PTR(-ENOMEM);
+
+	ret = of_property_read_string(np, "extcon-gpio,name", &pdata->name);
+	if (ret < 0)
+		pdata->name = np->name;
+
+	ret = of_property_read_u32_array(np,
+		"extcon-gpio,supported-cable", pdata->supported_cable, 2);
+	if (ret)
+		return ERR_PTR(-EINVAL);
+
+	gpio = of_get_named_gpio(np, "gpio", 0);
+	if ((gpio < 0) && (gpio != -ENOENT))
+		return ERR_PTR(gpio);
+
+	if (gpio == -ENOENT)
+		pdata->gpio = -1;
+	else
+		pdata->gpio = gpio;
+
+	if (pdata->gpio < 0)
+		pdata->default_state = of_property_read_bool(np,
+					"extcon-gpio,default-connected");
+
+	ret = of_property_read_u32(np, "extcon-gpio,irq-flags", &pval);
+	if (!ret)
+		pdata->irq_flags = pval;
+	else
+		pdata->irq_flags = IRQF_TRIGGER_RISING |
+						IRQF_TRIGGER_FALLING;
+
+	ret = of_property_read_u32(np, "extcon-gpio,debounce", &pval);
+	if (!ret)
+		pdata->debounce = pval;
+
+	pdata->gpio_active_low = of_property_read_bool(np,
+				"extcon-gpio,connection-state-low");
+
+	return pdata;
+}
+
 static int gpio_extcon_probe(struct platform_device *pdev)
 {
 	struct gpio_extcon_pdata *pdata = dev_get_platdata(&pdev->dev);
 	struct gpio_extcon_data *data;
 	int ret;
 
+	if (!pdata && pdev->dev.of_node) {
+		pdata = of_get_platform_data(pdev);
+		if (IS_ERR(pdata))
+			return PTR_ERR(pdata);
+	}
+
 	if (!pdata)
 		return -EBUSY;
-	if (!pdata->irq_flags || pdata->extcon_id > EXTCON_NONE)
+	if (!pdata->irq_flags)
 		return -EINVAL;
 
 	data = devm_kzalloc(&pdev->dev, sizeof(struct gpio_extcon_data),
 				   GFP_KERNEL);
 	if (!data)
 		return -ENOMEM;
+	data->dev = &pdev->dev;
 	data->pdata = pdata;
 
-	/* Initialize the gpio */
-	ret = gpio_extcon_init(&pdev->dev, data);
-	if (ret < 0)
-		return ret;
-
 	/* Allocate the memory of extcon devie and register extcon device */
-	data->edev = devm_extcon_dev_allocate(&pdev->dev, &pdata->extcon_id);
+	data->edev = devm_extcon_dev_allocate(&pdev->dev,
+					pdata->supported_cable);
 	if (IS_ERR(data->edev)) {
 		dev_err(&pdev->dev, "failed to allocate extcon device\n");
 		return -ENOMEM;
 	}
+
+	data->edev->name = pdata->name;
 
 	ret = devm_extcon_dev_register(&pdev->dev, data->edev);
 	if (ret < 0)
 		return ret;
 
 	INIT_DELAYED_WORK(&data->work, gpio_extcon_work);
+
+	if (!gpio_is_valid(pdata->gpio))
+		goto skip_gpio;
+
+	/* Initialize the gpio */
+	ret = gpio_extcon_init(&pdev->dev, data);
+	if (ret < 0)
+		return ret;
 
 	/*
 	 * Request the interrupt of gpio to detect whether external connector
@@ -136,6 +211,7 @@ static int gpio_extcon_probe(struct platform_device *pdev)
 	if (ret < 0)
 		return ret;
 
+skip_gpio:
 	platform_set_drvdata(pdev, data);
 	/* Perform initial detection */
 	gpio_extcon_work(&data->work.work);
@@ -168,12 +244,19 @@ static int gpio_extcon_resume(struct device *dev)
 
 static SIMPLE_DEV_PM_OPS(gpio_extcon_pm_ops, NULL, gpio_extcon_resume);
 
+static struct of_device_id of_extcon_gpio_tbl[] = {
+	{ .compatible = "extcon-gpio", },
+	{ /* end */ }
+};
+MODULE_DEVICE_TABLE(of, of_extcon_gpio_tbl);
+
 static struct platform_driver gpio_extcon_driver = {
 	.probe		= gpio_extcon_probe,
 	.remove		= gpio_extcon_remove,
 	.driver		= {
 		.name	= "extcon-gpio",
 		.pm	= &gpio_extcon_pm_ops,
+		.of_match_table = of_extcon_gpio_tbl,
 	},
 };
 

OE4T/linux-tegra-5.10

(cherry picked from commit 00bfd772f06d772619ef7b1fa516ff51a66f9280)
Reviewed-on: https://git-master.nvidia.com/r/1767857
(cherry picked from commit 103a488a38bc585f482d3ce5b79e0f4b4ca51f8a)
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2372288
Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
Reviewed-by: Krishna Yarlagadda <kyarlagadda@nvidia.com>
Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: Krishna Yarlagadda <kyarlagadda@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/extcon/extcon-gpio.c
@@ -10,15 +10,19 @@
  */
 
 #include <linux/extcon-provider.h>
+#include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_gpio.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/workqueue.h>
 
+#include "extcon.h"
 /**
  * struct gpio_extcon_data - A simple GPIO-controlled extcon device state container.
  * @edev:		Extcon device.
@@ -32,6 +36,14 @@
  *			while resuming from sleep.
  */
 struct gpio_extcon_data {
+	const char *name;
+	unsigned int supported_cable[2];
+	int gpio;
+	bool gpio_active_low;
+	unsigned long irq_flags;
+	bool default_state;
+
+	struct device *dev;
 	struct extcon_dev *edev;
 	struct delayed_work work;
 	unsigned long debounce_jiffies;
@@ -44,12 +56,22 @@ struct gpio_extcon_data {
 static void gpio_extcon_work(struct work_struct *work)
 {
 	int state;
+	int ret;
 	struct gpio_extcon_data	*data =
 		container_of(to_delayed_work(work), struct gpio_extcon_data,
 			     work);
 
-	state = gpiod_get_value_cansleep(data->gpiod);
-	extcon_set_state_sync(data->edev, data->extcon_id, state);
+	if (gpio_is_valid(data->gpio)) {
+		state = gpiod_get_value_cansleep(data->gpiod);
+		if (data->gpio_active_low)
+			state = !state;
+	} else {
+		state = data->default_state;
+	}
+	ret = extcon_set_state(data->edev,
+			       data->supported_cable[0], state);
+	if (ret < 0)
+		dev_err(data->dev, "cable state update failed\n");
 }
 
 static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
@@ -57,8 +79,55 @@ static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
 	struct gpio_extcon_data *data = dev_id;
 
 	queue_delayed_work(system_power_efficient_wq, &data->work,
-			      data->debounce_jiffies);
+			   data->debounce_jiffies);
 	return IRQ_HANDLED;
+}
+
+static int of_get_platform_data(struct platform_device *pdev,
+				struct gpio_extcon_data *data)
+{
+	struct device_node *np = pdev->dev.of_node;
+	int gpio;
+	u32 pval;
+	int ret;
+
+	ret = of_property_read_string(np, "extcon-gpio,name", &data->name);
+	if (ret < 0)
+		data->name = np->name;
+
+	ret = of_property_read_u32_array(np, "extcon-gpio,supported-cable",
+					 data->supported_cable, 2);
+	if (ret)
+		return -EINVAL;
+
+	gpio = of_get_named_gpio(np, "gpio", 0);
+	if (gpio < 0 && (gpio != -ENOENT))
+		return gpio;
+
+	if (gpio == -ENOENT)
+		data->gpio = -1;
+	else
+		data->gpio = gpio;
+
+	if (data->gpio < 0)
+		data->default_state = of_property_read_bool(np,
+					"extcon-gpio,default-connected");
+
+	ret = of_property_read_u32(np, "extcon-gpio,irq-flags", &pval);
+	if (!ret)
+		data->irq_flags = pval;
+	else
+		data->irq_flags = IRQF_TRIGGER_RISING |
+				  IRQF_TRIGGER_FALLING;
+
+	ret = of_property_read_u32(np, "extcon-gpio,debounce", &pval);
+	if (!ret)
+		data->debounce = pval;
+
+	data->gpio_active_low = of_property_read_bool(np,
+				"extcon-gpio,connection-state-low");
+
+	return 0;
 }
 
 static int gpio_extcon_probe(struct platform_device *pdev)
@@ -72,6 +141,12 @@ static int gpio_extcon_probe(struct platform_device *pdev)
 	data = devm_kzalloc(dev, sizeof(struct gpio_extcon_data), GFP_KERNEL);
 	if (!data)
 		return -ENOMEM;
+
+	if (!data && dev->of_node) {
+		ret = of_get_platform_data(pdev, data);
+		if (ret < 0)
+			return ret;
+	}
 
 	/*
 	 * FIXME: extcon_id represents the unique identifier of external
@@ -102,17 +177,22 @@ static int gpio_extcon_probe(struct platform_device *pdev)
 		irq_flags = IRQF_TRIGGER_RISING;
 
 	/* Allocate the memory of extcon devie and register extcon device */
-	data->edev = devm_extcon_dev_allocate(dev, &data->extcon_id);
+	data->edev = devm_extcon_dev_allocate(&pdev->dev,
+					      data->supported_cable);
 	if (IS_ERR(data->edev)) {
 		dev_err(dev, "failed to allocate extcon device\n");
 		return -ENOMEM;
 	}
 
+	data->edev->name = data->name;
 	ret = devm_extcon_dev_register(dev, data->edev);
 	if (ret < 0)
 		return ret;
 
 	INIT_DELAYED_WORK(&data->work, gpio_extcon_work);
+
+	if (!gpio_is_valid(data->gpio))
+		goto skip_gpio;
 
 	/*
 	 * Request the interrupt of gpio to detect whether external connector
@@ -124,6 +204,7 @@ static int gpio_extcon_probe(struct platform_device *pdev)
 	if (ret < 0)
 		return ret;
 
+skip_gpio:
 	platform_set_drvdata(pdev, data);
 	/* Perform initial detection */
 	gpio_extcon_work(&data->work.work);
@@ -148,7 +229,7 @@ static int gpio_extcon_resume(struct device *dev)
 	data = dev_get_drvdata(dev);
 	if (data->check_on_resume)
 		queue_delayed_work(system_power_efficient_wq,
-			&data->work, data->debounce_jiffies);
+				   &data->work, data->debounce_jiffies);
 
 	return 0;
 }
@@ -156,12 +237,19 @@ static int gpio_extcon_resume(struct device *dev)
 
 static SIMPLE_DEV_PM_OPS(gpio_extcon_pm_ops, NULL, gpio_extcon_resume);
 
+static const struct of_device_id of_extcon_gpio_tbl[] = {
+	{ .compatible = "extcon-gpio", },
+	{ /* end */ }
+};
+MODULE_DEVICE_TABLE(of, of_extcon_gpio_tbl);
+
 static struct platform_driver gpio_extcon_driver = {
 	.probe		= gpio_extcon_probe,
 	.remove		= gpio_extcon_remove,
 	.driver		= {
 		.name	= "extcon-gpio",
 		.pm	= &gpio_extcon_pm_ops,
+		.of_match_table = of_extcon_gpio_tbl,
 	},
 };
 