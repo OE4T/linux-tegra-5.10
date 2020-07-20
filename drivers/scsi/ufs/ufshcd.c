OE4T/linux-tegra-4.9

driver: ufs: add provisioning support
Added UFS provisioning support.
Option -U in flash command will access the functions add in this commit.

Accessed UFS Descriptors and Attributes are
1. Attribute: bBootLunEn, bRefClkFreq
2. Configuration Desc. Header field: bBootEnable, bDescrAccessEn
3. Unit Descriptors

Jira VFF-607

Change-Id: I8f9e7bd64bb945678bc66481a533666de9acef85
Signed-off-by: Andy Sobczyk <asobczyk@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1662483
Reviewed-by: Arul Sekar <aruls@nvidia.com>
Reviewed-by: Michael Jung <kyongsooj@nvidia.com>
Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>
Reviewed-by: Suresh Venkatachalam <skathirampat@nvidia.com>
GVS: Gerrit_Virtual_Submit
Reviewed-by: Sumeet Gupta <sumeetg@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/scsi/ufs/ufshcd.c
@@ -1980,6 +1980,34 @@ int ufshcd_set_refclk_value(struct ufs_hba *hba, u32 *value)
 EXPORT_SYMBOL(ufshcd_set_refclk_value);
 
 /**
+ * ufshcd_get_bootlun_en_value - get programmed bBootLunEn value
+ * @hba: per-adapter instance
+ * @value: variable to store read value
+ *
+ * Get BootLunEn value
+ */
+int ufshcd_get_bootlun_en_value(struct ufs_hba *hba, u32 *value)
+{
+	return ufshcd_query_attr(hba, UPIU_QUERY_OPCODE_READ_ATTR,
+							 QUERY_ATTR_IDN_BOOTLUN_EN, 0, 0, value);
+}
+EXPORT_SYMBOL(ufshcd_get_bootlun_en_value);
+
+/**
+ * ufshcd_set_bootlun_en_value - Write bBootLunEn value
+ * @hba: per-adapter instance
+ * @value: value to be written
+ *
+ * Set Refclkfreq value
+ */
+int ufshcd_set_bootlun_en_value(struct ufs_hba *hba, u32 *value)
+{
+	return ufshcd_query_attr(hba, UPIU_QUERY_OPCODE_WRITE_ATTR,
+							 QUERY_ATTR_IDN_BOOTLUN_EN, 0, 0, value);
+}
+EXPORT_SYMBOL(ufshcd_set_bootlun_en_value);
+
+/**
  * ufshcd_get_config_desc_lock - Read configuration descriptor lock
  * @hba: per-adapter instance
  * @value: varibale to store read value

OE4T/linux-tegra-5.10

Bug 200603932
(cherry picked from commit 20cb41422b80dae7337484d27301105e0779266d)
Reviewed-on: https://git-master.nvidia.com/r/1787971
(cherry picked from commit 4fc3c3426dc67ab301c11f98ed039429441364bc)
Signed-off-by: Mallikarjun Kasoju <mkasoju@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2371965
Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

drivers/scsi/ufs/ufshcd.c
@@ -2948,6 +2948,34 @@ int ufshcd_set_refclk_value(struct ufs_hba *hba, u32 *value)
 EXPORT_SYMBOL(ufshcd_set_refclk_value);
 
 /**
+ * ufshcd_get_bootlun_en_value - get programmed bBootLunEn value
+ * @hba: per-adapter instance
+ * @value: variable to store read value
+ *
+ * Get BootLunEn value
+ */
+int ufshcd_get_bootlun_en_value(struct ufs_hba *hba, u32 *value)
+{
+	return ufshcd_query_attr(hba, UPIU_QUERY_OPCODE_READ_ATTR,
+							 QUERY_ATTR_IDN_BOOT_LU_EN, 0, 0, value);
+}
+EXPORT_SYMBOL(ufshcd_get_bootlun_en_value);
+
+/**
+ * ufshcd_set_bootlun_en_value - Write bBootLunEn value
+ * @hba: per-adapter instance
+ * @value: value to be written
+ *
+ * Set Refclkfreq value
+ */
+int ufshcd_set_bootlun_en_value(struct ufs_hba *hba, u32 *value)
+{
+	return ufshcd_query_attr(hba, UPIU_QUERY_OPCODE_WRITE_ATTR,
+							 QUERY_ATTR_IDN_BOOT_LU_EN, 0, 0, value);
+}
+EXPORT_SYMBOL(ufshcd_set_bootlun_en_value);
+
+/**
  * ufshcd_get_config_desc_lock - Read configuration descriptor lock
  * @hba: per-adapter instance
  * @value: varibale to store read value