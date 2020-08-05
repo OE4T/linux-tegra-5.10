155bca6056aff901d1fcfe5b71fed7c3f721aae8
Naveen Kumar S <nkumars@nvidia.com>
2020-08-05 12:51:40 +0530

    video: tegra: host: return err if host1x not ready
    dc_common driver calls into host1x for channel mapping.
    If host1x probe is not yet done, added support to return
    EPROBE_DEFER which will inform clients like dc_common to
    defer their probe until host1x is ready.
    
    bug 200601926
    
    Change-Id: I94eb28bac0efe2a791247020c52546e2f925ad6d
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2394587
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Mikko Perttunen <mperttunen@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/video/tegra/host/nvhost_channel.c
+++ drivers/video/tegra/host/nvhost_channel.c
@@ -235,6 +235,8 @@ int nvhost_channel_map_with_vm(struct nvhost_device_data *pdata,
 	}
 
 	host = nvhost_get_host(pdata->pdev);
+	if (!host)
+		return -EPROBE_DEFER;
 	max_channels = nvhost_channel_nb_channels(host);
 
 	mutex_lock(&host->ch_alloc_mutex);
