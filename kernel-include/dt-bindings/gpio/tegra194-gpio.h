f817fe13df6fd094986ae76f15133db13c07acb9
Ronit Halder <rhalder@nvidia.com>
2020-08-08 21:23:03 +0530

    soc: t23x: kernel-include: add back removed comment
    - add comments missed in https://git-master.nvidia.com/r/2387107
    
    ESLC-5105
    
    Change-Id: I3448f6c9cd37090a6fd94d6a8eeabcc7884df98d
    Signed-off-by: Ronit Halder <rhalder@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/soc/t23x/+/2396169
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Vipin Kumar <vipink@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- kernel-include/dt-bindings/gpio/tegra194-gpio.h
+++ kernel-include/dt-bindings/gpio/tegra194-gpio.h
@@ -15,7 +15,7 @@
  */
 
 /*
- * This header provides constants for binding nvidia,tegra194-gpio.
+ * This header provides constants for binding nvidia,tegra194-gpio*.
  *
  * The first cell in Tegra's GPIO specifier is the GPIO ID. The macros below
  * provide names for this.
