f4e170ea14b46be21495eb98784a1d5c6f4cee22
Timo Alho <talho@nvidia.com>
2020-08-26 09:41:05 +0300

    Revert "firmware: tegra: Allow for DRAM MRQ carveouts"
    This reverts commit df98dba35d7e9c70c5f1424f39acbab3969d1d49.
    
    Due to recent POR change, the IPC communication in t234 will remain in
    SysRAM carveout. Revert pre-silicon changes made to add DRAM carveout
    support.
    
    Bug 200496257
    Bug 3098403
    
    Change-Id: Ib0a32f6166cc71297dad15c63c7458ebdfa866b1
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2403968
    Reviewed-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Reviewed-by: Timo Alho <talho@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Timo Alho <talho@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/firmware/tegra/bpmp.c
+++ drivers/firmware/tegra/bpmp.c
@@ -732,8 +732,6 @@ static int tegra_bpmp_probe(struct platform_device *pdev)
 	if (!bpmp->threaded_channels)
 		return -ENOMEM;
 
-	platform_set_drvdata(pdev, bpmp);
-
 	err = bpmp->soc->ops->init(bpmp);
 	if (err < 0)
 		return err;
@@ -756,6 +754,8 @@ static int tegra_bpmp_probe(struct platform_device *pdev)
 	}
 
 	dev_info(&pdev->dev, "firmware: %.*s\n", (int)sizeof(tag), tag);
+
+	platform_set_drvdata(pdev, bpmp);
 
 	err = of_platform_default_populate(pdev->dev.of_node, NULL, &pdev->dev);
 	if (err < 0)
@@ -809,8 +809,7 @@ static const struct dev_pm_ops tegra_bpmp_pm_ops = {
 };
 
 #if IS_ENABLED(CONFIG_ARCH_TEGRA_186_SOC) || \
-    IS_ENABLED(CONFIG_ARCH_TEGRA_194_SOC) || \
-    IS_ENABLED(CONFIG_ARCH_TEGRA_234_SOC)
+    IS_ENABLED(CONFIG_ARCH_TEGRA_194_SOC)
 static const struct tegra_bpmp_soc tegra186_soc = {
 	.channels = {
 		.cpu_tx = {
@@ -857,8 +856,7 @@ static const struct tegra_bpmp_soc tegra210_soc = {
 
 static const struct of_device_id tegra_bpmp_match[] = {
 #if IS_ENABLED(CONFIG_ARCH_TEGRA_186_SOC) || \
-    IS_ENABLED(CONFIG_ARCH_TEGRA_194_SOC) || \
-    IS_ENABLED(CONFIG_ARCH_TEGRA_234_SOC)
+    IS_ENABLED(CONFIG_ARCH_TEGRA_194_SOC)
 	{ .compatible = "nvidia,tegra186-bpmp", .data = &tegra186_soc },
 #endif
 #if IS_ENABLED(CONFIG_ARCH_TEGRA_210_SOC)
