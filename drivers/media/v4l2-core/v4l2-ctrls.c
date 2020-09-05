OE4T/linux-tegra-4.9

v4l2: set to USER_DS before copy_to_user
When calling from 32-bit user mode to 64-bit kernel,
enable argument validity checking in V4L2 core by
setting the USER_DS state. Once the given control value
is copied back to the caller, restore back to original
state.

Bug 2001016

Change-Id: I01fc84acb4704011d88b3154732e943f0d18bec2
Signed-off-by: Frank Chen <frankc@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1582813
GVS: Gerrit_Virtual_Submit
Reviewed-by: Ruicheng Dai <rdai@nvidia.com>
Reviewed-by: Bhanu Murthy V <bmurthyv@nvidia.com>

drivers/media/v4l2-core/v4l2-ctrls.c
@@ -1472,10 +1472,16 @@ static int ptr_to_user(struct v4l2_ext_control *c,
 		       union v4l2_ctrl_ptr ptr)
 {
 	u32 len;
+	long err = 0;
+	mm_segment_t old_fs = get_fs();
 
-	if (ctrl->is_ptr && !ctrl->is_string)
-		return copy_to_user(c->ptr, ptr.p, c->size) ?
-		       -EFAULT : 0;
+	if (ctrl->is_ptr && !ctrl->is_string) {
+		set_fs(USER_DS);
+		if (copy_to_user(c->ptr, ptr.p, c->size))
+			err = -EFAULT;
+		set_fs(old_fs);
+		return err;
+	}
 
 	switch (ctrl->type) {
 	case V4L2_CTRL_TYPE_STRING:
@@ -1484,8 +1490,11 @@ static int ptr_to_user(struct v4l2_ext_control *c,
 			c->size = ctrl->elem_size;
 			return -ENOSPC;
 		}
-		return copy_to_user(c->string, ptr.p_char, len + 1) ?
-		       -EFAULT : 0;
+		set_fs(USER_DS);
+		if (copy_to_user(c->string, ptr.p_char, len + 1))
+			err = -EFAULT;
+		set_fs(old_fs);
+		return err;
 	case V4L2_CTRL_TYPE_INTEGER64:
 		c->value64 = *ptr.p_s64;
 		break;

OE4T/linux-tegra-5.10

(cherry picked from commit c313bbaf80d90dfad069cab318a1a9186d1760ea)
Reviewed-on: https://git-master.nvidia.com/r/1760401
(cherry picked from commit 7f1977fad1ce9757d99ec929a7438a47fabdb690)
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2408254
Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
Reviewed-by: Ashwini Purkar <apurkar@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/media/v4l2-core/v4l2-ctrls.c
@@ -1972,10 +1972,16 @@ static int ptr_to_user(struct v4l2_ext_control *c,
 		       union v4l2_ctrl_ptr ptr)
 {
 	u32 len;
+	long err = 0;
+	mm_segment_t old_fs = get_fs();
 
-	if (ctrl->is_ptr && !ctrl->is_string)
-		return copy_to_user(c->ptr, ptr.p_const, c->size) ?
-		       -EFAULT : 0;
+	if (ctrl->is_ptr && !ctrl->is_string) {
+		set_fs(USER_DS);
+		if (copy_to_user(c->ptr, ptr.p_const, c->size))
+			err = -EFAULT;
+		set_fs(old_fs);
+		return err;
+	}
 
 	switch (ctrl->type) {
 	case V4L2_CTRL_TYPE_STRING:
@@ -1984,8 +1990,11 @@ static int ptr_to_user(struct v4l2_ext_control *c,
 			c->size = ctrl->elem_size;
 			return -ENOSPC;
 		}
-		return copy_to_user(c->string, ptr.p_char, len + 1) ?
-		       -EFAULT : 0;
+		set_fs(USER_DS);
+		if (copy_to_user(c->string, ptr.p_char, len + 1))
+			err = -EFAULT;
+		set_fs(old_fs);
+		return err;
 	case V4L2_CTRL_TYPE_INTEGER64:
 		c->value64 = *ptr.p_s64;
 		break;