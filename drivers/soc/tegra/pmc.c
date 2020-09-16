madisongh/linux-tegra

soc/tegra: pmc: Add support for BDSDMEM IO pad handling
The sequence for enabling/disabling NO_IOPOWER depends on
type of pads whether it is BDSDMEM_CFC or Non BDSDMEM_CFC.

Make the sequence as per power sequence documents.

Rail OFF/ON sequence for Non BDSDMEM_CFC* pads
   Turning OFF the Rail
      - Make E_NO_IOPWR field of the pad to be at Logic 1 by
        programming appropriate bit correspond to the rail in
        PMC_IMPL_NO_IOPOWER_0
      - Bring down the rail
   Turning ON the Rail
      - Bring up the rail
       - Make E_NO_IOPWR field of the pad to be at Logic 0 by
         programming appropriate bit correspond to the rail in
         PMC_IMPL_NO_IOPOWER_0

Rail OFF/ON sequence for BDSDMEM_CFC* pads
   List of rails affected in T186 are
      VDDIO_AO_HV, VDDIO_AUDIO_HV, VDDIO_DMIC_HV,
      VDDIO_SDMMC1_HV, VDDIO_SDMMC2_HV, VDDIO_SDMMC3_HV,
   Turning OFF the Rail
      - Bring Down the respective I/O rail VDD_IO =0 followed
        by the respective BIAS rail =0
      - Make E_NO_IOPWR field of the pad to be at Logic 1 by
        programming appropriate bit correspond to the rail in
        PMC_IMPL_NO_IOPOWER_0

   Turning ON the Rail
      - Make E_NO_IOPWR field of the pad to be at Logic 0 by
        programming appropriate bit correspond to the rail in
        PMC_IMPL_NO_IOPOWER_0
      - Bring up the respective BIAS rail to 1.8V , followed
        by theI/O rail i.e. VDDIO_ to correct level i.e. 1.8V or
        3.3V

Based on change commit 215781a7b52a9ac35fa0acd6e8bd484db5ed950b

bug 200259694

Change-Id: I8cc4c66e5bcc57f068ea3c25aa03062ba3179493
Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
Reviewed-on: http://git-master/r/1282187
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/soc/tegra/pmc.c
@@ -478,6 +478,7 @@ struct tegra_pmc_io_pad_soc {
 	int dpd_status_reg;
 	int dpd_timer_reg;
 	int dpd_sample_reg;
+	bool bdsdmem_cfc;
 	int io_pad_pwr_det_enable_reg;
 	int io_pad_pwr_det_val_reg;
 	int pad_uv_0;
@@ -3614,6 +3615,7 @@ static const u8 tegra210_cpu_powergates[] = {
 		.dpd_status_reg = TEGRA_PMC_IO_##_reg##_STATUS,	\
 		.dpd_timer_reg = TEGRA_PMC_SEL_DPD_TIM,		\
 		.dpd_sample_reg = TEGRA_PMC_IO_DPD_SAMPLE,	\
+		.bdsdmem_cfc = false,				\
 		.io_pad_pwr_det_enable_reg = TEGRA_PMC_PWR_DET_ENABLE, \
 		.io_pad_pwr_det_val_reg = TEGRA_PMC_PWR_DET_VAL, \
 		.pad_uv_0 = TEGRA_IO_PAD_VOLTAGE_1800000UV,	\
@@ -3948,8 +3950,10 @@ static int tegra_pmc_io_rail_change_notify_cb(struct notifier_block *nb,
 	const struct tegra_pmc_io_pad_soc *pad;
 	unsigned long flags;
 
-	if (!((event & REGULATOR_EVENT_POST_ENABLE) ||
-	      (event & REGULATOR_EVENT_PRE_DISABLE)))
+	if (!(event & (REGULATOR_EVENT_POST_ENABLE |
+		       REGULATOR_EVENT_PRE_DISABLE |
+		       REGULATOR_EVENT_PRE_ENABLE |
+		       REGULATOR_EVENT_DISABLE)))
 		return NOTIFY_OK;
 
 	tip_reg = container_of(nb, struct tegra_io_pad_regulator, nb);
@@ -3957,11 +3961,19 @@ static int tegra_pmc_io_rail_change_notify_cb(struct notifier_block *nb,
 
 	spin_lock_irqsave(&pwr_lock, flags);
 
-	if (event & REGULATOR_EVENT_POST_ENABLE)
-		pmc_iopower_enable(pad);
+	if (pad->bdsdmem_cfc) {
+		if (event & REGULATOR_EVENT_PRE_ENABLE)
+			pmc_iopower_enable(pad);
 
-	if (event & REGULATOR_EVENT_PRE_DISABLE)
-		pmc_iopower_disable(pad);
+		if (event & REGULATOR_EVENT_DISABLE)
+			pmc_iopower_disable(pad);
+	} else {
+		if (event & REGULATOR_EVENT_POST_ENABLE)
+			pmc_iopower_enable(pad);
+
+		if (event & REGULATOR_EVENT_PRE_DISABLE)
+			pmc_iopower_disable(pad);
+	}
 
 	dev_dbg(pmc->dev, "tegra-iopower: %s: event 0x%08lx state: %d\n",
 		pad->name, event, pmc_iopower_get_status(pad));

OE4T/linux-tegra-5.10

Make the sequence as per power sequence documents
Bug 200259694
Bug 2850798
(cherry picked from commit c162f48d6b12baaa87367d58eeb9b4b19a018b08)
Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1719529
(cherry picked from commit 9b44250362a2d09d1f25bfa58860c88d799b32b1)
Signed-off-by: Petlozu Pravareshwar <petlozup@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2410983

drivers/soc/tegra/pmc.c
@@ -420,6 +420,7 @@ struct tegra_io_pad_soc {
 	const char *name;
 	unsigned int io_power;
 	enum tegra_dpd_reg reg_index;
+	bool bdsdmem_cfc;
 };
 
 struct tegra_pmc_regs {
@@ -3800,6 +3801,7 @@ static const u8 tegra124_cpu_powergates[] = {
 		.voltage = (_voltage),				\
 		.name	= (_name),				\
 		.io_power	= (_iopower),			\
+		.bdsdmem_cfc	= (false),			\
 	})
 
 #define TEGRA_IO_PIN_DESC(_id, _dpd, _voltage, _name, _iopower)	\
@@ -4585,8 +4587,9 @@ static int tegra_pmc_io_rail_change_notify_cb(struct notifier_block *nb,
 	const struct tegra_io_pad_soc *pad;
 	unsigned long flags;
 
-	if (!((event & REGULATOR_EVENT_ENABLE) ||
-	      (event & REGULATOR_EVENT_PRE_DISABLE)))
+	if (!(event & (REGULATOR_EVENT_ENABLE |
+		       REGULATOR_EVENT_PRE_DISABLE |
+		       REGULATOR_EVENT_DISABLE)))
 		return NOTIFY_OK;
 
 	tip_reg = container_of(nb, struct tegra_io_pad_regulator, nb);
@@ -4594,11 +4597,19 @@ static int tegra_pmc_io_rail_change_notify_cb(struct notifier_block *nb,
 
 	spin_lock_irqsave(&pwr_lock, flags);
 
-	if (event & REGULATOR_EVENT_ENABLE)
-		pmc_iopower_enable(pad);
+	if (pad->bdsdmem_cfc) {
+		if (event & REGULATOR_EVENT_ENABLE)
+			pmc_iopower_enable(pad);
 
-	if (event & REGULATOR_EVENT_PRE_DISABLE)
-		pmc_iopower_disable(pad);
+		if (event & REGULATOR_EVENT_DISABLE)
+			pmc_iopower_disable(pad);
+	} else {
+		if (event & REGULATOR_EVENT_ENABLE)
+			pmc_iopower_enable(pad);
+
+		if (event & REGULATOR_EVENT_PRE_DISABLE)
+			pmc_iopower_disable(pad);
+	}
 
 	dev_dbg(pmc->dev, "tegra-iopower: %s: event 0x%08lx state: %d\n",
 		pad->name, event, pmc_iopower_get_status(pad));