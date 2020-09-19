OE4T/linux-tegra-4.9

t19x: mccoy: Add overlays for Jetson Clara-AGX
Following device-tree overlays are added for Jetson Clara-AGX
platform:

  1. ADAFRUIT SPH0645LM4H (I2S MEMS Microphone)
  2. FE-PI Audio (SGTL5000 Audio Codec)
  3. Jetson 40-pin Expansion Header
  4. Respeaker 4 Mic Array Card

40-pin Expansion Header overlay above is derived from the DTSI for
Galen (Xavier AGX). It is for use with the Jetson-IO python tool.
Remaining overlays may be applied using either Jetson-IO tool, or
directly on the platform base DTB file as below:

  $ fdtoverlay -i <base-dtb> -o <out-dtb> <overlay-dtbo>

Bug 2829904

Change-Id: I2185ac9cd8d7fbb8fd4fe19ba75544772684a3f8
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/mccoy-dts/+/2293997
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Jonathan Hunter <jonathanh@nvidia.com>
Reviewed-by: Viswanath L <viswanathl@nvidia.com>
Reviewed-by: Sharad Gupta <sharadg@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
GVS: Gerrit_Virtual_Submit
Tested-by: Viswanath L <viswanathl@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

Makefile
@@ -7,6 +7,10 @@ makefile-path := platform/t19x/mccoy/kernel-dts
 dts-include += platform/t19x/
 
 dtb-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000.dtb
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-hdr40.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-adafruit-sph0645lm4h.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-fe-pi-audio.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-array.dtbo
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))

OE4T/linux-tegra-5.10

(cherry picked from commit 5d49b8012424dd64d379ea379b8d8823a177871d)
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/mccoy-dts/+/2406578
Reviewed-by: automaticguardword <automaticguardword@nvidia.com>

Makefile
@@ -12,6 +12,10 @@ BUILD_ENABLE=y
 endif
 
 dtb-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000.dtb
+dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-hdr40.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-adafruit-sph0645lm4h.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-fe-pi-audio.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-array.dtbo
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))