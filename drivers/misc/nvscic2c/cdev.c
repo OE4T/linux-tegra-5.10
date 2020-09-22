9e04e6894530a81ff9f69c587d2c1c172e32bf25
Deepak Badgaiyan <dbadgaiyan@nvidia.com>
2020-09-23 00:41:39 +0530

    misc: nvscic2c: Modify nvscic2c module dependency
     + NvSciC2C module on Tegra is dependent on IVC.
       Make NvSciC2C module dependent TEGRA_HV_MANAGER.
       This will make sure that NvSciC2C mdoule is only
       enable when NVIDIA hypervisor management driver is enabled.
    
     + Include <linux/mod_devicetable.h> to include definition for
       "of_device_id"
    
    Jira C2C-345
    
    Change-Id: Ia9db58e38a71025c5446780c9d109e253dda6891
    Signed-off-by: Deepak Badgaiyan <dbadgaiyan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2418130
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Arihant Jejani <ajejani@nvidia.com>
    Reviewed-by: Sumeet Gupta <sumeetg@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/misc/nvscic2c/cdev.c
+++ drivers/misc/nvscic2c/cdev.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.	All rights reserved.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.	All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -36,6 +36,7 @@
 #include <linux/platform_device.h>
 #include <asm/io.h>
 #include <linux/string.h>
+#include <linux/mod_devicetable.h>
 
 #include "cdev.h"
 
