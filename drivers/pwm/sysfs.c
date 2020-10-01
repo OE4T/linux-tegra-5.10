28d74982e426b6e41b1f6d07deb237bf6fa6d324
Prathamesh Shete <pshete@nvidia.com>
2020-10-01 16:26:28 +0530

    pwm: core: add support for ramp and double pulse mode
    Add support to configure the ramp up and down time of pulse
    which is required for the soft PWM signal.
    
    Also add support to set the double pulse mode and their periods.
    With this, there is pair of the pulses with period of double_period.
    
    Change-Id: I176d39f4678f33c0448db13a0982c9bd77913559
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/1463821
    (cherry picked from commit a9fcd95720bec6a64923aa904ef50aa35e9e8c8e)
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1763868
    (cherry picked from commit 3646a3137552066d94d62d1f0c32f8c8eae4d8fb)
    Signed-off-by: Prathamesh Shete <pshete@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2412177
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/pwm/sysfs.c
+++ drivers/pwm/sysfs.c
@@ -215,6 +215,76 @@ static ssize_t capture_show(struct device *child,
 	return sprintf(buf, "%u %u\n", result.period, result.duty_cycle);
 }
 
+static ssize_t ramp_time_show(struct device *child,
+			      struct device_attribute *attr,
+			      char *buf)
+{
+	const struct pwm_device *pwm = child_to_pwm_device(child);
+	struct pwm_state state;
+
+	pwm_get_state(pwm, &state);
+
+	return sprintf(buf, "%u\n", state.ramp_time);
+}
+
+static ssize_t ramp_time_store(struct device *child,
+			       struct device_attribute *attr,
+			       const char *buf, size_t size)
+{
+	struct pwm_export *export = child_to_pwm_export(child);
+	struct pwm_device *pwm = export->pwm;
+	struct pwm_state state;
+	unsigned int val;
+	int ret;
+
+	ret = kstrtouint(buf, 0, &val);
+	if (ret)
+		return ret;
+
+	mutex_lock(&export->lock);
+	pwm_get_state(pwm, &state);
+	state.ramp_time = val;
+	ret = pwm_apply_state(pwm, &state);
+	mutex_unlock(&export->lock);
+
+	return ret ? : size;
+}
+
+static ssize_t double_period_show(struct device *child,
+				  struct device_attribute *attr,
+				  char *buf)
+{
+	const struct pwm_device *pwm = child_to_pwm_device(child);
+	struct pwm_state state;
+
+	pwm_get_state(pwm, &state);
+
+	return sprintf(buf, "%u\n", state.double_period);
+}
+
+static ssize_t double_period_store(struct device *child,
+				   struct device_attribute *attr,
+				   const char *buf, size_t size)
+{
+	struct pwm_export *export = child_to_pwm_export(child);
+	struct pwm_device *pwm = export->pwm;
+	struct pwm_state state;
+	unsigned int val;
+	int ret;
+
+	ret = kstrtouint(buf, 0, &val);
+	if (ret)
+		return ret;
+
+	mutex_lock(&export->lock);
+	pwm_get_state(pwm, &state);
+	state.double_period = val;
+	ret = pwm_apply_state(pwm, &state);
+	mutex_unlock(&export->lock);
+
+	return ret ? : size;
+}
+
 static ssize_t capture_window_length_show(struct device *child,
 					  struct device_attribute *attr,
 					  char *buf)
@@ -264,6 +334,8 @@ static DEVICE_ATTR_RW(duty_cycle);
 static DEVICE_ATTR_RW(enable);
 static DEVICE_ATTR_RW(polarity);
 static DEVICE_ATTR_RO(capture);
+static DEVICE_ATTR_RW(ramp_time);
+static DEVICE_ATTR_RW(double_period);
 static DEVICE_ATTR_RW(capture_window_length);
 static DEVICE_ATTR_RO(rpm);
 
@@ -273,6 +345,8 @@ static struct attribute *pwm_attrs[] = {
 	&dev_attr_enable.attr,
 	&dev_attr_polarity.attr,
 	&dev_attr_capture.attr,
+	&dev_attr_ramp_time.attr,
+	&dev_attr_double_period.attr,
 	&dev_attr_capture_window_length.attr,
 	&dev_attr_rpm.attr,
 	NULL
