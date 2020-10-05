af6d4c3f52d14b852fbc876de8e20153a2f9ca07
Ivan Raul Guadarrama <iguadarrama@nvidia.com>
2020-10-05 15:00:57 +0300

    video:tegra:host: sec_intf_crc_ctrl not silicon
    Set the enable bit for the sec_intf_crc_ctrl register for all
    platforms but silicon.
    
    Bug 200663377
    Bug 200619941
    Bug 3128211
    Jira TGFXCHIPS-109
    
    Change-Id: I79a169385dc67517711d759228e69e3dce94ad91
    Signed-off-by: Ivan Raul Guadarrama <iguadarrama@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2423541
    Reviewed-by: Mikko Perttunen <mperttunen@nvidia.com>
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Antoine Chauveau <achauveau@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/video/tegra/host/flcn/flcn.c
+++ drivers/video/tegra/host/flcn/flcn.c
@@ -523,7 +523,7 @@ int nvhost_flcn_finalize_poweron(struct platform_device *pdev)
 	nvhost_flcn_ctxtsw_init(pdev);
 	err = nvhost_flcn_start(pdev, 0);
 
-	if (tegra_platform_is_sim())
+	if (!tegra_platform_is_silicon())
 		host1x_writel(pdev, sec_intf_crc_ctrl_r(), 1u);
 
 	return err;
