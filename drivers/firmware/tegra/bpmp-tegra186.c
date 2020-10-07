ff3fdfd0576570178e060572d9092d6c9a2fbb72
Stefan Kristiansson <stefank@nvidia.com>
2020-10-07 11:14:16 +0300

    firmware: tegra: add usleep_range() to busy loop in channel reset
    On VDK, when busy looping waiting for bpmp to respond, CCPLEX simulation
    will steal simulation cycles from BPMP, making bpmp boot extremely slow.
    Adding an usleep_range() will allow BPMP more runtime, improving overall
    boot time.
    
    Bug 3145019
    
    Change-Id: Id27f0db9ac750b510953b297b408d117090e6918
    Signed-off-by: Stefan Kristiansson <stefank@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2424619
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Timo Alho <talho@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/firmware/tegra/bpmp-tegra186.c
+++ drivers/firmware/tegra/bpmp-tegra186.c
@@ -3,12 +3,14 @@
  * Copyright (c) 2018, NVIDIA CORPORATION.
  */
 
+#include <linux/delay.h>
 #include <linux/genalloc.h>
 #include <linux/mailbox_client.h>
 #include <linux/platform_device.h>
 
 #include <soc/tegra/bpmp.h>
 #include <soc/tegra/bpmp-abi.h>
+#include <soc/tegra/fuse.h>
 #include <soc/tegra/ivc.h>
 
 #include "bpmp-private.h"
@@ -144,8 +146,10 @@ static void tegra186_bpmp_channel_reset(struct tegra_bpmp_channel *channel)
 	tegra_ivc_reset(channel->ivc);
 
 	/* sync the channel state with BPMP */
-	while (tegra_ivc_notified(channel->ivc))
-		;
+	while (tegra_ivc_notified(channel->ivc)) {
+		if (tegra_platform_is_vdk())
+			usleep_range(100, 500);
+	}
 }
 
 static void tegra186_bpmp_channel_cleanup(struct tegra_bpmp_channel *channel)
