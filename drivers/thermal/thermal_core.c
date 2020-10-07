918d8fe2c27bfe301929c51e1f547a7fbe8e3cdc
Bibek Basu <bbasu@nvidia.com>
2020-10-07 03:31:07 -0700

    Revert "thermal: Fix possible null dereference"
    This reverts commit 787f4529a1596b489f7ce6a18e6a7494c410eccb.
    
    Bug 2125602
    
    Change-Id: Ia5ce65aca640addbb78a884b1ab9651eb27d68fd
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2424641
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Ankit Patel (SW-TEGRA) <anpatel@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Bibek Basu <bbasu@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1462,19 +1462,8 @@ thermal_zone_device_register(const char *type, int trips, u64 mask,
 	/* Update 'this' zone's governor information */
 	mutex_lock(&thermal_governor_lock);
 
-	if (tz->tzp) {
+	if (tz->tzp)
 		governor = thermal_find_governor(tz->tzp->governor_name);
-		/*
-		 * This situation can arise if a governor is not enabled from a config
-		 * file but is used in the device tree
-		 */
-		if (!governor) {
-			dev_info(&tz->device,
-					"%s gov not in list. using default\n",
-					tz->tzp->governor_name);
-			governor = def_governor;
-		}
-	}
 	else
 		governor = def_governor;
 
