madisongh/linux-tegra

clk: tegra: Enable misc bit
Change-Id: I3d6ec93ef2296c335b8c9b1b93d251cf17735a6a
Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
Reviewed-on: http://git-master/r/1265074
Reviewed-by: Automatic_Commit_Validation_User
GVS: Gerrit_Virtual_Submit
Reviewed-by: Jon Mayo <jmayo@nvidia.com>

drivers/clk/tegra/clk-tegra210.c
@@ -3978,6 +3978,10 @@ static void __init tegra210_clock_init(struct device_node *np)
 	value &= ~BIT(25);
 	clk_writel(value, clk_base + PLLD_BASE);
 
+	value = clk_readl(clk_base + MISC_CLK_ENB);
+	value |= BIT(28);
+	clk_writel(value, clk_base + MISC_CLK_ENB);
+
 	tegra_clk_apply_init_table = tegra210_clock_apply_init_table;
 
 	tegra_super_clk_gen5_init(clk_base, pmc_base, tegra210_clks,

OE4T/linux-tegra-5.10

Change-Id: I495d3c0aa37de7f246958cf8d1308b423306817d
Reviewed-on: https://git-master.nvidia.com/r/1551240
(cherry picked from commit 1e684f5927766090169165486d939e541c0d309c)
Signed-off-by: Joseph Lo <josephl@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1735532
(cherry picked from commit b20f3a617290d25dc4ab598723fd1411d44274e1)
[nicolinc: replaced obsolete clk_readl/_writel() with readl/writel()]
Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2427519
Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
Reviewed-by: Sachin Nikam <snikam@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/clk/tegra/clk-tegra210.c
@@ -260,6 +260,8 @@
 #define TEGRA210_DISPA_BASE 0x54200000
 #define TEGRA210_VIC_BASE  0x54340000
 
+#define MISC_CLK_ENB 0x48
+
 /*
  * SDM fractional divisor is 16-bit 2's complement signed number within
  * (-2^12 ... 2^12-1) range. Represented in PLL data structure as unsigned
@@ -3785,6 +3787,10 @@ static void __init tegra210_clock_init(struct device_node *np)
 	value = readl(clk_base + PLLD_BASE);
 	value &= ~BIT(25);
 	writel(value, clk_base + PLLD_BASE);
+
+	value = readl(clk_base + MISC_CLK_ENB);
+	value |= BIT(28);
+	writel(value, clk_base + MISC_CLK_ENB);
 
 	tegra_clk_apply_init_table = tegra210_clock_apply_init_table;
 