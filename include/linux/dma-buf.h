madisongh/linux-tegra

dma-buf: Allow disabling deferred unmapping per device
Use devres framework to allocate a field per device.
If this field is present, disable deferred unmapping.

Do not allow disabling deferred unmapping after dma-buf is
initialized. This way there is no need to handle the
various races that would otherwise have to be handled.

Also, once disabled, do not allow enabling deferred unmapping
again.

bug 1808360

Change-Id: I45fae3389e3f5bc38597284273aab427e8f52e1c
Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
Reviewed-on: http://git-master/r/1220646
GVS: Gerrit_Virtual_Submit
Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
Reviewed-by: Sachin Nikam <snikam@nvidia.com>

include/linux/dma-buf.h
@@ -256,4 +256,5 @@ void dma_buf_vunmap(struct dma_buf *, void *vaddr);
 int dma_buf_debugfs_create_file(const char *name,
 				int (*write)(struct seq_file *));
 bool dmabuf_is_nvmap(struct dma_buf *dmabuf);
+extern int dma_buf_disable_lazy_unmapping(struct device *device);
 #endif /* __DMA_BUF_H__ */

OE4T/linux-tegra-5.10

Bug 1808360
Bug 200380854
Bug 200632395
It clubs the commit: https://git-master.nvidia.com/r/#/c/linux-4.14/+/1697563/
dma-buf: fix slowness issue in dmabuf_can_defer_unmap API
Reviewed-on: http://git-master/r/1297728
(cherry picked from commit a7f6711cad628d44135060d1c6f98cbb523fcf8b)
Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1697559
(cherry picked from commit b82c994731637a692a87d7d5035c948b4eabb411)
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2443849
Reviewed-by: Puneet Saxena <puneets@nvidia.com>
Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: Puneet Saxena <puneets@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

include/linux/dma-buf.h
@@ -519,4 +519,5 @@ int dma_buf_mmap(struct dma_buf *, struct vm_area_struct *,
 		 unsigned long);
 void *dma_buf_vmap(struct dma_buf *);
 void dma_buf_vunmap(struct dma_buf *, void *vaddr);
+extern int dma_buf_disable_defer_unmapping(struct device *device);
 #endif /* __DMA_BUF_H__ */