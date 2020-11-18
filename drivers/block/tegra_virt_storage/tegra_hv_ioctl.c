OE4T/linux-tegra-4.9

Virtual Storage: Add support for UFS ioctls
Add support for UFS device management IOCTLs for the virtual storage
driver.

Bug 2776093

Change-Id: Iaa9f720743a4cc83f96b33de923675e0ade1da5c
Signed-off-by: Hardik Tushar Shah <hardikts@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2299240
Reviewed-by: Phoenix Jung <pjung@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
GVS: Gerrit_Virtual_Submit

drivers/block/tegra_virt_storage/tegra_hv_ioctl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2018, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2020, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -21,6 +21,7 @@
 #include <asm-generic/bug.h>
 #include <linux/slab.h>   /* kmalloc() */
 #include <scsi/scsi.h>
+#include <uapi/scsi/ufs/ioctl.h>
 #include <scsi/sg.h>
 #include <linux/mmc/ioctl.h>
 #include <linux/version.h>
@@ -175,6 +176,22 @@ free_ioctl_req:
 	return err;
 }
 
+static int vblk_sbumit_combo_ioctl_req(struct block_device *bdev,
+		unsigned int cmd, void __user *user)
+{
+	struct vblk_dev *vblkdev = bdev->bd_disk->private_data;
+
+	/*
+	 * The caller must have CAP_SYS_RAWIO, and must be calling this on the
+	 * whole block device, not on a partition.  This prevents overspray
+	 * between sibling partitions.
+	 */
+	if (!capable(CAP_SYS_RAWIO) || (bdev != bdev->bd_contains))
+		return -EPERM;
+
+	return vblk_submit_combo_query_io(vblkdev, cmd, user);
+}
+
 /* The ioctl() implementation */
 int vblk_ioctl(struct block_device *bdev, fmode_t mode,
 	unsigned int cmd, unsigned long arg)
@@ -188,6 +205,10 @@ int vblk_ioctl(struct block_device *bdev, fmode_t mode,
 	case MMC_IOC_CMD:
 	case SG_IO:
 		ret = vblk_submit_ioctl_req(bdev, cmd,
+			(void __user *)arg);
+		break;
+	case UFS_IOCTL_COMBO_QUERY:
+		ret = vblk_sbumit_combo_ioctl_req(bdev, cmd,
 			(void __user *)arg);
 		break;
 	default:  /* unknown command */

OE4T/linux-tegra-5.10

Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2402893
(cherry picked from commit 7c9b9e83e3af8aae705c9ef817f8a1b7db7ad89b)
Change-Id: I20589d6cb0320e0a83ca2c6b3e576110772c643d
Signed-off-by: std <std@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2447281
Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
Reviewed-by: Vipin Kumar <vipink@nvidia.com>

drivers/block/tegra_virt_storage/tegra_hv_ioctl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2018, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2020, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -21,6 +21,7 @@
 #include <asm-generic/bug.h>
 #include <linux/slab.h>   /* kmalloc() */
 #include <scsi/scsi.h>
+#include <uapi/scsi/ufs/ioctl.h>
 #include <scsi/sg.h>
 #include <linux/mmc/ioctl.h>
 #include <linux/version.h>
@@ -141,6 +142,22 @@ exit:
 	return err;
 }
 
+static int vblk_submit_combo_ioctl_req(struct block_device *bdev,
+		unsigned int cmd, void __user *user)
+{
+	struct vblk_dev *vblkdev = bdev->bd_disk->private_data;
+
+	/*
+	 * The caller must have CAP_SYS_RAWIO, and must be calling this on the
+	 * whole block device, not on a partition.  This prevents overspray
+	 * between sibling partitions.
+	 */
+	if (!capable(CAP_SYS_RAWIO) || (bdev != bdev->bd_contains))
+		return -EPERM;
+
+	return vblk_submit_combo_query_io(vblkdev, cmd, user);
+}
+
 /* The ioctl() implementation */
 int vblk_ioctl(struct block_device *bdev, fmode_t mode,
 	unsigned int cmd, unsigned long arg)
@@ -154,6 +171,10 @@ int vblk_ioctl(struct block_device *bdev, fmode_t mode,
 	case MMC_IOC_CMD:
 	case SG_IO:
 		ret = vblk_submit_ioctl_req(bdev, cmd,
+			(void __user *)arg);
+		break;
+	case UFS_IOCTL_COMBO_QUERY:
+		ret = vblk_submit_combo_ioctl_req(bdev, cmd,
 			(void __user *)arg);
 		break;
 	default:  /* unknown command */