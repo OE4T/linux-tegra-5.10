30a7d2b44fc5ac51c094cec046ed530d25f1f723
Vidya Sagar <vidyas@nvidia.com>
2020-12-03 19:04:48 +0530

    PCI: tegra: Set DesignWare IP version
    Set the DesignWare IP version for Tegra194 to 0x490A. This would be used
    by the DesigWare sub-system to do any version specific configuration
    (Ex:- TD bit programming for ECRC).
    
    Bug 200670381
    
    Change-Id: I79e66404d12392d3c0cda3115748b0c17853c353
    Tested-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Vidya Sagar <vidyas@nvidia.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2464749
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: Manikanta Maddireddy <mmaddireddy@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Manikanta Maddireddy <mmaddireddy@nvidia.com>

--- drivers/pci/controller/dwc/pcie-tegra194.c
+++ drivers/pci/controller/dwc/pcie-tegra194.c
@@ -2203,6 +2203,7 @@ static int tegra_pcie_dw_probe(struct platform_device *pdev)
 	pci->ops = &tegra_dw_pcie_ops;
 	pci->n_fts[0] = N_FTS_VAL;
 	pci->n_fts[1] = FTS_VAL;
+	pci->version = 0x490A;
 
 	pp = &pci->pp;
 	pcie->dev = &pdev->dev;
