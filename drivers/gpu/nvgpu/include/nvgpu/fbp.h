882e418d1b54dd3fa6b577658db5909eeb952dc1
tkudav <tkudav@nvidia.com>
2020-12-07 11:03:17 +0530

    gpu: nvgpu: Add details to common.fbp API retval
    Add doxygen comments to describe the possible return values from
    common.fbp API in case of error.
    
    JIRA NVGPU-6237
    
    Change-Id: Ifff997f18d61f855fc83914fbbe4ea09c5fafbaf
    Signed-off-by: tkudav <tkudav@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvgpu/+/2455034
    (cherry picked from commit 5d646ef5d3f18c91f2d60c0abf4cecb84154bf4a)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvgpu/+/2457392
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/gpu/nvgpu/include/nvgpu/fbp.h
+++ drivers/gpu/nvgpu/include/nvgpu/fbp.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -49,6 +49,8 @@ struct nvgpu_fbp;
  * and exposed to other units through APIs.
  *
  * @return 0 for success and <0 value for failure.
+ * @retval -ENOMEM in case there is insufficient memory to allocate struct
+ *         nvgpu_fbp
  */
 int nvgpu_fbp_init_support(struct gk20a *g);
 
