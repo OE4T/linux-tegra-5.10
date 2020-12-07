2cee96366dd16dcf7729247096b901ec9380a691
Ajith Kumar <ajithk@nvidia.com>
2020-12-07 19:02:35 +0530

    drivers: camera: remove timeout WAR
    RCE boot retry is adding to the VDK
    boot delays. With 10 sec timeout, RCE boot retries
    is taking good amount of time.
    
    For VDK, lower timeout to 5 seconds (from 10 seconds)
    to reduce the boot time of simulator.
    
    Bug 2805934
    Bug 200680689
    
    Change-Id: Iade5e4e2f7f261ec8987660b27435c0100b6adc9
    Signed-off-by: Ajith Kumar <ajithk@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2456100
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Kirill Artamonov <kartamonov@nvidia.com>
    Reviewed-by: Pekka Pessi <ppessi@nvidia.com>
    Reviewed-by: Sudhir Vyas <svyas@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/platform/tegra/tegra-camera-rtcpu.c
+++ drivers/platform/tegra/tegra-camera-rtcpu.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2020, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2015-2021, NVIDIA CORPORATION. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -42,8 +42,10 @@
 #include <linux/tegra-ivc-bus.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 0)
 #include <linux/tegra_pm_domains.h>
+#include <soc/tegra/chip-id.h>
 #else
 #include <linux/pm_domain.h>
+#include <soc/tegra/fuse.h>
 #endif
 #include <linux/tegra-rtcpu-monitor.h>
 #include <linux/tegra-rtcpu-trace.h>
@@ -1127,6 +1129,9 @@ static int tegra_cam_rtcpu_probe(struct platform_device *pdev)
 	/* 10 seconds to WAR rtcpu suspend issue (bug 2805934) */
 	timeout = 10000;
 #endif
+	if (tegra_platform_is_vdk())
+		timeout = 5000;
+
 	rtcpu->cmd_timeout = msecs_to_jiffies(timeout);
 
 	timeout = 60000;
