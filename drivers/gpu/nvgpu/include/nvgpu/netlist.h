deb9ad08b8d316c158fdef5a54a7a2eca1149aff
Seshendra Gadagottu <sgadagottu@nvidia.com>
2020-12-11 20:57:41 -0800

    gpu: nvgpu: update doxygen for common.netlist unit
    Moved description about APIs to be part of @brief.
    This is also fixing unwanted line breaks in the description
    where references are made to struct.
    
    Jira NVGPU-6183
    
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvgpu/+/2459231
    (cherry picked from commit eef9e7ac5f626a0b8e77f6292979a5dd6cbd5b89)
    Change-Id: I334917c22ad13f27ad2ac75fcd33eab29cf323b9
    Signed-off-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvgpu/+/2461747
    Reviewed-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/include/nvgpu/netlist.h
+++ drivers/gpu/nvgpu/include/nvgpu/netlist.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2020, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2021, NVIDIA CORPORATION.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -102,11 +102,11 @@ struct netlist_u32_list {
  * @brief Allocates memory for netlist bundles and populates
  * ctxsw region info from ctxsw firmware file.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function allocates memory for netlist unit variables and bundles.
  * Opens netlist ctxsw firmware file based on firmware definition and loads
  * different bundles into appropriate netlist regions structures.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return 0 in case of success, < 0 in case of failure.
  * @retval -ENOMEM if memory allocation for netlist variables or bundle fails.
@@ -117,20 +117,21 @@ int nvgpu_netlist_init_ctx_vars(struct gk20a *g);
 /**
  * @brief Frees memory allocated for netlist bundles.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function frees all allocated memory for netlist bundles and
  * netlist vars.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
+ *
  */
 void nvgpu_netlist_deinit_ctx_vars(struct gk20a *g);
 /**
  * @brief Allocates memory for netlist av list bundle.
  *
- * @param g [in]		Pointer to GPU driver struct.
- * @param avl [in]		Pointer to #netlist_av_list struct.
- *
  * This function allocates memory for netlist av list bundles for the
  * requested size from #netlist_av_list count.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
+ * @param avl [in]		Pointer to #netlist_av_list struct.
  *
  * @return  Pointer to #netlist_av.
  * @retval NULL if memory allocation for #netlist_av bundles fails.
@@ -140,11 +141,11 @@ struct netlist_av *nvgpu_netlist_alloc_av_list(struct gk20a *g,
 /**
  * @brief Allocates memory for netlist av64 list bundle.
  *
- * @param g [in]		Pointer to GPU driver struct.
- * @param av64l [in]		Pointer to #netlist_av64_list struct.
- *
  * This function allocates memory for netlist av64 list bundles for the
  * requested size from #netlist_av64_list count.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
+ * @param av64l [in]		Pointer to #netlist_av64_list struct.
  *
  * @return  Pointer to #netlist_av64 struct.
  * @retval NULL if memory allocation for #netlist_av64 bundles fails.
@@ -154,11 +155,11 @@ struct netlist_av64 *nvgpu_netlist_alloc_av64_list(struct gk20a *g,
 /**
  * @brief Allocates memory for netlist aiv list bundle.
  *
- * @param g [in]		Pointer to GPU driver struct.
- * @param aivl [in]		Pointer to #netlist_avi_list struct.
- *
  * This function allocates memory for netlist aiv list bundles for the
  * requested size from #netlist_aiv_list count.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
+ * @param aivl [in]		Pointer to #netlist_avi_list struct.
  *
  * @return  Pointer to #netlist_aiv struct.
  * @retval NULL if memory allocation for #netlist_aiv bundles fails.
@@ -168,11 +169,11 @@ struct netlist_aiv *nvgpu_netlist_alloc_aiv_list(struct gk20a *g,
 /**
  * @brief Allocates memory for netlist u32 list bundle.
  *
- * @param g [in]		Pointer to GPU driver struct.
- * @param u32l [in]		Pointer to #netlist_u32_list struct.
- *
  * This function allocates memory for netlist u32 list bundles for the
  * requested size from #netlist_u32_list count.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
+ * @param u32l [in]		Pointer to #netlist_u32_list struct.
  *
  * @return  Pointer to u32 list.
  * @retval NULL if memory allocation for u32 list bundles fails.
@@ -182,11 +183,11 @@ u32 *nvgpu_netlist_alloc_u32_list(struct gk20a *g,
 /**
  * @brief Get s/w non-context #netlist_av_list bundles from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns non-context #netlist_av_list bundles from
  * netlist firmware. SW needs to program these bundles to h/w as part
  * of golden context creation.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  Pointer to #netlist_av_list struct.
  */
@@ -195,11 +196,11 @@ struct netlist_av_list *nvgpu_netlist_get_sw_non_ctx_load_av_list(
 /**
  * @brief Get s/w context #netlist_aiv_list bundles from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns s/w context #netlist_aiv_list bundles from
  * netlist firmware. SW needs to program these bundles to h/w as part
  * of golden context creation.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  Pointer to #netlist_aiv_list struct.
  */
@@ -208,11 +209,11 @@ struct netlist_aiv_list *nvgpu_netlist_get_sw_ctx_load_aiv_list(
 /**
  * @brief Get s/w method init #netlist_av_list bundles from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns s/w method init #netlist_av_list bundles from
  * netlist firmware. SW needs to program these bundles to h/w as part
  * of golden context creation.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  Pointer to #netlist_av_list struct.
  */
@@ -221,11 +222,11 @@ struct netlist_av_list *nvgpu_netlist_get_sw_method_init_av_list(
 /**
  * @brief Get s/w init #netlist_av_list bundles from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns s/w bundles #netlist_av_list bundles from
  * netlist firmware. SW needs to program these bundles to h/w as part
  * of golden context creation.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  Pointer to #netlist_av_list struct.
  */
@@ -234,11 +235,11 @@ struct netlist_av_list *nvgpu_netlist_get_sw_bundle_init_av_list(
 /**
  * @brief Get s/w veid init #netlist_av_list bundles from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns s/w veid #netlist_av_list bundles from
  * netlist firmware. SW needs to program these bundles to h/w as part
  * of golden context creation.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  Pointer to #netlist_av_list struct.
  */
@@ -247,11 +248,11 @@ struct netlist_av_list *nvgpu_netlist_get_sw_veid_bundle_init_av_list(
 /**
  * @brief Get s/w u64 init #netlist_av64_list bundles from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns s/w u64 #netlist_av64_list bundles from
  * netlist firmware. SW needs to program these bundles to h/w as part
  * of golden context creation.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  Pointer to #netlist_av64_list struct.
  */
@@ -260,21 +261,21 @@ struct netlist_av64_list *nvgpu_netlist_get_sw_bundle64_init_av64_list(
 /**
  * @brief Get FECS imem ucode size from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns h/w fecs imem ucode size from netlist firmware.
  * SW needs this information for falcon FECS ucode programming.
  *
  * @return  FECS imem ucode size.
+ * @param g [in]		Pointer to GPU driver struct.
+ *
  */
 u32 nvgpu_netlist_get_fecs_inst_count(struct gk20a *g);
 /**
  * @brief Get FECS dmem ucode size from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns h/w fecs dmem ucode size from netlist firmware.
  * SW needs this information for falcon FECS ucode programming.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  FECS dmem ucode size.
  */
@@ -282,10 +283,10 @@ u32 nvgpu_netlist_get_fecs_data_count(struct gk20a *g);
 /**
  * @brief Get GPCCS imem ucode size from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns h/w gpccs imem ucode size from netlist firmware.
  * SW needs this information for falcon GPCCS ucode programming.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  GPCCS imem ucode size.
  */
@@ -293,10 +294,10 @@ u32 nvgpu_netlist_get_gpccs_inst_count(struct gk20a *g);
 /**
  * @brief Get GPCCS dmem ucode size from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns h/w gpccs dmem ucode size from netlist firmware.
  * SW needs this information for falcon GPCCS ucode programming.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  GPCCS dmem ucode size.
  */
@@ -304,10 +305,10 @@ u32 nvgpu_netlist_get_gpccs_data_count(struct gk20a *g);
 /**
  * @brief Get pointer to FECS imem ucode from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns pointer to fecs imem ucode from netlist firmware.
  * SW will use this information for falcon FECS imem ucode programming.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  U32 pointer to FECS imem ucode.
  */
@@ -315,10 +316,10 @@ u32 *nvgpu_netlist_get_fecs_inst_list(struct gk20a *g);
 /**
  * @brief Get pointer to FECS dmem ucode from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns pointer to fecs dmem ucode from netlist firmware.
  * SW will use this information for falcon FECS ucode programming.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  U32 pointer to FECS dmem ucode.
  */
@@ -326,10 +327,10 @@ u32 *nvgpu_netlist_get_fecs_data_list(struct gk20a *g);
 /**
  * @brief Get pointer Io GPCCS imem ucode from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns pointer to gpccs imem ucode from netlist firmware.
  * SW will use this information for falcon GPCCS ucode programming.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  U32 pointer to GPCCS imem ucode.
  */
@@ -337,10 +338,10 @@ u32 *nvgpu_netlist_get_gpccs_inst_list(struct gk20a *g);
 /**
  * @brief Get pointer to GPCCS dmem ucode from firmware.
  *
- * @param g [in]		Pointer to GPU driver struct.
- *
  * This function returns pointer to gpccs dmem ucode from netlist firmware.
  * SW will use this information for falcon GPCCS ucode programming.
+ *
+ * @param g [in]		Pointer to GPU driver struct.
  *
  * @return  U32 pointer to GPCCS dmem ucode.
  */
