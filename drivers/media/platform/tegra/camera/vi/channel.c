7e8e9c53777377b7c9c91345de5c15ab7b0d6b06
snchen <snchen@nvidia.com>
2020-12-14 16:12:12 +0800

    camera: correct the BW calculations
    Correct the bandwidth unit from bit to byte.
    
    bug 3193599
    
    Change-Id: If426cff7492d511ba631d3627e7d3948c3dc5822
    Signed-off-by: snchen <snchen@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2459367
    (cherry picked from commit fcf471eaa60b2f1dda6877a9f8459e8b8937457d)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2468366
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/media/platform/tegra/camera/vi/channel.c
+++ drivers/media/platform/tegra/camera/vi/channel.c
@@ -1,7 +1,7 @@
 /*
  * NVIDIA Tegra Video Input Device
  *
- * Copyright (c) 2015-2020, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2015-2021, NVIDIA CORPORATION.  All rights reserved.
  *
  * Author: Bryan Wu <pengw@nvidia.com>
  *
@@ -1861,6 +1861,7 @@ static void tegra_channel_populate_dev_info(struct tegra_camera_dev_info *cdev,
 	cdev->bpp = chan->fmtinfo->bpp.numerator;
 	/* BW in kBps */
 	cdev->bw = cdev->pixel_rate * cdev->bpp / 1024;
+	cdev->bw /= 8;
 }
 
 void tegra_channel_remove_subdevices(struct tegra_channel *chan)
