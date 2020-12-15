OE4T/linux-tegra-4.9

gpu: nvgpu: channel_setup_bind: must be bound to TSG
In nvgpu_channel_setup_bind, return an error if the
channel isn't bound to a TSG, as future operations
rely on being bound.

Bug 200543218

Change-Id: If33b01b8176c7488445c23080ad9d11f341bff43
Signed-off-by: Peter Daifuku <pdaifuku@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/2215160
Reviewed-by: Thomas Fleury <tfleury@nvidia.com>
GVS: Gerrit_Virtual_Submit
Reviewed-by: Luis Dib <ldib@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/gpu/nvgpu/common/fifo/channel.c
@@ -1136,6 +1136,7 @@ int nvgpu_channel_setup_bind(struct channel_gk20a *c,
 {
 	struct gk20a *g = c->g;
 	struct vm_gk20a *ch_vm;
+	struct tsg_gk20a *tsg;
 	u32 gpfifo_size, gpfifo_entry_size;
 	u64 gpfifo_gpu_va;
 	int err = 0;
@@ -1176,6 +1177,16 @@ int nvgpu_channel_setup_bind(struct channel_gk20a *c,
 		err = -EINVAL;
 		goto clean_up_idle;
 	}
+
+	/* The channel needs to be bound to a tsg at this point */
+	tsg = tsg_gk20a_from_ch(c);
+	if (tsg == NULL) {
+		nvgpu_err(g,
+			"not bound to tsg at time of setup_bind");
+		err = -EINVAL;
+		goto clean_up_idle;
+	}
+
 	ch_vm = c->vm;
 
 	if (nvgpu_mem_is_valid(&c->gpfifo.mem) ||

OE4T/linux-tegra-5.10

Update usermode setup_bind test to bind channel to
the tsg before calling nvgpu_setup_bind
Manual port from rel-32
(cherry picked from commit 56f8e5b878348d1ac7f0b133d395808e0caed769)
Reviewed-on: https://git-master.nvidia.com/r/2218885
Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
Reviewed-by: svc-mobile-misra <svc-mobile-misra@nvidia.com>
Reviewed-by: Alex Waterman <alexw@nvidia.com>

drivers/gpu/nvgpu/common/fifo/channel.c
@@ -2201,6 +2201,7 @@ int nvgpu_channel_setup_bind(struct nvgpu_channel *c,
 		struct nvgpu_setup_bind_args *args)
 {
 	struct gk20a *g = c->g;
+	struct nvgpu_tsg *tsg;
 	int err = 0;
 
 #ifdef CONFIG_NVGPU_VPR
@@ -2236,6 +2237,15 @@ int nvgpu_channel_setup_bind(struct nvgpu_channel *c,
 	if (!nvgpu_channel_as_bound(c)) {
 		nvgpu_err(g,
 			"not bound to an address space at time of setup_bind");
+		err = -EINVAL;
+		goto clean_up_idle;
+	}
+
+	/* The channel needs to be bound to a tsg at this point */
+	tsg = nvgpu_tsg_from_ch(c);
+	if (tsg == NULL) {
+		nvgpu_err(g,
+			"not bound to tsg at time of setup_bind");
 		err = -EINVAL;
 		goto clean_up_idle;
 	}