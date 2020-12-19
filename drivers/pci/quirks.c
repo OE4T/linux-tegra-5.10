1351eedeb45537dda5f472faae5d0d63a41d3566
Manikanta Maddireddy <mmaddireddy@nvidia.com>
2020-12-19 18:17:38 +0530

    PCI: Disable MSI for Tegra FPGA root port
    Tegra PCIe rootports don't generate MSI interrupts for PME and AER events.
    Since PCIe spec (Ref: r4.0 sec 7.7.1.2 and 7.7.2.2) doesn't support using
    a mix of INTx and MSI/MSI-X, MSI needs to be disabled to avoid root ports
    service drivers registering their respective ISRs with MSI interrupt and
    to let only INTx be used for all events.
    
    Bug 200670381
    
    Change-Id: I388d4ab0678a11b00533ee95d877c17f063e0924
    Signed-off-by: Manikanta Maddireddy <mmaddireddy@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.9/+/2464752
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: Vidya Sagar <vidyas@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -2717,6 +2717,9 @@ DECLARE_PCI_FIXUP_CLASS_EARLY(PCI_VENDOR_ID_NVIDIA, 0x10e5,
 DECLARE_PCI_FIXUP_CLASS_EARLY(PCI_VENDOR_ID_NVIDIA, 0x10e6,
 			      PCI_CLASS_BRIDGE_PCI, 8,
 			      pci_quirk_nvidia_tegra_disable_rp_msi);
+DECLARE_PCI_FIXUP_CLASS_EARLY(PCI_VENDOR_ID_NVIDIA, 0x229e,
+			      PCI_CLASS_BRIDGE_PCI, 8,
+			      pci_quirk_nvidia_tegra_disable_rp_msi);
 
 /*
  * Some versions of the MCP55 bridge from Nvidia have a legacy IRQ routing
