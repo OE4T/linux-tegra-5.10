OE4T/linux-tegra-4.9

t19x: jakku: Added overlay for M.2 Key E Slot
Below overlay is added for use with Jetson-IO tool:
 - M.2 Key E Slot, with i2s3/dmic1+dmic2 on pins (8, 10, 12, 14)

Bug 200581790

Change-Id: I36308a04f53f0bf24f51b018ded64356a1f35b5a
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/jakku-dts/+/2423935
Reviewed-by: svcacv <svcacv@nvidia.com>
Reviewed-by: Viswanath L <viswanathl@nvidia.com>
Reviewed-by: Sharad Gupta <sharadg@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
GVS: Gerrit_Virtual_Submit
Tested-by: Viswanath L <viswanathl@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

Makefile
@@ -11,6 +11,7 @@ dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-adafruit-sph0
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-adafruit-uda1334a.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-fe-pi-audio.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-respeaker-4-mic-array.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-m2ke.dtbo
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))

OE4T/linux-tegra-5.10

(cherry picked from commit 4388f18440797ada57ede98bc9da5741a919741a)
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/jakku-dts/+/2533817
Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>

Makefile
@@ -15,6 +15,7 @@ dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-hdr40.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-adafruit-sph0645lm4h.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-fe-pi-audio.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-respeaker-4-mic-array.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-m2ke.dtbo
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))