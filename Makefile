OE4T/linux-tegra-4.9

t19x: mccoy: Adafruit UDA1334A support
Added overlay for supporting
Adafruit I2S Stereo Decoder UDA1334A breakout

Bug 200679216

Change-Id: I0066762c045e630273647ec9e4326ad00d7a537f
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/mccoy-dts/+/2479724
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Viswanath L <viswanathl@nvidia.com>
Reviewed-by: Asha Talambedu <atalambedu@nvidia.com>
Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: Asha Talambedu <atalambedu@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
GVS: Gerrit_Virtual_Submit

Makefile
@@ -9,6 +9,7 @@ dts-include += platform/t19x/
 dtb-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000.dtb
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-hdr40.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-adafruit-sph0645lm4h.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-adafruit-uda1334a.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-fe-pi-audio.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-array.dtbo
 

OE4T/linux-tegra-5.10

(cherry picked from commit 2f3cd8d5101bda7a7eee80173b4b681640c4923a)
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/mccoy-dts/+/2536045
Reviewed-by: svcacv <svcacv@nvidia.com>
Reviewed-by: Sharad Gupta <sharadg@nvidia.com>

Makefile
@@ -14,6 +14,7 @@ endif
 dtb-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000.dtb
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-hdr40.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-adafruit-sph0645lm4h.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-adafruit-uda1334a.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-fe-pi-audio.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-array.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-csi.dtbo