524418980d09df1760f1ed89e6e45cbf418074fe
Antony Clince Alex <aalex@nvidia.com>
2021-06-18 09:42:02 +0000

    gpu: nvgpu: ga10b: fix typo in header guard
    Header guard macro was incorrectly defined as 'USERD_GK10B_H' instead
    of 'USERD_GA10B_H'.
    
    Change-Id: I5e22fe55eba068b73614989da3b7e524da5a95f5
    Signed-off-by: Antony Clince Alex <aalex@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvgpu/+/2547000
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    Reviewed-by: Vijayakumar Subbu <vsubbu@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/nvgpu/hal/fifo/userd_ga10b.h
+++ drivers/gpu/nvgpu/hal/fifo/userd_ga10b.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2020-2021, NVIDIA CORPORATION.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -21,7 +21,7 @@
  */
 
 #ifndef USERD_GA10B_H
-#define USERD_GK10B_H
+#define USERD_GA10B_H
 
 #ifdef CONFIG_NVGPU_USERD
 struct gk20a;
