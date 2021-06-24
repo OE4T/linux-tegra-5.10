8da617690aec4a23991c817cd85357c70be9a5f3
Double Lo <double.lo@cypress.com>
2021-06-24 16:50:24 -0700

    [PATCH 122/149] brcmfmac: load 54591 firmware for chip ID 0x4355
    Use 54591 firmware for chip ID 0x4355.
    
    Bug 3328249
    
    Change-Id: I4173d94e03a7b4b9e9a5e1c06b37d31d0ecec0e7
    Signed-off-by: Double Lo <double.lo@cypress.com>
    Signed-off-by: Chi-hsien Lin <chi-hsien.lin@cypress.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.10/+/2549650
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    Reviewed-by: Mohan Thadikamalla <mohant@nvidia.com>
    Reviewed-by: Bibhay Ranjan <bibhayr@nvidia.com>
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Shobek Attupurath <sattupurath@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/pcie.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/pcie.c
@@ -58,8 +58,7 @@ BRCMF_FW_DEF(4365C, "brcmfmac4365c-pcie");
 BRCMF_FW_DEF(4366B, "brcmfmac4366b-pcie");
 BRCMF_FW_DEF(4366C, "brcmfmac4366c-pcie");
 BRCMF_FW_DEF(4371, "brcmfmac4371-pcie");
-CY_FW_DEF(4355, "cyfmac89459-pcie");
-CY_FW_DEF(54591, "cyfmac54591-pcie");
+CY_FW_DEF(4355, "cyfmac54591-pcie");
 
 static const struct brcmf_firmware_mapping brcmf_pcie_fwnames[] = {
 	BRCMF_FW_ENTRY(BRCM_CC_43602_CHIP_ID, 0xFFFFFFFF, 43602),
@@ -81,7 +80,6 @@ static const struct brcmf_firmware_mapping brcmf_pcie_fwnames[] = {
 	BRCMF_FW_ENTRY(BRCM_CC_43664_CHIP_ID, 0xFFFFFFF0, 4366C),
 	BRCMF_FW_ENTRY(BRCM_CC_4371_CHIP_ID, 0xFFFFFFFF, 4371),
 	BRCMF_FW_ENTRY(CY_CC_89459_CHIP_ID, 0xFFFFFFFF, 4355),
-	BRCMF_FW_ENTRY(CY_CC_54591_CHIP_ID, 0xFFFFFFFF, 54591),
 };
 
 #define BRCMF_PCIE_FW_UP_TIMEOUT		5000 /* msec */
@@ -1415,12 +1413,7 @@ int brcmf_pcie_get_fwname(struct device *dev, const char *ext, u8 *fw_name)
 	};
 	u32 chip;
 
-	if (devinfo->ci->chip == CY_CC_89459_CHIP_ID &&
-	    devinfo->pdev->device == CY_PCIE_54591_DEVICE_ID)
-		chip = CY_CC_54591_CHIP_ID;
-	else
-		chip = bus_if->chip;
-
+	chip = bus_if->chip;
 	fwreq = brcmf_fw_alloc_request(chip, bus_if->chiprev,
 				       brcmf_pcie_fwnames,
 				       ARRAY_SIZE(brcmf_pcie_fwnames),
@@ -1862,11 +1855,7 @@ brcmf_pcie_prepare_fw_request(struct brcmf_pciedev_info *devinfo)
 	};
 	u32 chip;
 
-	if (devinfo->ci->chip == CY_CC_89459_CHIP_ID &&
-	    devinfo->pdev->device == CY_PCIE_54591_DEVICE_ID)
-		chip = CY_CC_54591_CHIP_ID;
-	else
-		chip = devinfo->ci->chip;
+	chip = devinfo->ci->chip;
 	fwreq = brcmf_fw_alloc_request(chip, devinfo->ci->chiprev,
 				       brcmf_pcie_fwnames,
 				       ARRAY_SIZE(brcmf_pcie_fwnames),
