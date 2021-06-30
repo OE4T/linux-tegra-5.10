OE4T/linux-tegra-4.9

t19x: jaxi: Create new dtb for safe-jetpack
* Clone from p2888-0008-p2822-0000 dtb.
* Enable safety-sce nodes.

Bug 200652821
Bug 200674619

Change-Id: I3d587ee30376a8698596469642137442fd552416
Signed-off-by: Mantravadi Karthik <mkarthik@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/galen-industrial-dts/+/2497555
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
Reviewed-by: svcacv <svcacv@nvidia.com>
Reviewed-by: Bibek Basu <bbasu@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
GVS: Gerrit_Virtual_Submit

Makefile
@@ -6,6 +6,7 @@ makefile-path := platform/t19x/galen-industrial/kernel-dts
 
 dtb-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0008-e3366-1199.dtb
 dtb-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0008-p2822-0000.dtb
+dtb-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0008-p2822-0000-safejetpack.dtb
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))

OE4T/linux-tegra-5.10

(cherry picked from commit d008de5bf7d6947b4927996928d3ddb5e93974ee)
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/galen-industrial-dts/+/2549784
Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>

Makefile
@@ -15,6 +15,7 @@ endif
 
 dtb-$(BUILD_19x_ENABLE) += tegra194-p2888-0008-e3366-1199.dtb
 dtb-$(BUILD_19x_ENABLE) += tegra194-p2888-0008-p2822-0000.dtb
+dtb-$(BUILD_19x_ENABLE) += tegra194-p2888-0008-p2822-0000-safejetpack.dtb
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))