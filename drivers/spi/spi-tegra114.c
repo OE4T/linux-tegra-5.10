OE4T/linux-tegra-4.9

spi: tegra: de-assert CS before resetting CLK
CS de-assert is required before clock goes to it's default state.

Bug 200106113
Bug 200124829

Change-Id: I525613c099f65a08a0dc3de6e4a5bc3a9c3d708e
Signed-off-by: Sanket Parmar <sparmer@nvidia.com>
(cherry picked from commit 6471929fbe25058707f9133271e10a29c68112c9)
Reviewed-on: http://git-master/r/787081
(cherry picked from commit 7ac1a5ca2608f080d137f96a99f07540683d27b9)
Signed-off-by: Amlan Kundu <akundu@nvidia.com>
Signed-off-by: Krishna Yarlagadda <kyarlagadda@nvidia.com>
Reviewed-on: http://git-master/r/1241486
Reviewed-by: Sumeet Gupta <sumeetg@nvidia.com>
(cherry picked from commit 14dc5c36c0d1c609cf6847344aff5238c4f17a2f)
Reviewed-on: https://git-master.nvidia.com/r/1559215
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/spi/spi-tegra114.c
@@ -1385,6 +1385,7 @@ static int tegra_spi_transfer_one_message(struct spi_master *master,
 	int ret;
 	int gval = 1;
 	bool skip = false;
+	u32 cmd1 = 0;
 
 	msg->status = 0;
 	msg->actual_length = 0;
@@ -1394,7 +1395,6 @@ static int tegra_spi_transfer_one_message(struct spi_master *master,
 
 	single_xfer = list_is_singular(&msg->transfers);
 	list_for_each_entry(xfer, &msg->transfers, transfer_list) {
-		u32 cmd1;
 
 		reinit_completion(&tspi->xfer_completion);
 
@@ -1475,6 +1475,21 @@ complete_xfer:
 	}
 	ret = 0;
 exit:
+	/* CS de-assert is required before clock goes to it's default state. */
+	cmd1 = tegra_spi_readl(tspi, SPI_COMMAND1);
+	if (!tspi->is_hw_based_cs) {
+		if (spi->mode & SPI_CS_HIGH) {
+			/* Active high. Reset the value to make it deactive */
+			cmd1 &= ~SPI_CS_SS_VAL;
+		} else {
+			/* Active low. Set the value to make it deactive */
+			cmd1 |= SPI_CS_SS_VAL;
+		}
+	}
+
+	tegra_spi_writel(tspi, cmd1, SPI_COMMAND1);
+	tegra_spi_writel(tspi, tspi->def_command1_reg, SPI_COMMAND1);
+
 	msg->status = ret;
 	spi_finalize_current_message(master);
 	return ret;

OE4T/linux-tegra-5.10

(cherry picked from commit 7d1e2b752b3a6932579cd6d561bebea9b2e583bc)
Reviewed-on: https://git-master.nvidia.com/r/1747177
(cherry picked from commit a95e568abf94340ffa61559c017fd344f9415299)
Signed-off-by: Vishwaroop A <va@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.10/+/2474746
Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
GVS: Gerrit_Virtual_Submit

drivers/spi/spi-tegra114.c
@@ -1380,6 +1380,7 @@ static int tegra_spi_transfer_one_message(struct spi_controller *ctrl,
 	int gval = 1;
 	bool skip = false;
 	int single_xfer;
+	u32 cmd1 = 0;
 
 	msg->status = 0;
 	msg->actual_length = 0;
@@ -1389,7 +1390,6 @@ static int tegra_spi_transfer_one_message(struct spi_controller *ctrl,
 
 	single_xfer = list_is_singular(&msg->transfers);
 	list_for_each_entry(xfer, &msg->transfers, transfer_list) {
-		u32 cmd1;
 
 		reinit_completion(&tspi->xfer_completion);
 
@@ -1471,6 +1471,21 @@ complete_xfer:
 	}
 	ret = 0;
 exit:
+	/* CS de-assert is required before clock goes to it's default state. */
+	cmd1 = tegra_spi_readl(tspi, SPI_COMMAND1);
+	if (!tspi->is_hw_based_cs) {
+		if (spi->mode & SPI_CS_HIGH) {
+			/* Active high. Reset the value to make it deactive */
+			cmd1 &= ~SPI_CS_SW_VAL;
+		} else {
+			/* Active low. Set the value to make it deactive */
+			cmd1 |= SPI_CS_SW_VAL;
+		}
+	}
+
+	tegra_spi_writel(tspi, cmd1, SPI_COMMAND1);
+	tegra_spi_writel(tspi, tspi->def_command1_reg, SPI_COMMAND1);
+
 	msg->status = ret;
 	spi_finalize_current_message(ctrl);
 	return ret;