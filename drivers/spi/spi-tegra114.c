InES-HPMM/linux-l4t

spi: tegra: select best clk source
Choose from various parents to provide best clock source

Bug 200043595

Change-Id: If614366b3c0a0fd05f3e8bac0465907a8160ca38
Signed-off-by: Krishna Yarlagadda <kyarlagadda@nvidia.com>

drivers/spi/spi-tegra114.c
@@ -198,6 +198,7 @@ struct tegra_spi_data {
 	bool					clock_always_on;
 	u32					spi_max_frequency;
 	u32					cur_speed;
+	unsigned				min_div;
 
 	struct spi_device			*cur_spi;
 	unsigned				cur_pos;
@@ -784,6 +785,82 @@ static void tegra_spi_deinit_dma_param(struct tegra_spi_data *tspi,
 	dma_release_channel(dma_chan);
 }
 
+static void set_best_clk_source(struct spi_device *spi,
+		unsigned long rate)
+{
+	long new_rate;
+	unsigned long err_rate, crate, prate;
+	unsigned int cdiv, fin_err = rate;
+	int ret;
+	struct clk *pclk, *fpclk = NULL;
+	const char *pclk_name, *fpclk_name;
+	struct device_node *node;
+	struct property *prop;
+	struct tegra_spi_data *tspi = spi_master_get_devdata(spi->master);
+
+	node = spi->master->dev.of_node;
+	if (!of_property_count_strings(node, "nvidia,clk-parents"))
+		return;
+
+	/* when parent of a clk changes divider is not changed
+	 * set a min div with which clk will not cross max rate
+	 */
+	if (!tspi->min_div) {
+		of_property_for_each_string(node, "nvidia,clk-parents",
+				prop, pclk_name) {
+			pclk = clk_get(tspi->dev, pclk_name);
+			if (IS_ERR(pclk))
+				continue;
+			prate = clk_get_rate(pclk);
+			crate = tspi->spi_max_frequency;
+			cdiv = DIV_ROUND_UP(prate, crate);
+			if (cdiv > tspi->min_div)
+				tspi->min_div = cdiv;
+		}
+	}
+
+	pclk = clk_get_parent(tspi->clk);
+	crate = clk_get_rate(tspi->clk);
+	prate = clk_get_rate(pclk);
+	cdiv = DIV_ROUND_UP(prate, crate);
+	if (cdiv < tspi->min_div) {
+		crate = DIV_ROUND_UP(prate, tspi->min_div);
+		clk_set_rate(tspi->clk, crate);
+	}
+
+	of_property_for_each_string(node, "nvidia,clk-parents",
+				prop, pclk_name) {
+		pclk = clk_get(tspi->dev, pclk_name);
+		if (IS_ERR(pclk))
+			continue;
+
+		ret = clk_set_parent(tspi->clk, pclk);
+		if (ret < 0) {
+			dev_warn(tspi->dev,
+				"Error in setting parent clk src %s\n",
+				pclk_name);
+			continue;
+		}
+
+		new_rate = clk_round_rate(tspi->clk, rate);
+		if (new_rate < 0)
+			continue;
+
+		err_rate = abs(new_rate - rate);
+		if (err_rate < fin_err) {
+			fpclk = pclk;
+			fin_err = err_rate;
+			fpclk_name = pclk_name;
+		}
+	}
+
+	if (fpclk) {
+		dev_dbg(tspi->dev, "Setting clk_src %s\n",
+				fpclk_name);
+		clk_set_parent(tspi->clk, fpclk);
+	}
+}
+
 static int tegra_spi_start_transfer_one(struct spi_device *spi,
 		struct spi_transfer *t, bool is_first_of_msg,
 		bool is_single_xfer)
@@ -802,6 +879,7 @@ static int tegra_spi_start_transfer_one(struct spi_device *spi,
 	if (!speed)
 		speed = tspi->spi_max_frequency;
 	if (speed != tspi->cur_speed) {
+		set_best_clk_source(spi, speed);
 		ret = clk_set_rate(tspi->clk, speed);
 		if (ret) {
 			dev_err(tspi->dev, "Failed to set clk freq %d\n", ret);
@@ -1563,6 +1641,7 @@ static int tegra_spi_probe(struct platform_device *pdev)
 	tspi->max_buf_size = SPI_FIFO_DEPTH << 2;
 	tspi->dma_buf_size = DEFAULT_SPI_DMA_BUF_LEN;
 	tspi->spi_max_frequency = pdata->spi_max_frequency;
+	tspi->min_div = 0;
 
 	ret = tegra_spi_init_dma_param(tspi, true);
 	if (ret < 0)

OE4T/linux-tegra-5.10

Fix build error as "'fpclk_name' may be used uninitialized".
Check and avoid divide by zero where clock rate can be zero.
Bug 200319741
Coverity ID 29578
Bug 200601675
Change based on below commits from linux-4.14
a065748 spi: tegra: select best clk source
04c60ef spi: spi-tegra114: Avoid divide by zero
Change-Id: I286f85db86ea7d6b8cd4e4db0c33b560b8c00513
(cherry picked from commit 97b7595ef19ee2b823959be1db7bed77f43122e6)
Reviewed-on: http://git-master/r/1165022
(cherry picked from commit f9eb68d31e00b9b61d7239de9c37ed54873bc50e)
Reviewed-on: https://git-master.nvidia.com/r/1747160
(cherry picked from commit a06574834fd107ebccfae3d617df1542f828704d and
edited by va to work in k5.10)
Signed-off-by: Vishwaroop A <va@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.10/+/2474738
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/spi/spi-tegra114.c
@@ -190,6 +190,7 @@ struct tegra_spi_data {
 	unsigned				irq;
 	bool					clock_always_on;
 	u32					cur_speed;
+	unsigned				min_div;
 
 	struct spi_device			*cur_spi;
 	struct spi_device			*cs_control;
@@ -850,6 +851,96 @@ static void tegra_spi_set_cmd2(struct spi_device *spi, u32 speed)
 	tspi->last_used_cs = spi->chip_select;
 }
 
+static void set_best_clk_source(struct tegra_spi_data *tspi,
+				unsigned long rate)
+{
+	long new_rate;
+	unsigned long err_rate, crate, prate;
+	unsigned int cdiv, fin_err = rate;
+	int ret;
+	struct clk *pclk, *fpclk = NULL;
+	const char *pclk_name, *fpclk_name = NULL;
+	struct device_node *node;
+	struct property *prop;
+
+	node = tspi->ctrl->dev.of_node;
+	if (!of_property_count_strings(node, "nvidia,clk-parents"))
+		return;
+
+	/* when parent of a clk changes divider is not changed
+	 * set a min div with which clk will not cross max rate
+	 */
+	if (!tspi->min_div) {
+		of_property_for_each_string(node, "nvidia,clk-parents",
+					    prop, pclk_name) {
+			pclk = clk_get(tspi->dev, pclk_name);
+			if (IS_ERR(pclk))
+				continue;
+			prate = clk_get_rate(pclk);
+			crate = tspi->ctrl->max_speed_hz;
+			cdiv = DIV_ROUND_UP(prate, crate);
+			if (cdiv > tspi->min_div)
+				tspi->min_div = cdiv;
+		}
+	}
+
+	pclk = clk_get_parent(tspi->clk);
+	crate = clk_get_rate(tspi->clk);
+	prate = clk_get_rate(pclk);
+	if (crate) {
+		cdiv = DIV_ROUND_UP(prate, crate);
+		if (cdiv < tspi->min_div) {
+			crate = DIV_ROUND_UP(prate, tspi->min_div);
+			clk_set_rate(tspi->clk, crate);
+		}
+	}
+
+	of_property_for_each_string(node, "nvidia,clk-parents",
+				    prop, pclk_name) {
+		pclk = clk_get(tspi->dev, pclk_name);
+		if (IS_ERR(pclk))
+			continue;
+
+		ret = clk_set_parent(tspi->clk, pclk);
+		if (ret < 0)
+			continue;
+
+		new_rate = clk_round_rate(tspi->clk, rate);
+		if (new_rate < 0)
+			continue;
+
+		err_rate = abs(new_rate - rate);
+		if (err_rate < fin_err) {
+			fpclk = pclk;
+			fin_err = err_rate;
+			fpclk_name = pclk_name;
+		}
+	}
+
+	if (fpclk) {
+		dev_dbg(tspi->dev, "Setting clk_src %s\n",
+			fpclk_name);
+		clk_set_parent(tspi->clk, fpclk);
+	}
+}
+
+static int tegra_spi_set_clock_rate(struct tegra_spi_data *tspi, u32 speed)
+{
+	int ret;
+
+	if (speed == tspi->cur_speed)
+		return 0;
+	set_best_clk_source(tspi, speed);
+	ret = clk_set_rate(tspi->clk, speed);
+	if (ret) {
+		dev_err(tspi->dev, "Failed to set clk freq %d\n", ret);
+		return -EINVAL;
+	}
+	tspi->cur_speed = speed;
+
+	return 0;
+}
+
 static u32 tegra_spi_setup_transfer_one(struct spi_device *spi,
 					struct spi_transfer *t,
 					bool is_first_of_msg,
@@ -861,11 +952,11 @@ static u32 tegra_spi_setup_transfer_one(struct spi_device *spi,
 	u8 bits_per_word = t->bits_per_word;
 	u32 command1;
 	int req_mode;
+	int ret;
 
-	if (speed != tspi->cur_speed) {
-		clk_set_rate(tspi->clk, speed);
-		tspi->cur_speed = speed;
-	}
+	ret = tegra_spi_set_clock_rate(tspi, speed);
+	if (ret < 0)
+		return ret;
 
 	tspi->cur_spi = spi;
 	tspi->cur_pos = 0;
@@ -1451,6 +1542,10 @@ static void tegra_spi_parse_dt(struct tegra_spi_data *tspi)
 	if (of_find_property(np, "nvidia,clock-always-on", NULL))
 		tspi->clock_always_on = true;
 
+	if (of_property_read_u32(np, "spi-max-frequency",
+				 &tspi->ctrl->max_speed_hz))
+		tspi->ctrl->max_speed_hz = 25000000; /* 25MHz */
+
 	/*
 	 * Last child node or first node which has property as default-cs will
 	 * become the default. When no client is defined, default chipselect
@@ -1528,10 +1623,6 @@ static int tegra_spi_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, ctrl);
 	tspi = spi_controller_get_devdata(ctrl);
 
-	if (of_property_read_u32(pdev->dev.of_node, "spi-max-frequency",
-				 &ctrl->max_speed_hz))
-		ctrl->max_speed_hz = 25000000; /* 25MHz */
-
 	/* the spi->mode bits understood by this driver: */
 	ctrl->use_gpio_descriptors = true;
 	ctrl->mode_bits = SPI_CPOL | SPI_CPHA | SPI_CS_HIGH | SPI_LSB_FIRST |
@@ -1588,6 +1679,7 @@ static int tegra_spi_probe(struct platform_device *pdev)
 
 	tspi->max_buf_size = SPI_FIFO_DEPTH << 2;
 	tspi->dma_buf_size = DEFAULT_SPI_DMA_BUF_LEN;
+	tspi->min_div = 0;
 
 	ret = tegra_spi_init_dma_param(tspi, true);
 	if (ret < 0)