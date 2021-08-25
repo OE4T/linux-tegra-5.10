17f47ca99c7cf0df3faceab14a5710d9e5507a80
Mikko Perttunen <mperttunen@nvidia.com>
2021-08-25 11:35:08 +0300

    video: tegra: host: Skip timed out jobs
    Instead of NOOPing and rerunning timed out jobs, just skip them.
    This has no harmful effect, and is necessary not to hang the
    channel on T234 where executing NOOP opcodes outside of MLOCK
    triggers ILLEGAL_MLOCK.
    
    Bug 3365097
    
    Change-Id: I7fe6874eeddb6daf0ae3b006b9971e559458b7e9
    Signed-off-by: Mikko Perttunen <mperttunen@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2582832
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    Reviewed-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/host/nvhost_cdma.c
+++ drivers/video/tegra/host/nvhost_cdma.c
@@ -460,7 +460,7 @@ out:
 	is_empty = list_empty(&cdma->sync_queue);
 	mutex_unlock(&cdma->sync_queue_lock);
 	if (!is_empty)
-		get_restart = job->first_get;
+		get_restart = (job->first_get + job->num_slots * 8) & (PUSH_BUFFER_SIZE - 1);
 
 	/* do CPU increments as long as this context continues */
 	mutex_lock(&cdma->sync_queue_lock);
@@ -487,6 +487,8 @@ out:
 		/* cleanup push buffer */
 		cdma_op().timeout_pb_cleanup(cdma, job->first_get,
 			job->num_slots);
+
+		get_restart = (job->first_get + job->num_slots * 8) & (PUSH_BUFFER_SIZE - 1);
 	}
 	mutex_unlock(&cdma->sync_queue_lock);
 
