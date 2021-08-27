4584786bef6981a534d88e8e1bf02e7a83427b3b
Jon Hunter <jonathanh@nvidia.com>
2021-08-27 13:30:35 +0100

    UPSTREAM: drm/tegra: Update NVDEC driver to latest upstream version
    Update the NVDEC driver to the latest version that is being upstreamed.
    Main notable updates are ...
    
    1. Remove reset handling, done by power domain code
    2. Add pm-runtime autosuspend support to improve performance
    3. Update device-tree property name for the NVDEC instance
    
    JIRA LS-96
    
    Change-Id: Ibe980fe0c71408fd7754b2c75d979bce12b10602
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2584498
    (cherry picked from commit 6c04abe593cfbd7c2a07b07ecb4a38fd1cfdcf60)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2587388
    Reviewed-by: David Ung <davidu@nvidia.com>
    Reviewed-by: Mikko Perttunen <mperttunen@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: David Ung <davidu@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/gpu/drm/tegra/nvdec.c
+++ drivers/gpu/drm/tegra/nvdec.c
@@ -25,6 +25,7 @@ struct nvdec_config {
 	const char *firmware;
 	unsigned int version;
 	bool supports_sid;
+	unsigned int num_instances;
 };
 
 struct nvdec {
@@ -35,13 +36,12 @@ struct nvdec {
 	struct host1x_channel *channel;
 	struct device *dev;
 	struct clk *clk;
-	struct reset_control *rst;
 
 	/* Platform configuration */
 	const struct nvdec_config *config;
 };
 
-static inline struct nvdec *to_vic(struct tegra_drm_client *client)
+static inline struct nvdec *to_nvdec(struct tegra_drm_client *client)
 {
 	return container_of(client, struct nvdec, client);
 }
@@ -56,14 +56,13 @@ static int nvdec_boot(struct nvdec *nvdec)
 #ifdef CONFIG_IOMMU_API
 	struct iommu_fwspec *spec = dev_iommu_fwspec_get(nvdec->dev);
 #endif
-	int err = 0;
+	int err;
 
 #ifdef CONFIG_IOMMU_API
 	if (nvdec->config->supports_sid && spec) {
 		u32 value;
 
-		value = TRANSCFG_ATT(1, TRANSCFG_SID_FALCON) |
-			TRANSCFG_ATT(0, TRANSCFG_SID_HW);
+		value = TRANSCFG_ATT(1, TRANSCFG_SID_FALCON) | TRANSCFG_ATT(0, TRANSCFG_SID_HW);
 		nvdec_writel(nvdec, value, VIC_TFBIF_TRANSCFG);
 
 		if (spec->num_ids > 0) {
@@ -81,8 +80,7 @@ static int nvdec_boot(struct nvdec *nvdec)
 
 	err = falcon_wait_idle(&nvdec->falcon);
 	if (err < 0) {
-		dev_err(nvdec->dev,
-			"failed to set application ID and FCE base\n");
+		dev_err(nvdec->dev, "falcon boot timed out\n");
 		return err;
 	}
 
@@ -94,7 +92,7 @@ static int nvdec_init(struct host1x_client *client)
 	struct tegra_drm_client *drm = host1x_to_drm_client(client);
 	struct drm_device *dev = dev_get_drvdata(client->host);
 	struct tegra_drm *tegra = dev->dev_private;
-	struct nvdec *nvdec = to_vic(drm);
+	struct nvdec *nvdec = to_nvdec(drm);
 	int err;
 
 	err = host1x_client_iommu_attach(client);
@@ -142,7 +140,7 @@ static int nvdec_exit(struct host1x_client *client)
 	struct tegra_drm_client *drm = host1x_to_drm_client(client);
 	struct drm_device *dev = dev_get_drvdata(client->host);
 	struct tegra_drm *tegra = dev->dev_private;
-	struct nvdec *nvdec = to_vic(drm);
+	struct nvdec *nvdec = to_nvdec(drm);
 	int err;
 
 	/* avoid a dangling pointer just in case this disappears */
@@ -251,24 +249,16 @@ static int nvdec_runtime_resume(struct device *dev)
 
 	usleep_range(10, 20);
 
-	err = reset_control_deassert(nvdec->rst);
+	err = nvdec_load_firmware(nvdec);
 	if (err < 0)
 		goto disable;
 
-	usleep_range(10, 20);
-
-	err = nvdec_load_firmware(nvdec);
-	if (err < 0)
-		goto assert;
-
 	err = nvdec_boot(nvdec);
 	if (err < 0)
-		goto assert;
+		goto disable;
 
 	return 0;
 
-assert:
-	reset_control_assert(nvdec->rst);
 disable:
 	clk_disable_unprepare(nvdec->clk);
 	return err;
@@ -277,13 +267,6 @@ disable:
 static int nvdec_runtime_suspend(struct device *dev)
 {
 	struct nvdec *nvdec = dev_get_drvdata(dev);
-	int err;
-
-	err = reset_control_assert(nvdec->rst);
-	if (err < 0)
-		return err;
-
-	usleep_range(2000, 4000);
 
 	clk_disable_unprepare(nvdec->clk);
 
@@ -293,7 +276,7 @@ static int nvdec_runtime_suspend(struct device *dev)
 static int nvdec_open_channel(struct tegra_drm_client *client,
 			    struct tegra_drm_context *context)
 {
-	struct nvdec *nvdec = to_vic(client);
+	struct nvdec *nvdec = to_nvdec(client);
 	int err;
 
 	err = pm_runtime_get_sync(nvdec->dev);
@@ -313,7 +296,7 @@ static int nvdec_open_channel(struct tegra_drm_client *client,
 
 static void nvdec_close_channel(struct tegra_drm_context *context)
 {
-	struct nvdec *nvdec = to_vic(context->client);
+	struct nvdec *nvdec = to_nvdec(context->client);
 
 	host1x_channel_put(context->channel);
 	pm_runtime_put(nvdec->dev);
@@ -331,6 +314,7 @@ static const struct nvdec_config nvdec_t210_config = {
 	.firmware = NVIDIA_TEGRA_210_NVDEC_FIRMWARE,
 	.version = 0x21,
 	.supports_sid = false,
+	.num_instances = 1,
 };
 
 #define NVIDIA_TEGRA_186_NVDEC_FIRMWARE "nvidia/tegra186/nvdec.bin"
@@ -339,6 +323,7 @@ static const struct nvdec_config nvdec_t186_config = {
 	.firmware = NVIDIA_TEGRA_186_NVDEC_FIRMWARE,
 	.version = 0x18,
 	.supports_sid = true,
+	.num_instances = 1,
 };
 
 #define NVIDIA_TEGRA_194_NVDEC_FIRMWARE "nvidia/tegra194/nvdec.bin"
@@ -347,6 +332,7 @@ static const struct nvdec_config nvdec_t194_config = {
 	.firmware = NVIDIA_TEGRA_194_NVDEC_FIRMWARE,
 	.version = 0x19,
 	.supports_sid = true,
+	.num_instances = 2,
 };
 
 static const struct of_device_id tegra_nvdec_of_match[] = {
@@ -361,8 +347,8 @@ static int nvdec_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct host1x_syncpt **syncpts;
-	struct resource *regs;
 	struct nvdec *nvdec;
+	u32 instance;
 	int err;
 
 	/* inherit DMA mask from host1x parent */
@@ -382,13 +368,7 @@ static int nvdec_probe(struct platform_device *pdev)
 	if (!syncpts)
 		return -ENOMEM;
 
-	regs = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	if (!regs) {
-		dev_err(&pdev->dev, "failed to get registers\n");
-		return -ENXIO;
-	}
-
-	nvdec->regs = devm_ioremap_resource(dev, regs);
+	nvdec->regs = devm_platform_get_and_ioremap_resource(pdev, 0, NULL);
 	if (IS_ERR(nvdec->regs))
 		return PTR_ERR(nvdec->regs);
 
@@ -398,13 +378,12 @@ static int nvdec_probe(struct platform_device *pdev)
 		return PTR_ERR(nvdec->clk);
 	}
 
-	if (!dev->pm_domain) {
-		nvdec->rst = devm_reset_control_get(dev, "nvdec");
-		if (IS_ERR(nvdec->rst)) {
-			dev_err(&pdev->dev, "failed to get reset\n");
-			return PTR_ERR(nvdec->rst);
-		}
-	}
+	err = of_property_read_u32(dev->of_node, "nvidia,instance", &instance);
+	if (err < 0)
+		instance = 0;
+
+	if (instance >= nvdec->config->num_instances)
+		return -EINVAL;
 
 	nvdec->falcon.dev = dev;
 	nvdec->falcon.regs = nvdec->regs;
@@ -418,11 +397,10 @@ static int nvdec_probe(struct platform_device *pdev)
 	INIT_LIST_HEAD(&nvdec->client.base.list);
 	nvdec->client.base.ops = &nvdec_client_ops;
 	nvdec->client.base.dev = dev;
-	{
-		u32 interface;
-		err = of_property_read_u32(dev->of_node, "nvidia,interface", &interface);
-		nvdec->client.base.class = interface ? HOST1X_CLASS_NVDEC1 : HOST1X_CLASS_NVDEC;
-	}
+	if (instance == 0)
+		nvdec->client.base.class = HOST1X_CLASS_NVDEC;
+	else
+		nvdec->client.base.class = HOST1X_CLASS_NVDEC1;
 	nvdec->client.base.syncpts = syncpts;
 	nvdec->client.base.num_syncpts = 1;
 	nvdec->dev = dev;
@@ -438,16 +416,11 @@ static int nvdec_probe(struct platform_device *pdev)
 	}
 
 	pm_runtime_enable(&pdev->dev);
-	if (!pm_runtime_enabled(&pdev->dev)) {
-		err = nvdec_runtime_resume(&pdev->dev);
-		if (err < 0)
-			goto unregister_client;
-	}
+	pm_runtime_set_autosuspend_delay(&pdev->dev, 500);
+	pm_runtime_use_autosuspend(&pdev->dev);
 
 	return 0;
 
-unregister_client:
-	host1x_client_unregister(&nvdec->client.base);
 exit_falcon:
 	falcon_exit(&nvdec->falcon);
 
