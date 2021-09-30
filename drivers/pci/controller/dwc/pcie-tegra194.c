3510dc98ea679043a113f1048b2eadadafc0a127
Manikanta Maddireddy <mmaddireddy@nvidia.com>
2021-10-01 00:54:32 +0530

    PCI: tegra194: Handle errors in BPMP response
    The return value from tegra_bpmp_transfer indicates the success or
    failure of the IPC transaction with BPMP. If the transaction
    succeeded, we also need to check the actual command's result code.
    Add code to do this.
    
    Bug 200773727
    
    Change-Id: I4e498fbb12cd7db383325cac819d13155b233850
    Signed-off-by: Manikanta Maddireddy <mmaddireddy@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.10/+/2603483
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/pci/controller/dwc/pcie-tegra194.c
+++ drivers/pci/controller/dwc/pcie-tegra194.c
@@ -2343,6 +2343,7 @@ static int tegra_pcie_bpmp_set_pll_state(struct tegra_pcie_dw *pcie,
 	struct mrq_uphy_response resp;
 	struct tegra_bpmp_message msg;
 	struct mrq_uphy_request req;
+	int err;
 
 	memset(&req, 0, sizeof(req));
 	memset(&resp, 0, sizeof(resp));
@@ -2362,7 +2363,13 @@ static int tegra_pcie_bpmp_set_pll_state(struct tegra_pcie_dw *pcie,
 	msg.rx.data = &resp;
 	msg.rx.size = sizeof(resp);
 
-	return tegra_bpmp_transfer(pcie->bpmp, &msg);
+	err = tegra_bpmp_transfer(pcie->bpmp, &msg);
+	if (err)
+		return err;
+	if (msg.rx.ret)
+		return -EINVAL;
+
+	return 0;
 }
 
 static void tegra_pcie_downstream_dev_to_D0(struct tegra_pcie_dw *pcie)
