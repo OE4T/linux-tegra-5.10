OE4T/linux-tegra-4.9

Add IMX274 camera overlay files
Add IMX274 camera overlay files to statically configure
IMX274 on McCoy. LI camera modules are no longer detected
on McCoy through their EEPROM board IDs as the 3.3v power rail
is gated for safety reasons.

bug 3223106

Change-Id: I95e72635c1462ccc6f09e4a96e84410ec353718d
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/mccoy-dts/+/2510888
Tested-by: Anubhav Rai <arai@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
Reviewed-by: Anubhav Rai <arai@nvidia.com>
Reviewed-by: svcacv <svcacv@nvidia.com>
Reviewed-by: Shrikant Dhapke <sdhapke@nvidia.com>
Reviewed-by: Sudhir Vyas <svyas@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
GVS: Gerrit_Virtual_Submit

Makefile
@@ -13,6 +13,8 @@ dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-adafruit-uda
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-fe-pi-audio.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-array.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-csi.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-dual-imx274.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-imx274.dtbo
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))

OE4T/linux-tegra-5.10

(cherry picked from commit 7b72689e4d3ce12d107cba3c3bfc0df2b689f464)
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/mccoy-dts/+/2560229
Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
Reviewed-by: Devang Kubavat <dkubavat@nvidia.com>

Makefile
@@ -18,6 +18,8 @@ dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-adafruit-uda1334a.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-fe-pi-audio.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-array.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-csi.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-dual-imx274.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-imx274.dtbo
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))