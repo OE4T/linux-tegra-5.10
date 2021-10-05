OE4T/linux-tegra-4.9

t19x: mccoy: ReSpeaker 4 Mic Linear Array support
Added overlay for supporting ReSpeaker 4 Mic Linear Array
on mccoy

Bug 200679216

Change-Id: I4ad6e10d4b21d48df7ed5691ba62553c7b2a28da
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/mccoy-dts/+/2547895
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

Makefile
@@ -12,6 +12,7 @@ dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-adafruit-sph
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-adafruit-uda1334a.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-fe-pi-audio.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-array.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-lin-array.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-csi.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-dual-imx274.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p2888-0004-e3900-0000-imx274.dtbo

OE4T/linux-tegra-5.10

(cherry picked from commit 33ab8484e430dc0a8e5020685b6a12eb80d3184c)
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/mccoy-dts/+/2551198

Makefile
@@ -17,6 +17,7 @@ dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-adafruit-sph0645lm4h.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-adafruit-uda1334a.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-fe-pi-audio.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-array.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-respeaker-4-mic-lin-array.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-csi.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-dual-imx274.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p2888-0004-e3900-0000-imx274.dtbo