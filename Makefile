OE4T/linux-tegra-4.9

t19x: jakku: Enable IMX477 on Jakku
Add IMX477 DT files to enable the sensor on Jakku.
Add Jetson IO overlay files to allow user to configure the
camera sensor.
Add support for following configs
1. Dual IMX219 default config
2. Dual IMX477
3. IMX477 on csi_portA and IMX219 on csi_portB

IMX219 Dual config enables user to switch back to the default
config if the IMX477 is configured, as Jetson IO tool currently
doesn't support rollback to previous configuration.

Bug 3296468

Change-Id: I95ca7653a892a05b5a7a0eebfd7a64484d74ec39
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/jakku-dts/+/2529004
Tested-by: Anubhav Rai <arai@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
Reviewed-by: Anubhav Rai <arai@nvidia.com>
Reviewed-by: Viswanath L <viswanathl@nvidia.com>
Reviewed-by: svcacv <svcacv@nvidia.com>
Reviewed-by: Bibek Basu <bbasu@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
GVS: Gerrit_Virtual_Submit

Makefile
@@ -12,6 +12,9 @@ dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-adafruit-uda1
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-fe-pi-audio.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-respeaker-4-mic-array.dtbo
 dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-m2ke.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-camera-imx477-imx219.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-camera-imx477-dual.dtbo
+dtbo-$(CONFIG_ARCH_TEGRA_19x_SOC) += tegra194-p3668-all-p3509-0000-camera-imx219-dual.dtbo
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))

OE4T/linux-tegra-5.10

(cherry picked from commit 637c0e5f8ffd7a236863ee38c6d20184bb10fdc7)
Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/platform/t19x/jakku-dts/+/2601910

Makefile
@@ -18,6 +18,9 @@ dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-fe-pi-audio.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-respeaker-4-mic-array.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-respeaker-4-mic-lin-array.dtbo
 dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-m2ke.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-camera-imx477-imx219.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-camera-imx477-dual.dtbo
+dtbo-$(BUILD_ENABLE) += tegra194-p3668-all-p3509-0000-camera-imx219-dual.dtbo
 
 ifneq ($(dtb-y),)
 dtb-y := $(addprefix $(makefile-path)/,$(dtb-y))