5cc81f5471777885c3bf9080c83ce5d2d7556269
Bhadram Varka <vbhadram@nvidia.com>
2022-02-08 18:16:06 +0530

    nvethernet: fix coverity defects
    CID 9871576 Untrusted array index read
    CID 10112340 Dereference before null check
    CID 10127838 Unchecked return value
    CID 10127841 Dead default in switch
    CID 10127905 Unchecked return value
    CID 10127961 Unused value
    
    Bug 3461002
    
    Change-Id: If041434e57295b282dacf06ed66b1a436b96a165
    Signed-off-by: Bhadram Varka <vbhadram@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2665776
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-cert <svc-mobile-cert@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    Reviewed-by: Mohan Thadikamalla <mohant@nvidia.com>
    Reviewed-by: Rakesh Goyal <rgoyal@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/net/ethernet/nvidia/nvethernet/sysfs.c
+++ drivers/net/ethernet/nvidia/nvethernet/sysfs.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2021, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2019-2022, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -684,10 +684,9 @@ static int parse_inputs(const char *buf,
 			case 2:
 				flags |= OSI_LUT_FLAGS_BYTE2_PATTERN_VALID;
 				break;
+			default:
 			case 3:
 				flags |= OSI_LUT_FLAGS_BYTE3_PATTERN_VALID;
-				break;
-			default:
 				break;
 			}
 			lut_in->byte_pattern[i] = byte[i];
