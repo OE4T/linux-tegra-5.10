86be5112b29e58ca79a59711b2c83301435d8d97
Rajesh Devaraj <rdevaraj@nvidia.com>
2022-03-08 20:27:57 +0000

    gpu: nvgpu: plug-in misc-ec interface for av+l
    This patch adds misc-ec interface into NvGPU driver to report GPU HW
    errors to Safety_Services, in AV+L. For this purpose, it introduces
    a new flag "CONFIG_NVGPU_ENABLE_MISC_EC".
    
    JIRA NVGPU-8094
    
    Change-Id: Id8fff69487cad9ed4eb082a7d8615a1e15867ffa
    Signed-off-by: Rajesh Devaraj <rdevaraj@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvgpu/+/2678394
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-cert <svc-mobile-cert@nvidia.com>
    Reviewed-by: Dinesh T <dt@nvidia.com>
    Reviewed-by: Ankur Kishore <ankkishore@nvidia.com>

--- drivers/gpu/nvgpu/os/linux/cic/cic_report_err.c
+++ drivers/gpu/nvgpu/os/linux/cic/cic_report_err.c
@@ -17,13 +17,33 @@
 #include <nvgpu/cic_mon.h>
 #include <nvgpu/types.h>
 
+#ifdef CONFIG_NVGPU_ENABLE_MISC_EC
+#include <linux/tegra-epl.h>
+#include "os/linux/os_linux.h"
+#endif
+
 struct gk20a;
 
-int nvgpu_cic_mon_report_err_safety_services(struct gk20a *g,
-		u32 err_id)
+int nvgpu_cic_mon_report_err_safety_services(struct gk20a *g, u32 err_id)
 {
+	int ret = 0U;
+
+#ifdef CONFIG_NVGPU_ENABLE_MISC_EC
+	struct device *dev = dev_from_gk20a(g);
+
 	/**
-	 * ToDo: Add MISC_EC API to report error.
+	 * MISC_EC_SW_ERR_CODE_0 register has been allocated for NvGPU
+	 * to report GPU HW errors to Safety_Services via MISC_EC interface.
 	 */
-	return 0;
+	ret = epl_report_misc_ec_error(dev, MISC_EC_SW_ERR_CODE_0, err_id);
+	if (ret != 0) {
+		nvgpu_err(g, "Error reporting to Safety_Services failed");
+		nvgpu_err(g, "ret (%d). err (0x%x)", ret, err_id);
+	} else {
+		nvgpu_err(g, "Reported err (0x%x) to Safety_Services",
+					err_id);
+	}
+#endif
+
+	return ret;
 }
