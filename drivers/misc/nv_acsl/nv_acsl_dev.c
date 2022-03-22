494f60b8ce0f4f35ee79e8dc33510764eeb4be2d
Prateek Patel <prpatel@nvidia.com>
2022-03-22 06:15:24 +0000

    drivers:acsl: fix coverity defect
    Checking csm_args.size > sizeof(csm_args) implies that csm_args.size
    may be up to 84 on the false branch but maximum expected payload size
    is 20.
    
    CID 10133310
    
    Bug 3461002
    
    Change-Id: I6bf4ca7e566e277d8735fd9d3597614098b1cc01
    Signed-off-by: Prateek Patel <prpatel@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2685021
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-cert <svc-mobile-cert@nvidia.com>
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    Reviewed-by: Viswanath L <viswanathl@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: Dara Ramesh <dramesh@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/misc/nv_acsl/nv_acsl_dev.c
+++ drivers/misc/nv_acsl/nv_acsl_dev.c
@@ -174,7 +174,7 @@ static long acsl_dev_ioctl(struct file *filp,
 			return -EACCES;
 		ret = copy_from_user(&csm_args, uarg,
 				sizeof(csm_args));
-		if (ret || (csm_args.size > sizeof(csm_args))) {
+		if (ret || (csm_args.size > MAX_PAYLOAD)) {
 			ret = -EACCES;
 			break;
 		}
@@ -200,7 +200,7 @@ static long acsl_dev_ioctl(struct file *filp,
 			return -EACCES;
 		ret = copy_from_user(&csm_args, uarg,
 				sizeof(csm_args));
-		if (ret || (csm_args.size > sizeof(csm_args))) {
+		if (ret || (csm_args.size > MAX_PAYLOAD)) {
 			ret = -EACCES;
 			break;
 		}
@@ -215,7 +215,7 @@ static long acsl_dev_ioctl(struct file *filp,
 			return -EACCES;
 		ret = copy_from_user(&csm_args, uarg,
 				sizeof(csm_args));
-		if (ret || (csm_args.size > sizeof(csm_args))) {
+		if (ret || (csm_args.size > MAX_PAYLOAD)) {
 			ret = -EACCES;
 			break;
 		}
@@ -266,7 +266,7 @@ static long acsl_dev_ioctl(struct file *filp,
 			return -EACCES;
 		ret = copy_from_user(&csm_args, uarg,
 				sizeof(csm_args));
-		if (ret || (csm_args.size > sizeof(csm_args))) {
+		if (ret || (csm_args.size > MAX_PAYLOAD)) {
 			ret = -EACCES;
 			break;
 		}
@@ -280,7 +280,7 @@ static long acsl_dev_ioctl(struct file *filp,
 			return -EACCES;
 		ret = copy_from_user(&csm_args, uarg,
 				sizeof(csm_args));
-		if (ret || (csm_args.size > sizeof(csm_args))) {
+		if (ret || (csm_args.size > MAX_PAYLOAD)) {
 			ret = -EACCES;
 			break;
 		}
