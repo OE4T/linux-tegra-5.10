ac879458ca4fcfd9bb4a729b2d1a6c50f552a665
Sagar Kadamati <skadamati@nvidia.com>
2022-04-08 13:33:23 +0530

    gpu: nvgpu: gr: split gm20b_gr_falcon_ctrl_ctxsw
    NVGPU_GR_FALCON_METHOD_FECS_TRACE_FLUSH is not used from gp10b
    
    So spliting gm20b_gr_falcon_ctrl_ctxsw() into below functions
     * gm20b_gr_falcon_ctrl_ctxsw()
     * gm20b_gr_falcon_ctrl_ctxsw_internal()
    
    Jira NVGPU-7287
    
    Change-Id: I00433d5ac8dc4f64d4d90c8ae0cebee424a5bd41
    Signed-off-by: Sagar Kadamati <skadamati@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvgpu/+/2694431
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: svc-mobile-cert <svc-mobile-cert@nvidia.com>
    Reviewed-by: Ankur Kishore <ankkishore@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/gpu/nvgpu/hal/gr/falcon/gr_falcon_gm20b_fusa.c
+++ drivers/gpu/nvgpu/hal/gr/falcon/gr_falcon_gm20b_fusa.c
@@ -818,12 +818,6 @@ defined(CONFIG_NVGPU_CTXSW_FW_ERROR_CODE_TESTING)
 		op.cond.fail = GR_IS_UCODE_OP_AND;
 		flags |= NVGPU_GR_FALCON_SUBMIT_METHOD_F_SLEEP;
 		break;
-#ifdef CONFIG_NVGPU_FECS_TRACE
-	case NVGPU_GR_FALCON_METHOD_FECS_TRACE_FLUSH:
-		op.method.addr =
-			gr_fecs_method_push_adr_write_timestamp_record_v();
-		break;
-#endif
 	case NVGPU_GR_FALCON_METHOD_SET_WATCHDOG_TIMEOUT:
 		op.method.addr =
 			gr_fecs_method_push_adr_set_watchdog_timeout_f();
@@ -838,6 +832,31 @@ defined(CONFIG_NVGPU_CTXSW_FW_ERROR_CODE_TESTING)
 	}
 
 	return gm20b_gr_falcon_submit_fecs_method_op(g, op, flags);
+}
+
+int gm20b_gr_falcon_ctrl_ctxsw_internal(struct gk20a *g, u32 fecs_method,
+			u32 data, u32 *ret_val)
+{
+#ifdef CONFIG_NVGPU_FECS_TRACE
+	if (fecs_method == NVGPU_GR_FALCON_METHOD_FECS_TRACE_FLUSH) {
+		struct nvgpu_fecs_method_op op = {
+			.mailbox = { .id = 0U, .data = 0U, .ret = NULL,
+					.clr = ~U32(0U), .ok = 0U, .fail = 0U},
+			.method.addr = gr_fecs_method_push_adr_write_timestamp_record_v(),
+			.method.data = 0U,
+			.cond.ok = GR_IS_UCODE_OP_NOT_EQUAL,
+			.cond.fail = GR_IS_UCODE_OP_SKIP,
+		};
+		u32 flags = 0U;
+
+		nvgpu_log_info(g, "fecs method %d data 0x%x ret_value %p",
+							fecs_method, data, ret_val);
+
+		return gm20b_gr_falcon_submit_fecs_method_op(g, op, flags);
+	}
+#endif
+
+	return gm20b_gr_falcon_ctrl_ctxsw(g, fecs_method, data, ret_val);
 }
 
 u32 gm20b_gr_falcon_get_current_ctx(struct gk20a *g)
