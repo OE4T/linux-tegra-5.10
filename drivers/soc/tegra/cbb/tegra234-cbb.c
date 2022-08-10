dcace97f7e423f5b9089b969feb4eb5a86953dfe
Amulya Yarlagadda <ayarlagadda@nvidia.com>
2022-08-10 16:17:42 -0700

    Revert "soc: tegra: cbb: fix master_id retrieval and warn"
    This reverts commit 6766fbebc27c72262672ed1e996dac48d3b49cd9.
    
    Bug 3751527
    
    Reason for revert: possible regression 
    
    Change-Id: Ia021f425cd356d796601c5a453f32f239a909c31
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.10/+/2759138
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Amulya Yarlagadda <ayarlagadda@nvidia.com>

--- drivers/soc/tegra/cbb/tegra234-cbb.c
+++ drivers/soc/tegra/cbb/tegra234-cbb.c
@@ -95,6 +95,7 @@ struct tegra234_cbb_fabric {
 	const char *name;
 	phys_addr_t off_mask_erd;
 	phys_addr_t firewall_base;
+	bool erd_mask_inband_err;
 	const char * const *master_id;
 	unsigned int notifier_offset;
 	const struct tegra_cbb_error *errors;
@@ -555,14 +556,14 @@ static irqreturn_t tegra234_cbb_isr(int irq, void *data)
 			if (err)
 				goto unlock;
 
+			mstr_id =  FIELD_GET(USRBITS_MSTR_ID, priv->mn_user_bits);
+
 			/*
-			 * If illegal request is from CCPLEX(id:0x1) master then call WARN()
+			 * If illegal request is from CCPLEX(id:0x1) master then call BUG() to
+			 * crash system.
 			 */
-			if (priv->fabric->off_mask_erd) {
-				mstr_id =  FIELD_GET(USRBITS_MSTR_ID, priv->mn_user_bits);
-				if (mstr_id == 0x1)
-					is_inband_err = 1;
-			}
+			if ((mstr_id == 0x1) && priv->fabric->off_mask_erd)
+				is_inband_err = 1;
 		}
 	}
 
