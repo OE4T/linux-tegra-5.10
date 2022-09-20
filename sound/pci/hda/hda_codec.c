5d0c665ba4f412ba1640c263bebe14feb343c3c5
Mohan Kumar <mkumard@nvidia.com>
2022-09-20 22:57:46 +0530

    ALSA: hda/tegra: Remove android specific codes
    This revert change was mainly added to send zeroes to the
    hdmi receiver and later added another change to send non-
    zero values to make the denon receiver always active. This
    change are specific to Adnroid usecase or receiver specific.
    
    As a part of OOT development on kernel side, the goal was to
    align the downstream and upstream codes for HDA driver. As these
    changes can't be upstreamed due to its nature of fix. Removing
    the code in downstream.
    
    For android usescase or release, this change can be cherry-picked
    to release branch after branch out from dev-main
    
    Bug 3763937
    Bug 24055017
    Bug 2091698
    Bug 2151174
    Bug 2258187
    Bug 2256507
    Bug 2310189
    Bug 200588542
    
    Change-Id: I695d73cb4c2b032b65ce7bdcb7d385411dd5ddc7
    Signed-off-by: Mohan Kumar <mkumard@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.10/+/2778965
    (cherry picked from commit e47facfa43c9aa5198731e61f7bdbe64761d4536)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.10/+/2788283
    Reviewed-by: Sameer Pujar <spujar@nvidia.com>
    Reviewed-by: Sharad Gupta <sharadg@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- sound/pci/hda/hda_codec.c
+++ sound/pci/hda/hda_codec.c
@@ -2410,72 +2410,6 @@ static int snd_hda_spdif_out_switch_put(struct snd_kcontrol *kcontrol,
 	return change;
 }
 
-int snd_hda_max_pcm_ch_info(struct snd_kcontrol *kcontrol,
-		struct snd_ctl_elem_info *uinfo)
-{
-	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
-	uinfo->count = 1;
-	uinfo->value.integer.min = 0;
-	uinfo->value.integer.max = 0xFFFFFFFF;
-	return 0;
-}
-
-int snd_hda_hdmi_decode_info(struct snd_kcontrol *kcontrol,
-				struct snd_ctl_elem_info *uinfo)
-{
-	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
-	uinfo->count = 1;
-	uinfo->value.integer.min = 0;
-	uinfo->value.integer.max = 0xFFFFFFFF;
-	return 0;
-}
-
-int snd_hda_comfort_noise_info(struct snd_kcontrol *kcontrol,
-		struct snd_ctl_elem_info *uinfo)
-{
-	uinfo->type = SNDRV_CTL_ELEM_TYPE_BOOLEAN;
-	uinfo->count = 1;
-	uinfo->value.integer.min = 0;
-	uinfo->value.integer.max = 1;
-	return 0;
-}
-
-static int snd_hda_max_pcm_ch_get(struct snd_kcontrol *kcontrol,
-		struct snd_ctl_elem_value *ucontrol)
-{
-	struct hda_codec *codec = snd_kcontrol_chip(kcontrol);
-
-	ucontrol->value.integer.value[0] = codec->max_pcm_channels;
-	return 0;
-}
-
-static int snd_hda_hdmi_decode_get(struct snd_kcontrol *kcontrol,
-					struct snd_ctl_elem_value *ucontrol)
-{
-	struct hda_codec *codec = snd_kcontrol_chip(kcontrol);
-
-	ucontrol->value.integer.value[0] = codec->recv_dec_cap;
-	return 0;
-}
-
-static int snd_hda_comfort_noise_get(struct snd_kcontrol *kcontrol,
-					struct snd_ctl_elem_value *ucontrol)
-{
-	struct hda_codec *codec = snd_kcontrol_chip(kcontrol);
-
-	ucontrol->value.integer.value[0] = codec->comfort_noise;
-	return 0;
-}
-
-static int snd_hda_comfort_noise_put(struct snd_kcontrol *kcontrol,
-				     struct snd_ctl_elem_value *ucontrol)
-{
-	struct hda_codec *codec = snd_kcontrol_chip(kcontrol);
-
-	codec->comfort_noise = ucontrol->value.integer.value[0];
-	return 0;
-}
-
 static const struct snd_kcontrol_new dig_mixes[] = {
 	{
 		.access = SNDRV_CTL_ELEM_ACCESS_READ,
@@ -2504,27 +2438,6 @@ static const struct snd_kcontrol_new dig_mixes[] = {
 		.info = snd_hda_spdif_out_switch_info,
 		.get = snd_hda_spdif_out_switch_get,
 		.put = snd_hda_spdif_out_switch_put,
-	},
-	{
-		.access = SNDRV_CTL_ELEM_ACCESS_READ,
-		.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
-		.name = "HDA Decode Capability",
-		.info = snd_hda_hdmi_decode_info,
-		.get = snd_hda_hdmi_decode_get,
-	},
-	{
-		.access = SNDRV_CTL_ELEM_ACCESS_READ,
-		.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
-		.name = "HDA Maximum PCM Channels",
-		.info = snd_hda_max_pcm_ch_info,
-		.get = snd_hda_max_pcm_ch_get,
-	},
-	{
-		.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
-		.name = "HDA Comfort Noise",
-		.info = snd_hda_comfort_noise_info,
-		.get = snd_hda_comfort_noise_get,
-		.put = snd_hda_comfort_noise_put,
 	},
 	{ } /* end */
 };
