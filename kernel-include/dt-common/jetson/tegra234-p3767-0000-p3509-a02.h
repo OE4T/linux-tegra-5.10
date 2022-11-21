aa77558009f02ddbb394fd2d57b4b1e8ab19bfb9
Asha Talambedu <atalambedu@nvidia.com>
2022-11-21 15:53:18 +0530

    dt-common: Add Orin NX Dev Kit board def
    DT header file is added for supporting overlays on Orin NX
    Developer Kit platform. The file includes board compatible string
    and definitions for 40-pin header.
    
    Bug 3866629
    
    Change-Id: Ic23699375f4ff2b59a5a7475dfb0c6de5f56a4f9
    Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/soc/tegra/+/2813129
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Asha Talambedu <atalambedu@nvidia.com>
    Reviewed-by: Viswanath L <viswanathl@nvidia.com>
    Reviewed-by: Sheetal . <sheetal@nvidia.com>
    Reviewed-by: Mohan Kumar D <mkumard@nvidia.com>
    Reviewed-by: Sharad Gupta <sharadg@nvidia.com>
    Tested-by: Sheetal . <sheetal@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

---
+++ kernel-include/dt-common/jetson/tegra234-p3767-0000-p3509-a02.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#include "tegra234-p3767-0000-common.h"
+
+#define JETSON_COMPATIBLE	"nvidia,p3509-0000+p3767-0000"
