efdaaf41cfbce764f8fdc7ed9a073dfc3bd1de92
Rakesh Goyal <rgoyal@nvidia.com>
2023-01-05 02:39:14 +0530

    nvethernet: fix syntax error in fpe ioctl
    Issue: Due to syntax error, only 1 byte getting
    copied from user space to kernel space.
    
    Fix: Fix syntax error. Add error logs
    
    Bug 3789747
    
    Change-Id: Id8608e51ab5bbdc92331a39344c62d16bbbae84e
    Signed-off-by: Rakesh Goyal <rgoyal@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2837523
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-cert <svc-mobile-cert@nvidia.com>
    Reviewed-by: Narayan Reddy <narayanr@nvidia.com>
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    Reviewed-by: Srinivas Ramachandran <srinivasra@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/net/ethernet/nvidia/nvethernet/ioctl.c
+++ drivers/net/ethernet/nvidia/nvethernet/ioctl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2022, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2019-2023, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -997,7 +997,8 @@ static int ether_config_fpe(struct net_device *dev,
 	}
 
 	if (copy_from_user(&ioctl_data.fpe, (void __user *)u_fpe_cfg,
-			   sizeof(struct osi_fpe_config) != 0U)) {
+			   sizeof(struct osi_fpe_config)) != 0U) {
+		dev_err(pdata->dev, "%s: copy_from_user error\n", __func__);
 		return -EFAULT;
 	}
 
