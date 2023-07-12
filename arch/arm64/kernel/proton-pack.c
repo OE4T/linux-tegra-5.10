376421e6357530e4d4846d7b66db466eb0a846a5
Johnny Liu <johnliu@nvidia.com>
2023-07-12 08:31:27 +0000

    [UPSTREAM BACKPORT]: arm64: spectre: increase parameters that can be used to turn off bhb mitigation individually
    In our environment, it was found that the mitigation BHB has a great
    impact on the benchmark performance. For example, in the lmbench test,
    the "process fork && exit" test performance drops by 20%.
    So it is necessary to have the ability to turn off the mitigation
    individually through cmdline, thus avoiding having to compile the
    kernel by adjusting the config.
    
    Bug 4162075
    Bug 3566167
    Bug 3566164
    
    Signed-off-by: Liu Song <liusong@linux.alibaba.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/1661514050-22263-1-git-send-email-liusong@linux.alibaba.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Change-Id: If67f95fc65053916c04be689e9af36cf3fa0b2a8
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-5.10/+/2935037
    Reviewed-by: Johnny Liu <johnliu@nvidia.com>
    Reviewed-by: Sumit Gupta <sumitg@nvidia.com>
    Reviewed-by: Rajkumar Kasirajan <rkasirajan@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>
    Tested-by: Johnny Liu <johnliu@nvidia.com>

--- arch/arm64/kernel/proton-pack.c
+++ arch/arm64/kernel/proton-pack.c
@@ -1052,6 +1052,14 @@ static void kvm_setup_bhb_slot(const char *hyp_vecs_start)
 static void kvm_setup_bhb_slot(const char *hyp_vecs_start) { }
 #endif /* CONFIG_KVM */
 
+static bool __read_mostly __nospectre_bhb;
+static int __init parse_spectre_bhb_param(char *str)
+{
+	__nospectre_bhb = true;
+	return 0;
+}
+early_param("nospectre_bhb", parse_spectre_bhb_param);
+
 void spectre_bhb_enable_mitigation(const struct arm64_cpu_capabilities *entry)
 {
 	enum mitigation_state fw_state, state = SPECTRE_VULNERABLE;
@@ -1063,7 +1071,7 @@ void spectre_bhb_enable_mitigation(const struct arm64_cpu_capabilities *entry)
 		/* No point mitigating Spectre-BHB alone. */
 	} else if (!IS_ENABLED(CONFIG_MITIGATE_SPECTRE_BRANCH_HISTORY)) {
 		pr_info_once("spectre-bhb mitigation disabled by compile time option\n");
-	} else if (cpu_mitigations_off()) {
+	} else if (cpu_mitigations_off() || __nospectre_bhb) {
 		pr_info_once("spectre-bhb mitigation disabled by command line option\n");
 	} else if (supports_ecbhb(SCOPE_LOCAL_CPU)) {
 		state = SPECTRE_MITIGATED;
